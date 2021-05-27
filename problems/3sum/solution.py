227d6f01aae650032e39d995b04e167a686eb4f5
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-05-27 16:03:54 +0000

    Sync LeetCode submission - 3Sum (python3)

--- problems/3sum/solution.py
+++ problems/3sum/solution.py
@@ -3,20 +3,22 @@ class Solution:
         k=len(nums)
         nums.sort()
 
-        arr=[]
+        res=[]
     
-    
-        for i in range(0,k):
+        target = 0
+        for i in range(0,k-2):
             start=i+1
             end=k-1
-            res=0-nums[i]
+            
             while start < end:
-                if nums[i]+nums[start]+nums[end]==0 and [nums[i],nums[start],nums[end]] not in arr: 
-                    arr.append([nums[i],nums[start],nums[end]])
+                cur = nums[i]
+                cur = cur + nums[start] + nums[end]
+                if cur==target and [nums[i],nums[start],nums[end]] not in res: 
+                    res.append([nums[i],nums[start],nums[end]])
                     start+=1
                     end-=1
-                elif nums[start]+nums[end] > res:
+                elif cur > target:
                     end-=1
                 else:
                     start+=1
-        return arr
+        return res
