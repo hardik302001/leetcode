cbd5733ba1f419d37cd62d73ebbc33218cb8ed28
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-06-05 06:41:23 +0000

    Sync LeetCode submission - Max Consecutive Ones III (cpp)

---
+++ problems/max_consecutive_ones_iii/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    /*
+        Approach:
+        1. The question translates to finding the max length of subArray with at most K 0s.
+        2. Use sliding window technique.
+        3. Keep count of the number of 0s found in the current window.
+        4. If the count is > K, then increment l until the count goes to <=K.
+        5. At each iteration find the maxLen.
+        6. Time complexity: O( N ).
+    
+    */
+    int longestOnes(vector<int>& A, int K) {
+        int zeroCount = 0, l = 0;
+        int maxLen = 0;
+        for( int r = 0 ; r < A.size() ; ++r ) {
+            if( A[ r ] == 0 ) ++zeroCount;
+            if( zeroCount > K ) {
+                if( A[ l ] == 0 ) --zeroCount;
+                ++l;
+            }
+            maxLen = max( maxLen, r - l + 1 );
+        }
+        return maxLen;
+    }
+};
