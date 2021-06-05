85d13f1c42b883fe40cfb558dcb33d03ba073ce2
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-06-05 06:10:06 +0000

    Sync LeetCode submission - Search a 2D Matrix II (python3)

---
+++ problems/search_a_2d_matrix_ii/solution.py
@@ -0,0 +1,25 @@
+class Solution:
+    def searchMatrix(self, mat: List[List[int]], x: int) -> bool:
+        if mat:
+            m, n = len(mat) , len(mat[0])
+            r, c = 0,  len(mat[0]) - 1             
+            
+            '''
+            
+                 *  *  *  * (*)  <--start frm this elemenet checking in both directions
+                 *  *  *  *  *
+                 *  *  *  *  *
+                 *  *  *  *  *
+                 *  *  *  *  *
+            
+            
+            '''
+            
+            while r<m and c>=0:
+                if mat[r][c]==x:
+                    return True
+                if mat[r][c]<x:
+                    r+=1
+                else:
+                    c-=1
+        return False
