98f9bf908c48f9bb640d42b49d52f1698ff684f8
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-06-17 10:46:24 +0000

    Sync LeetCode submission - Rotate List (cpp)

---
+++ problems/rotate_list/solution.cpp
@@ -0,0 +1,35 @@
+//k = k%length
+//link last node to head node
+//move head(n-k)times and make prev of head to null.
+class Solution {
+public:
+
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head==NULL or head->next==NULL or k==0){
+            return head;
+        }
+        
+        
+        int l = 1;
+        ListNode *tmp=  head;
+        while(tmp->next!=NULL){
+            l++;
+            tmp = tmp->next;
+        }
+        k = k%l;//step 1
+        tmp->next = head;//step2
+        int pos = l-k;
+        
+        for(int i = 0;i<pos;i++){
+            tmp = tmp->next;
+        }
+        
+        
+        head = tmp->next;
+        tmp->next = NULL;
+        
+        return head;
+        
+        
+    }
+};
