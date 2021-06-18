6ab81784d0fe0e4319570e3b471cf1f575972c43
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-06-18 08:50:35 +0000

    Sync LeetCode submission - Kth Largest Element in an Array (cpp)

--- problems/kth_largest_element_in_an_array/solution.cpp
+++ problems/kth_largest_element_in_an_array/solution.cpp
@@ -1,51 +1,24 @@
-//COURTESY: CODINGMINUTES..
-//QUICKSELECT :KTH SMALLEST
-//BUT HERE kth largest..so n-k smallest in reversed!!
-//thats all we did
-
-
+//for better method see previous code
+//Method 2: Using priority queue of size k
 
 class Solution {
 public:
-    int partition(vector<int>& a,int s,int e){
-        int pivot = a[e];
-        int i = s-1;
-        for (int j = s;j<e;j++){
-            if(a[j]<=pivot){
-                i++;
-                swap(a[i],a[j]);
-            }
-        }
-        
-        swap(a[i+1],a[e]);
-        return i+1;
-    }  //standard partition functio 
-    
-    int findKthLargest(vector<int>& nums, int k) {
-        reverse(nums.begin(),nums.end());
-        int s = 0;
-        int e = nums.size()-1;
-        int c = (e + 1) - k;              //kth smallest here in n-k in reversed
-        while(1){
-            //rather than going recursive we went for while 1 ..bcz we did not had s and e as parameters in actual function findKthLargest
-            
-            //and only thing varying was also s and e
-            
-            int p = partition(nums,s,e);
-            cout<<p<<endl;
-            if(p==c){
-                return nums[p];
-            }
-            else if(c<p){
-                e = p - 1;
+    int findKthLargest(vector<int>& a, int k) {
+        priority_queue<int,vector<int>>p;
+        k=a.size()-k+1;
+        for(int i=0;i<a.size();i++){
+            if(p.size()<k){
+                p.push(a[i]);
             }
             else{
-                s = p + 1;
+                if(p.top()>a[i]){
+                    p.pop();
+                    p.push(a[i]);
+                }
             }
-            
-            
-            //rest all is standard and same
-            
         }
+        return p.top();
     }
 };
+//Time: In worst case, O(nlogk).
+
