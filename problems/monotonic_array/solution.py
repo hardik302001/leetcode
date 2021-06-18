83cd78b45f34ea87d314d0edcd43acec32c8d78b
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-06-18 11:15:51 +0000

    Sync LeetCode submission - Monotonic Array (python3)

--- problems/monotonic_array/solution.py
+++ problems/monotonic_array/solution.py
@@ -1,11 +1,11 @@
 class Solution(object):
     def isMonotonic(self, A):
-        if A[0] <= A[-1]:
-            A = A[::-1]
-        
-        # Expected A is decreasing order
-        for i in range(1, len(A)):
-            if A[i-1] < A[i]:
-                return False
-        
-        return True
+        increasing = decreasing = True
+
+        for i in range(len(A) - 1):
+            if A[i] > A[i+1]:
+                increasing = False
+            if A[i] < A[i+1]:
+                decreasing = False
+
+        return increasing or decreasing
