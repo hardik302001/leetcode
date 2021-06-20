2681550c6af35f1e9abc136d96ac653b59c7cd7b
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-06-20 07:05:22 +0000

    Sync LeetCode submission - Reverse String (python3)

---
+++ problems/reverse_string/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def reverseString(self, s):
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left, right = left + 1, right - 1
