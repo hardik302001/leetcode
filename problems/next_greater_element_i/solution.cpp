c84d7c07e864cbc6bd68406512573cb92354981a
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-06-22 03:55:55 +0000

    Sync LeetCode submission - Next Greater Element I (cpp)

--- problems/next_greater_element_i/solution.cpp
+++ problems/next_greater_element_i/solution.cpp
@@ -1,43 +1,36 @@
-//see previous solution
-
-
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        stack<int>s;
-        map<int,int>m;
-        
-        for(int i = 0;i<nums2.size();i++){
-            if(s.empty()){
-                s.push(nums2[i]);
-            }
-            else{
-                if(s.top()>=nums2[i]){
-                    s.push(nums2[i]);
-                }
-                else{
-                    while(!s.empty() and s.top()<nums2[i]){
-                        m.insert({s.top(),nums2[i]});
-                        s.pop();                      //we need to do it seperately bcz in cpp..s.pop( dont return anything...bcz its actually a stl) ..so we took the element...put it into map..and then popped it ...while in python stack is actually a list..so we get an element on stack.pop()
-                    }
-                    s.push(nums2[i]);
-                }
-            }
-        }
-        
-        vector<int>v;
-        
-        for(int i = 0;i<nums1.size();i++){
-            if(m.find(nums1[i])==m.end()){
-                v.push_back(-1);
-            }
-            else{
-                v.push_back(m[nums1[i]]);
-            }
-        }
-        
-        
-        return v;
-        
+    
+    map<int,int>m1; 
+    stack<int>st;
+     int n=nums2.size();
+    //reverse traversal...
+  for(int i=n-1;i>=0;i--){
+      
+      while(!st.empty() && st.top()<=nums2[i]){    //less than equal consider is must! in both nge-i and nge-ii       
+          st.pop();
+      }
+
+      if(st.empty()){
+          m1[nums2[i]]=-1;
+      }
+      
+      else{
+          m1[nums2[i]]=st.top();
+      }
+      
+      st.push(nums2[i]);
+
+      
+  }
+    vector<int>ans;
+    
+    for(auto i:nums1){
+        ans.push_back(m1[i]);
     }
+    
+    
+    return ans;
+}
 };
