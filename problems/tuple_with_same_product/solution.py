871716921e3f65a8c413963fe05b3d3f44f4d275
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-06-24 02:18:37 +0000

    Sync LeetCode submission - Tuple with Same Product (python3)

---
+++ problems/tuple_with_same_product/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def tupleSameProduct(self, nums: List[int]) -> int:
+        d = {}
+        n = len(nums)
+        for i in range(n):
+            for j in range(i+1,n):
+                p = nums[i]*nums[j]
+                if p in d:
+                    d[p]+=1
+                else:
+                    d[p] = 1
+        ans = 0
+        print(d)
+        for i in d:
+            ans = ans + d[i]*(d[i]-1)
+        
+        return ans*4
