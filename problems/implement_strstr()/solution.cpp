5f8f0593f06237c50d5ec3c7c761c66eaa02c6d0
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-06-24 03:18:14 +0000

    Sync LeetCode submission - Implement strStr() (cpp)

---
+++ problems/implement_strstr()/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int m = haystack.size(), n = needle.size();
+        for (int i = 0; i <= m - n; i++) {
+            int j;
+            for (j = 0; j < n; j++) {
+                if (haystack[i + j] != needle[j]) {
+                    break;
+                }
+            }
+            if (j == n) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+
+
+
+
+
+//this is sane as brute...refer kmp
