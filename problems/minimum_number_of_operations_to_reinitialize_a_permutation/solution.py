4c6fa65dbdc0953fe0c4932dc41f59eafb4de2d0
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-07-13 03:19:50 +0000

    Sync LeetCode submission - Minimum Number of Operations to Reinitialize a Permutation (python3)

---
+++ problems/minimum_number_of_operations_to_reinitialize_a_permutation/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reinitializePermutation(self, n: int) -> int:
+        ans = 0
+        perm = list(range(n))
+        while True: 
+            ans += 1
+            perm = [perm[n//2+(i-1)//2] if i&1 else perm[i//2] for i in range(n)]
+            if all(perm[i] == i for i in range(n)): return ans
