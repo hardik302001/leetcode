ef83253ec56371d8a39cf2baccb06e1c32f8370f
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-07-14 13:01:24 +0000

    Sync LeetCode submission - Remove Duplicates from Sorted Array (python3)

--- problems/remove_duplicates_from_sorted_array/solution.py
+++ problems/remove_duplicates_from_sorted_array/solution.py
@@ -1,17 +1,31 @@
 '''
-Since the array is already sorted, we can keep two pointers ii and jj, where ii is the slow-runner while j is the fast-runner. As long as nums[i] = nums[j]nums[i]=nums[j], we increment jj to skip the duplicate.
+Since the array is already sorted, we can keep two pointers ii and jj, where ii is the slow-runner while j is the fast-runner. As long as nums[i] = nums[j] , we increment jj to skip the duplicate.
 
-When we encounter nums[j] != nums[i]nums[j] , the duplicate run has ended so we must copy its value to nums[i + 1]nums[i+1]. ii is then incremented and we repeat the same process again until jj reaches the end of array.
+When we encounter nums[j] != nums[i] , the duplicate run has ended so we must copy its value to nums[i + 1]. ii is then incremented and we repeat the same process again until jj reaches the end of array.
 
 '''
 
 
-class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        count = 0
-        for i in range(1,len(nums)):
-            if nums[i] != nums[count]:
-                count +=1
-                nums[count] = nums[i]
+# class Solution:
+#     def removeDuplicates(self, nums: List[int]) -> int:
+#         count = 1
+#         for i in range(1,len(nums)):
+#             if nums[i] != nums[count-1]:
+#                 nums[count] = nums[i]
+#                 count+=1
                 
-        return count+1
+#         return count
+    
+
+#THIS CODE IS GENERALISED VERSION FOR ANY K, rather than atmost  one occurence or rather than atmost 2 occurence, for more info visit remove-duplicates-from-sorted-array-ii
+    
+class Solution:
+    def removeDuplicates(self, nums):
+        slow, fast = 1, 1
+
+        while fast < len(nums):
+            if nums[slow - 1] != nums[fast]:
+                nums[slow] = nums[fast]
+                slow += 1
+            fast += 1
+        return slow
