c679f3d575d757cf138853e873833266f57e7738
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-07-15 12:42:52 +0000

    Sync LeetCode submission - Power of Three (python3)

---
+++ problems/power_of_three/solution.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isPowerOfThree(self, n):
+        if n <= 0:
+        	return False
+
+        while n % 3 == 0:        	
+        	n = n / 3
+
+        return True if n == 1 else False
