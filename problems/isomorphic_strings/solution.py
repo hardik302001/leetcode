682ba9349a772e0389ca3b44518e4e447ca5ab43
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-07-15 15:43:37 +0000

    Sync LeetCode submission - Isomorphic Strings (python3)

--- problems/isomorphic_strings/solution.py
+++ problems/isomorphic_strings/solution.py
@@ -1,10 +1,18 @@
+#visit word pattern   question on leetcode
+
+
+
+
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         if len(set(s)) == len(set(t)):
             m = {}
             for i in range(len(s)):
                 if s[i] not in m:
-                    m[s[i]] = t[i]
+                    if t[i] not in m.values():    # abba cccc
+                        m[s[i]] = t[i]
+                    else:
+                        return False
                 else:
                     if t[i] != m[s[i]]:
                         return False
