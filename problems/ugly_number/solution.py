f665e2c737d8a2cf6b8a990cb3ffb12437ef96c0
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-07-15 16:00:05 +0000

    Sync LeetCode submission - Ugly Number (python3)

---
+++ problems/ugly_number/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isUgly(self, num: int) -> bool:
+        if num <= 0:
+            return False
+        for x in [2, 3, 5]:
+            while num % x == 0:
+                num = num / x
+        return num == 1
