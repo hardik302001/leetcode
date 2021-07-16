2d606c82bf08b316f283be0cc37d2fa38c0a0ce3
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-07-16 09:25:18 +0000

    Sync LeetCode submission - 4Sum (python3)

--- problems/4sum/solution.py
+++ problems/4sum/solution.py
@@ -1,25 +1,30 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        k=len(nums)
         nums.sort()
-
-        res = set()
-        for i in range(0,k):
-            for j in range(i+1,k):
-                start=j+1
-                end=k-1
-            
-                while start < end:
-                    cur = nums[i] + nums[j]
-                    cur = cur + nums[start] + nums[end]
-                    # print(cur)
-                    # print(res)
-                    if cur==target and (nums[i],nums[j],nums[start],nums[end]) not in res:  
-                        res.add((nums[i],nums[j],nums[start],nums[end]))
-                        start+=1
-                        end-=1
-                    elif cur > target:
-                        end-=1
+        res = []
+        n = len(nums)
+        for i in range(n):
+            for j in range(i+1,n):
+                cur = nums[i] + nums[j]
+                s = j+1
+                e = n-1
+                need = target - cur
+                while s<e:
+                    if nums[s] + nums[e]==need:
+                        x = []
+                        x.append(nums[i])
+                        x.append(nums[j])
+                        x.append(nums[s])
+                        x.append(nums[e])
+                        if x in res:
+                            pass
+                        else:
+                            res.append(x)
+                        s = s +1
+                        e = e -1
+                    elif nums[s] + nums[e]>need:
+                        e = e -1
                     else:
-                        start+=1
+                        s = s +1
         return res
+                
