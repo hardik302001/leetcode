7a1777ccbc58e506ab8002fc02ee528a526516c6
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-07-17 11:25:18 +0000

    Sync LeetCode submission - Valid Palindrome II (python3)

---
+++ problems/valid_palindrome_ii/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        i = 0
+        j = len(s)-1
+        while i < j:
+            if s[i] != s[j]:
+                delete_i = s[i+1:j+1]
+                delete_j = s[i:j]
+                return self._isPalindrome(delete_i) or self._isPalindrome(delete_j)
+            i += 1
+            j -= 1
+        return True
+    
+    def _isPalindrome(self, s):
+        return s == s[::-1]
