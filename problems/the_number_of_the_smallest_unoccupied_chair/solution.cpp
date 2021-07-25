b0fc1202d1a17a9a90902c688719296c5a1c316f
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-07-25 16:44:03 +0000

    Sync LeetCode submission - The Number of the Smallest Unoccupied Chair (cpp)

--- problems/the_number_of_the_smallest_unoccupied_chair/solution.cpp
+++ problems/the_number_of_the_smallest_unoccupied_chair/solution.cpp
@@ -1,54 +1,36 @@
+//Approach-1 (Naive O(n^2) approach that comes to mind first)
 class Solution {
 public:
-    int smallestChair(vector<vector<int>>& times, int tar) {
+    int smallestChair(vector<vector<int>>& times, int targetFriend) {
         int n = times.size();
-        int alpha = 0;
-        vector<pair<int, int> > st(n);
-        vector<pair<int, pair<int,int> > > vec;
-        priority_queue<int, vector<int> , greater<int>> p;
-        for(int i = 0;i<n;i++){
-            p.push(i);
-            alpha++;
-        }
         
-        int a = times[tar][0];
-        int b= times[tar][1];
-        for(int i = 0;i<n;i++){
-            if(times[i][0]<a){
-                vec.push_back(make_pair(times[i][0], make_pair(1, i)));
-            }
-            if(times[i][1]<=a){
-                vec.push_back(make_pair(times[i][1], make_pair(-1, i)));
-            }
-        }
+        vector<int> endTimes(n, -1); //at max we will have 0 to n-1 chairs
         
-        sort(vec.begin(),vec.end());
+        /*
+            We need to sort the times based on arrival time but we don't want to
+            loose the friend number after sorting. So, store the arrival time
+            of targetFriend because it's given in the question that 
+            Each arrival time is distinct.
+        */
         
-        int sze = vec.size();
-        vector<int>res(sze);
-        for(int i = 0;i<sze;i++){
-            if(vec[i].second.first<=0){
-                st[vec[i].second.second].second = i;
-            }
-            else{
-                st[vec[i].second.second].first = i;
+        int targetArrivalTime = times[targetFriend][0];
+        
+        sort(begin(times), end(times));
+        
+        for(vector<int> &time : times) {
+            int arrival = time[0];
+            int depart  = time[1];
+            //Find the first unoccupied chair
+            for(int i = 0; i<n; i++) {
+                if(endTimes[i] <= arrival) {
+                    endTimes[i] = depart; //update with current friend depart time
+                    
+                    if(arrival == targetArrivalTime)
+                        return i;
+                    break;
+                }
             }
         }
-        
-        for(int i = 0;i<sze;i++){
-            if(vec[i].second.first>0){
-                int e = p.top();
-                res[i] = e;
-                p.pop();
-            }
-            else{
-                int ind = st[vec[i].second.second].first;
-                int l = res[ind];
-                p.push(l);
-            }
-        }
-        
-        return p.top();
-        
+        return -1;
     }
 };
