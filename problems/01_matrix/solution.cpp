2aaa1d08cb1fe3c85d954e50dfb1b025d717a5f8
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-07-29 14:26:37 +0000

    Sync LeetCode submission - 01 Matrix (cpp)

---
+++ problems/01_matrix/solution.cpp
@@ -0,0 +1,66 @@
+//quite similar as 
+
+//https://leetcode.com/problems/nearest-exit-from-entrance-in-maze/
+
+
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int M = mat.size();
+        int N = mat[0].size();
+        queue<pair<int, int>> todo;
+        vector<int> dx = {-1, 0,1, 0};
+        vector<int>dy = {0,1,0,-1};
+        
+        vector<vector<int>> dis(M,vector<int>(N,-1));
+        for(int i=0;i<M;i++)
+            for(int j=0;j<N;j++)
+            {
+                if(mat[i][j]==0)
+                { 
+                    todo.push({i,j});
+                    dis[i][j]=0;
+                }
+            }
+        
+        while (!todo.empty()) {
+            int curx = todo.front().first;
+            int cury = todo.front().second;
+            
+            todo.pop();
+            
+            for(int i = 0;i<4;i++){
+                int newx = curx + dx[i];
+                int newy = cury + dy[i];
+                
+                if(inside(newx , newy , M , N , dis) and dis[newx][newy]==-1){
+                    dis[newx][newy] = dis[curx][cury] + 1;
+                    todo.push({newx, newy});
+                    
+                    
+                }
+                
+            }   
+            
+        }
+        
+        
+        return dis;
+            
+    }
+
+
+
+
+
+     bool inside(int x, int y, int &M, int &N, vector<vector<int> > &dis) {
+        if ((x < 0 || x >= M || y < 0 || y >= N )) {
+            return false;
+        }
+        return true;
+    }
+
+};
+
+   
+   
