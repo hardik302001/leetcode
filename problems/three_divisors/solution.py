70d8db5fd006c5b6215a3f4c47176f16383e3d96
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-08-01 04:08:51 +0000

    Sync LeetCode submission - Three Divisors (python3)

---
+++ problems/three_divisors/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+
+    
+    def isThree(self, n: int) -> bool:
+        def prime(n):
+            if n==2 or n==3: return True
+            if n%2==0 or n<2: return False
+            for i in range(3, int(n**0.5)+1, 2):   # only odd numbers
+                if n%i==0:
+                    return False    
+
+            return True
+        p = int(n**0.5)
+        return p*p==n and prime(p)
