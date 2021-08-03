ec81b8599239b5a1f6ab26f5c8b6641a5720603d
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-08-03 11:48:52 +0000

    Sync LeetCode submission - Subsets II (cpp)

---
+++ problems/subsets_ii/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+	vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> ans;
+        set<vector<int>>s;
+        int n = nums.size();
+        int maxsize = 1<<n;
+
+        for(int subsetmask=0;subsetmask<maxsize;subsetmask++){
+            vector<int> temp;
+            for(int j=0;j<n;j++){
+               if((subsetmask&(1<<j))){
+                   temp.push_back(nums[j]);
+               }
+            }
+            sort(temp.begin(),temp.end());
+            s.insert(temp);                            //to avoid repetition, bcz we need unique.. see tc [ 1,2,2 ]
+       }
+       
+       for(auto x:s){
+            ans.push_back(x);
+       }
+        
+       return ans;      
+    }
+};
