0abcecdb96d6147d59ce8143d86306a03732fa5e
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-08-09 07:40:59 +0000

    Sync LeetCode submission - Add Strings (cpp)

--- problems/add_strings/solution.cpp
+++ problems/add_strings/solution.cpp
@@ -2,32 +2,26 @@ class Solution {
 public:
     string addStrings(string num1, string num2) {
         int carry = 0;
-        string res = "";
+        string ans = "";
         while(num1.size() or num2.size() or carry){
-            int tot = carry;
-            if(num1.length()>0){
-                char ch = (num1[num1.length()-1]);
-                //cout<<"cha "<<ch<<" "<<endl;
-                int a = ch - '0';
-                tot = tot + a;
-               // cout<<"a "<<a<<endl;
+            int temp = 0;
+            if(num1.size()){
+                temp += (num1[num1.size()-1]-'0');
                 num1.pop_back();
             }
-            if(num2.length()>0){
-                char ch = (num2[num2.length()-1]);
-             //   cout<<"chb "<<ch <<" "<<endl;
-                int b = ch - '0';
-                tot = tot + b;
-           //     cout<<" b "<<b<<endl;
+            if(num2.size()){
+                temp += (num2[num2.size()-1]-'0');
                 num2.pop_back();
             }
-            //cout<<tot<<" ";
-            carry = tot/10;
-            int last = tot%10;
             
-            res = res + to_string(last);
+            temp += carry;
+            carry = temp/10;
+            cout<<temp<<" ";
+            ans = to_string(temp%10) + ans;
         }
-        reverse(res.begin(),res.end());
-        return res;
+    
+        return ans;
     }
+    
+    
 };
