01264708c63265a48b7816421d2f8b6e5a339139
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-08-09 14:57:21 +0000

    Sync LeetCode submission - Permutations (cpp)

---
+++ problems/permutations/solution.cpp
@@ -0,0 +1,28 @@
+//total n! permutuations
+//see prev submission
+
+
+class Solution {
+public:
+   // method 1: standard backtracing solution
+    vector<vector<int> > permute(vector<int> &num) {
+        vector<vector<int> > ans;
+        permutation(num, 0, ans);
+        return ans;
+    }
+
+    void permutation(vector<int>& num, int begin, vector<vector<int> > &ans) {        
+        //now we ar etaking nums vector by value, so we need to backtrack it
+        if (begin >= num.size()) {
+            ans.push_back(num);
+            return;
+        }
+
+        // every number chosen to be the begin once
+        for (int i = begin; i < num.size(); ++i) {
+            swap(num[begin], num[i]);
+            permutation(num, begin+1, ans);
+            swap(num[begin], num[i]);            //backtracking step
+        }
+    }
+};
