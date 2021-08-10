5624b9616a8446fa57fc95de2702916cccec4437
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-08-10 05:00:38 +0000

    Sync LeetCode submission - Truncate Sentence (cpp)

---
+++ problems/truncate_sentence/solution.cpp
@@ -0,0 +1,20 @@
+class Solution 
+{
+public:
+    string truncateSentence(string s, int k) 
+    {
+        string resStr = "";
+        int words = 0;
+        
+        for ( char i : s ) {
+            if ( i == ' ') 
+                words++;
+            if ( words < k ) 
+                resStr += i;
+            else
+                break;
+        }
+        
+        return resStr;
+    }
+};
