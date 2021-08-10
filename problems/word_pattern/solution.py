1e7fdf4e24dee2ca350ba39074219bb4afe1f790
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-08-10 17:45:25 +0000

    Sync LeetCode submission - Word Pattern (python3)

--- problems/word_pattern/solution.py
+++ problems/word_pattern/solution.py
@@ -1,22 +1,27 @@
 #same as isomorphic strings
+#Points to be noted:
+#compare length, not length of set
+#we need bijection, so we need to comapre in d.values also!!
+#this is the final code, dont look at previous code!!
+#same code goes for ISOMORPHIC STRINGS
 
 
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         s = list(s.split())
-        if len(s)!=len(pattern):
-            return False
-        
-        d = {}
-        
-        for i in range(len(s)):
-            if pattern[i] not in d:
-                if s[i] not in d.values():
-                    d[pattern[i]] = s[i]
+        if len(s)==len(pattern):
+            d = {}
+            for i in range(len(pattern)):
+                if pattern[i] not in d:
+                    if s[i] not in d.values():      #bijection
+                        d[pattern[i]] = s[i]
+                    else:
+                        return False
                 else:
-                    return False
-            else:
-                if s[i]!= d[pattern[i]]:
-                    return False
-        return True
+                    if s[i]!= d[pattern[i]]:
+                        return False
+            return True
+        
+        
+        return False   #for different length
                 
