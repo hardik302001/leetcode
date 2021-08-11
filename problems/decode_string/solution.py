7f8cc9667c6ff6d2714be8517ede74f1a7a65253
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-08-11 06:26:58 +0000

    Sync LeetCode submission - Decode String (python3)

--- problems/decode_string/solution.py
+++ problems/decode_string/solution.py
@@ -1,18 +1,24 @@
+#very ekeegent code!
+#take all four example to understand thecode!
+
+
 class Solution(object):
     def decodeString(self, s):
         stack = []; curNum = 0; curString = ''
         for c in s:
             if c == '[':
-                stack.append(curString)
+                stack.append(curString)   #keep prev string in stack
                 stack.append(curNum)
-                curString = ''
+                curString = ''             #now inirialize a new string, we will put all char between [...] in this string
                 curNum = 0
+           
             elif c == ']':
-                num = stack.pop()
-                prevString = stack.pop()
-                curString = prevString + num*curString
+                num = stack.pop()   #previous added element was the num in stack
+                prevString = stack.pop()  #before that we had the prev result string in stack
+                curString = prevString + num*curString  #we append this new string to previous string now..and thi is our new string! 
             elif c.isdigit():
                 curNum = curNum*10 + int(c)
             else:
                 curString += c
         return curString
+    
