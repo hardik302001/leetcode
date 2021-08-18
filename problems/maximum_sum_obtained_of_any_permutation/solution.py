2d6f5d869d5bc457a7241c33b4b1ba3efae3ed00
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-08-18 13:09:57 +0000

    Sync LeetCode submission - Maximum Sum Obtained of Any Permutation (python3)

---
+++ problems/maximum_sum_obtained_of_any_permutation/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxSumRangeQuery(self, nums: List[int], requests: List[List[int]]) -> int:
+        overlap = [0]*len(nums) + [0] # record the frequency for each index 
+        cum = 0
+        for start, end in requests: # traverse the interval will be time-consuming
+            overlap[start]+=1
+            overlap[end+1]-=1
+        for i in range(len(overlap)): # we do it only once 
+            cum += overlap[i]
+            overlap[i] = cum
+        nums.sort() # sort the index and nums 
+        overlap.sort()
+        ans = sum([nums[i]*overlap[i+1] for i in range(len(nums))]) # multiply by index
+        return ans%(10**9+7)
