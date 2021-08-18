2d0241a5f80d41d25412e6fa7d8e7974272b13cc
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-08-18 08:14:24 +0000

    Sync LeetCode submission - Number of Islands (cpp)

--- problems/number_of_islands/solution.cpp
+++ problems/number_of_islands/solution.cpp
@@ -1,31 +1,70 @@
 class Solution {
 public:
     
-    void dfs(vector<vector<char>>& grid, int x, int y) {
-        int m = grid.size(), n = grid[0].size();
-        grid[x][y] = '0';
-    if(x > 0 && grid[x - 1][y] == '1')
-    dfs(grid, x - 1, y);
-    if(x < grid.size() - 1 && grid[x + 1][y] == '1')
-    dfs(grid, x + 1, y);
-    if(y > 0 && grid[x][y - 1] == '1')
-    dfs(grid, x, y - 1);
-    if(y < grid[0].size() - 1 && grid[x][y + 1] == '1')
-    dfs(grid, x, y + 1);
-    }
+  
     
-    int numIslands(vector<vector<char>>& grid) {
-        int m = grid.size(), n = grid[0].size();
+    int numIslands(vector<vector<char>>& mat) {
         int ans = 0;
-        for(int i = 0; i < m ; i++) {
-            for(int j = 0; j < n; j++) {
-                if(grid[i][j] == '1') {
-                    dfs(grid, i, j);
-                    ans++;
-                }
+        int M = mat.size();
+        int N = mat[0].size();
+        queue<pair<int, int>> todo;
+        vector<int> dx = {-1, 0,1, 0};
+        vector<int>dy = {0,1,0,-1};
+        
+        vector<vector<int>> vis(M,vector<int>(N,-1));
+        for(int i=0;i<M;i++)
+            for(int j=0;j<N;j++)
+            {   
+                    if(mat[i][j]=='1')
+                    { 
+                        todo.push({i,j});
+                        mat[i][j] ='0';
+                        ans++;
+                        bfs(M,N, todo, mat, dx, dy);
+                        
+                    }
             }
-        }
+        
+        
+        
         return ans;
     }
+    
+    
+    void bfs(int M , int N , queue<pair<int, int>> &todo, vector<vector<char> > &mat, vector<int>dx, vector<int> dy){
+        while (!todo.empty()) {
+            int curx = todo.front().first;
+            int cury = todo.front().second;
+            todo.pop();
+            
+            for(int i = 0;i<4;i++){
+                int newx = curx + dx[i];
+                int newy = cury + dy[i];
+                
+                if(inside(newx , newy , M , N , mat) and mat[newx][newy]=='1'){
+                    todo.push({newx, newy});
+                    mat[newx][newy] = '0';
+                    
+                }
+                
+            }   
+            
+        }
+        
+    
+    }
+
+
+
+
+
+     bool inside(int x, int y, int &M, int &N, vector<vector<char> > &mat) {
+        if ((x < 0 || x >= M || y < 0 || y >= N )) {
+            return false;
+        }
+        return true;
+    }
+    
+
     
 };
