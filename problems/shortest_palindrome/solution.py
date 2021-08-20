5a8d8ddc51819f4f27c5bd2fff03a7e59e87e89a
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-08-20 10:29:27 +0000

    Sync LeetCode submission - Shortest Palindrome (python3)

---
+++ problems/shortest_palindrome/solution.py
@@ -0,0 +1,16 @@
+'''
+we are finding longest common substring from the string !
+by string matching here so O(n2)
+
+we will optimise it in coming solutions!
+
+'''
+
+class Solution:
+    def shortestPalindrome(self, s: str) -> str:
+        n = len(s)
+        
+        for i in range(n,0,-1):
+            if s[:i]==s[:i][::-1]:
+                return s[i:n][::-1] + s
+        return s
