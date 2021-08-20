fa782d39f75ef5cbf762d203f0a43208e19a3073
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-08-20 10:12:29 +0000

    Sync LeetCode submission - Implement strStr() (cpp)

--- problems/implement_strstr()/solution.cpp
+++ problems/implement_strstr()/solution.cpp
@@ -1,24 +1,34 @@
 class Solution {
 public:
+    
+    vector<int> prefix_function(string s) {
+        int n = (int)s.length();
+        vector<int> pi(n);
+        for (int i = 1; i < n; i++) {
+            int j = pi[i-1];
+            while (j > 0 && s[i] != s[j])
+                j = pi[j-1];
+            if (s[i] == s[j])
+                j++;
+            pi[i] = j;
+        }
+        return pi;
+    }
+    
+    
     int strStr(string haystack, string needle) {
-        int m = haystack.size(), n = needle.size();
-        for (int i = 0; i <= m - n; i++) {
-            int j;
-            for (j = 0; j < n; j++) {
-                if (haystack[i + j] != needle[j]) {
-                    break;
-                }
-            }
-            if (j == n) {
-                return i;
+        vector<int> pi = prefix_function(needle + "#" + haystack);
+        int n = haystack.size();
+        int m = needle.size();
+        int res = -1;
+        
+        for(int i = 0; i<(m+n+1);i++){
+            if(pi[i]==m){
+                res = i-2*m;    //visit https://cp-algorithms.com/string/prefix-function.html for more info
+                break;
             }
         }
-        return -1;
+        
+        return res;
     }
 };
-
-
-
-
-
-//this is sane as brute...refer kmp
