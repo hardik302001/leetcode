c88f94c838a1459390150f360ea8ec182820e583
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-08-27 11:37:51 +0000

    Sync LeetCode submission - Move Zeroes (cpp)

--- problems/move_zeroes/solution.cpp
+++ problems/move_zeroes/solution.cpp
@@ -1,10 +1,23 @@
 class Solution {
 public:
-void moveZeroes(vector<int>& nums) {
-    for (int lastNonZeroFoundAt = 0, cur = 0; cur < nums.size(); cur++) {
-        if (nums[cur] != 0) {
-            swap(nums[lastNonZeroFoundAt++], nums[cur]);
+    //move zero at end
+    
+    void moveZeroes(vector<int>& nums) {
+        for (int lastNonZeroFoundAt = 0, cur = 0; cur < nums.size(); cur++) {
+            if (nums[cur] != 0) {
+                swap(nums[lastNonZeroFoundAt++], nums[cur]);
+            }
         }
     }
-}
+
+    //zeroat begin
+    
+    void moveZeroesatBEGIN(vector<int>& nums) {
+        int n = nums.size();
+        for (int lastNonZeroFoundAt = n-1, cur = n-1; cur >= 0; cur--) {
+            if (nums[cur] != 0) {
+                swap(nums[lastNonZeroFoundAt--], nums[cur]);
+            }  
+        }
+    }
 };
