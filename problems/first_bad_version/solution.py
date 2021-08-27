a78913a9b9d48b7dc562fab8cbe3b5f1d6c66582
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-08-27 06:57:54 +0000

    Sync LeetCode submission - First Bad Version (python3)

---
+++ problems/first_bad_version/solution.py
@@ -0,0 +1,18 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return an integer
+# def isBadVersion(version):
+
+class Solution:
+    def firstBadVersion(self, n):
+        l = 0
+        r = n
+        
+        while l<r:
+            m = l+(r-l)//2
+            if not isBadVersion(m):
+                l = m+1
+            else:
+                r= m  #bcz we need first occurence if bad word
+                
+        return l
