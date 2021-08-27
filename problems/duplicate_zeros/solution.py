f8a6fbc19991aa274b892dffa397716397a06106
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-08-27 11:23:42 +0000

    Sync LeetCode submission - Duplicate Zeros (python3)

---
+++ problems/duplicate_zeros/solution.py
@@ -0,0 +1,14 @@
+
+#Start from the back and adjust items to correct locations. If item is zero then duplicate it.
+
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        zeroes = arr.count(0)
+        n = len(arr)
+        for i in range(n-1, -1, -1):
+            if i + zeroes < n:
+                arr[i + zeroes] = arr[i]
+            if arr[i] == 0: 
+                zeroes -= 1
+                if i + zeroes < n:
+                    arr[i + zeroes] = 0
