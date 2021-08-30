dfa9b6d473938c37206c76155afa0f232aae3844
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-08-30 11:07:27 +0000

    Sync LeetCode submission - Range Addition II (python3)

--- problems/range_addition_ii/solution.py
+++ problems/range_addition_ii/solution.py
@@ -1,12 +1,8 @@
 class Solution:
     def maxCount(self, m: int, n: int, ops: List[List[int]]) -> int:
-        r_min=m
-        c_min=n
+        a = m
+        b = n
         for i in ops:
-            if i[0]<r_min:
-                r_min=i[0]
-            if i[1]<c_min:
-                c_min=i[1]
-        return r_min*c_min
-    
-#we need to consider the most common area
+            a = min(a, i[0])
+            b = min(b, i[1])
+        return a*b
