d35546de32ac5c236fd6f915c55f174ae4d2d344
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-08-31 04:56:02 +0000

    Sync LeetCode submission - Flood Fill (cpp)

--- problems/flood_fill/solution.cpp
+++ problems/flood_fill/solution.cpp
@@ -1,27 +1,51 @@
 class Solution 
 {
 public:
-    void helper(vector<vector<int>>&image,vector<vector<bool>>&vis,int sr,int sc,int newColor,int prev)
+    vector<vector<int>> floodFill(vector<vector<int>>& mat, int sr, int sc, int newColor) 
     {
-        if(sr<0 || sc<0 || sr>=image.size() || sc>=image[0].size() || image[sr][sc]!=prev || vis[sr][sc])
-        {
-            return;
+        int M = mat.size();
+        int N = mat[0].size();
+        int old = mat[sr][sc];
+        if (old==newColor){    //else it will lead to tle..or keep a vis array!
+            return mat;
         }
-        vis[sr][sc]=true;
-        image[sr][sc]=newColor;
-        helper(image,vis,sr+1,sc,newColor,prev);
-        helper(image,vis,sr,sc+1,newColor,prev);
-        helper(image,vis,sr-1,sc,newColor,prev);
-        helper(image,vis,sr,sc-1,newColor,prev);
+        
+        
+        queue<pair<int, int>> todo;
+        vector<int> dx = {-1, 0,1, 0};
+        vector<int> dy = {0,1,0,-1};
+        
+        todo.push({sr,sc});
+        
+        
+        while (!todo.empty()) {
+            int curx = todo.front().first;
+            int cury = todo.front().second;
+            mat[curx][cury] = newColor;
+            todo.pop();
+            
+            for(int i = 0;i<4;i++){
+                int newx = curx + dx[i];
+                int newy = cury + dy[i];
+                
+                if(inside(newx , newy , M , N , mat) and mat[newx][newy]==old){
+                    todo.push({newx, newy});
+                    
+                }
+                
+            }   
+            
+        }
+        
+        
+        return mat;
+            
     }
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) 
-    {
-        int n=image.size(),m=image[0].size();
-        vector<vector<bool>>vis(n,vector<bool>(m,false)); //use vis for backup, to avoid rte and inf looping
-        if(newColor==image[sr][sc]){
-            return image;
+
+     bool inside(int x, int y, int &M, int &N, vector<vector<int> > &mat) {
+        if ((x < 0 || x >= M || y < 0 || y >= N )) {
+            return false;
         }
-        helper(image,vis,sr,sc,newColor,image[sr][sc]);
-        return image;
+        return true;
     }
 };
