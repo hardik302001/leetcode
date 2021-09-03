810a666be5414e944e61b26c6de8e8c46f46559e
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-09-03 04:53:18 +0000

    Sync LeetCode submission - Maximum Subarray (python3)

--- problems/maximum_subarray/solution.py
+++ problems/maximum_subarray/solution.py
@@ -1,18 +1,7 @@
 class Solution:
-    def maxSubArray(self, a: List[int]) -> int:
-        size = len(a)
-        max_so_far = a[0]
-        max_ending_here = 0
-     
-        for i in range(0, size):
-            max_ending_here = max_ending_here + a[i]
-            if max_ending_here < 0:
-                max_ending_here = 0
-         
-            # Do not compare for all elements. Compare only     
-            # when  max_ending_here > 0
-            elif (max_so_far < max_ending_here):
-                max_so_far = max_ending_here
-        if max_so_far <0:
-            return max(a)
-        return max_so_far
+    def maxSubArray(self, nums: List[int]) -> int:
+        n = len(nums)
+        
+        for i in range(1,len(nums)):
+            nums[i] = max(nums[i], nums[i-1] + nums[i])
+        return max(nums)
