916f3ae9e9f0b291773624d2f186acfc74f10885
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-09-03 10:22:11 +0000

    Sync LeetCode submission - Minimum Difference Between Largest and Smallest Value in Three Moves (python3)

---
+++ problems/minimum_difference_between_largest_and_smallest_value_in_three_moves/solution.py
@@ -0,0 +1,15 @@
+# If the size <= 4, then just delete all the elements and either no or one element would be left giving the answer 0.
+# else sort the array
+# there are 4 possibilities now:-
+
+# delete 3 elements from left and none from right
+# delete 2 elements from left and one from right
+# and so on.. now just print the minima.
+
+
+class Solution:
+    def minDifference(self, nums: List[int]) -> int:
+        
+        if len(nums) <= 4: return 0
+        nums.sort()
+        return min(nums[-1] - nums[3], nums[-2] - nums[2], nums[-3] - nums[1], nums[-4] - nums[0])
