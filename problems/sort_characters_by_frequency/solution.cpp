779c62f6233890359e81a4ec0f9f8e7c40f15f6d
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-09-07 14:53:05 +0000

    Sync LeetCode submission - Sort Characters By Frequency (cpp)

--- problems/sort_characters_by_frequency/solution.cpp
+++ problems/sort_characters_by_frequency/solution.cpp
@@ -1,36 +1,35 @@
 
 class Solution {
 public:
-
-static bool comp(pair<char,int>&a, pair<char,int>&b)
-{
-    if(a.first < b.first){
-        
-        return true;
-    }
-    
-    if(a.first == b.first){
-        
+    static bool cmp(pair<char,int> a, pair<char,int> b){
         if(a.second > b.second){
-            
             return true;
         }
+        else if(a.second<b.second){
+            return false;
+        }
+        else{
+            if(a.first>b.first){
+                return true;
+            }
+            else{
+                return false;
+            }
+            
+        }
     }
-   return false;
-}
-string frequencySort(string s) {
-    
-    map<char,int> m;
-    
-    for(int i=0; i<s.size( ); i++){
-        
-        m[s[i]]++;
+    string frequencySort(string s) {
+        map<char,int> m;
+        for(char c: s) m[c]++;
+        vector<pair<char,int>> v;
+        for(auto x: m) v.push_back({x.first,x.second});
+        sort(v.begin(),v.end(),cmp);
+        s="";
+        for(auto x: v){
+            while(x.second--){
+                s+=x.first;    
+            }
+        }
+        return s;
     }
-     sort(s.begin(), s.end(), [&](char a, int b) { 
-            return m[a] > m[b] || (m[a] == m[b] && a < b); 
-        });
-   return s;
-    
-    
-}
 };
