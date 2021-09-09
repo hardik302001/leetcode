b1147b3b393152f0cf8d8e50d07ebe2ead1e693a
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-09-09 05:07:44 +0000

    Sync LeetCode submission - Count Number of Nice Subarrays (python3)

---
+++ problems/count_number_of_nice_subarrays/solution.py
@@ -0,0 +1,39 @@
+#problem is same as subarray sum equals k
+# https://leetcode.com/problems/subarray-sum-equals-k/
+
+#just convert nums[i] = 0, if even 
+#else
+#nums[i] = 1
+
+
+#now ques is exactly same as suabrray sum k (where k is odd count)
+
+class Solution:
+    def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+        
+        n = len(nums)
+        for i in range(n):
+            nums[i] = nums[i]%2
+        
+        d = {}
+        d[0] = 1
+        s = 0
+        count = 0
+        for i in range(len(nums)):
+            s += nums[i]
+            if s-k in d: # --- I
+                count += d[s-k]
+               # or return True
+               # or return indicies
+
+               # add sum to frq dict
+            if s in d:
+                d[s] += 1 # --- II
+            else:
+                d[s] = 1
+        return count
+
+        
+        
+        
+        
