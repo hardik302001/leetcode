79a61f96785bb7b763830e6a95348f35ae98a67c
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-09-22 15:44:20 +0000

    Sync LeetCode submission - Distribute Candies to People (python3)

---
+++ problems/distribute_candies_to_people/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def distributeCandies(self, candies, n):
+        res = [0] * n
+        i = 0
+        while candies > 0:
+            res[i % n] += min(candies, i + 1)
+            candies -= i + 1
+            i += 1
+        return res
