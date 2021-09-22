b9ad95531da4923a63f63c0a3ec64bdedb9d35b4
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-09-22 08:43:01 +0000

    Sync LeetCode submission - Split a String Into the Max Number of Unique Substrings (cpp)

---
+++ problems/split_a_string_into_the_max_number_of_unique_substrings/solution.cpp
@@ -0,0 +1,28 @@
+// similar to https://leetcode.com/problems/maximum-length-of-a-concatenated-string-with-unique-characters
+
+
+
+class Solution {
+public:
+    int max_unique_substrings(string s,unordered_set<string>&seen)
+    {
+        int maximum = 0;
+        for(int i = 1;i<s.length() + 1;i++)
+        {
+			//simply take current substring as candidate and put it in seen if being seen for the 1st time
+            string candidate = s.substr(0,i);
+            if(seen.count(candidate) == 0)
+            {
+                seen.insert(candidate);
+				//recur for the remaining substring
+                maximum = max(maximum, 1 + max_unique_substrings(s.substr(i),seen));
+                seen.erase(candidate);//backtracking
+            }
+        }
+        return maximum;
+    }
+    int maxUniqueSplit(string s) {
+        unordered_set<string> seen;
+        return max_unique_substrings(s,seen);
+    }
+};
