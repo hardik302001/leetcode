7828d19cf7fed80fd94ce2b7abdbbb2edd73b20c
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-09-27 11:11:26 +0000

    Sync LeetCode submission - Maximum Subarray (python3)

--- problems/maximum_subarray/solution.py
+++ problems/maximum_subarray/solution.py
@@ -1,7 +1,17 @@
+#courtesy codencode kadane algo
+
+
+
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         n = len(nums)
         
+#         for i in range(1,len(nums)):
+#             nums[i] = max(nums[i], nums[i-1] + nums[i])
+#         return max(nums)
+    
+    
+        
         for i in range(1,len(nums)):
-            nums[i] = max(nums[i], nums[i-1] + nums[i])
+            nums[i] = max(nums[i-1], 0) + nums[i]  #to consider a subarray  ending at index i, we need to add nums[i], what extra we have is nums[i-1]( that is a bonus), if it is negative we dont consider it, else we consider it
         return max(nums)
