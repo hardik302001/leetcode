93a4dfe54634710b064715ecfeb723b200b7f537
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-09-28 11:37:49 +0000

    Sync LeetCode submission - Sort Array By Parity (python3)

--- problems/sort_array_by_parity/solution.py
+++ problems/sort_array_by_parity/solution.py
@@ -1,17 +1,14 @@
-class Solution:
-    def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        e = []
-        o =[]
-        #O(n) time
-        #O(n) space
-        
-        
-        #for O(1) space implement using quicksort
-        
-        
-        for i in nums:
-            if i%2:
-                o.append(i)
-            else:
-                e.append(i)
-        return e+o
+#also see sort by parity 2
+
+
+class Solution(object):
+    def sortArrayByParity(self, A):
+        i, j = 0, len(A) - 1
+        while i < j:
+            if A[i] % 2 > A[j] % 2:        # if oddd in beginning, and even towards end, then swap
+                A[i], A[j] = A[j], A[i]
+
+            if A[i] % 2 == 0: i += 1
+            if A[j] % 2 == 1: j -= 1
+
+        return A
