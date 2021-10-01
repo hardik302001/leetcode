124fbd0ae890b64e2304283f01bb66bec69e1280
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-10-01 10:42:14 +0000

    Sync LeetCode submission - Minimum ASCII Delete Sum for Two Strings (cpp)

--- problems/minimum_ascii_delete_sum_for_two_strings/solution.cpp
+++ problems/minimum_ascii_delete_sum_for_two_strings/solution.cpp
@@ -1,23 +1,22 @@
-//same as lcs
+//same as lcs https://leetcode.com/problems/longest-common-subsequence/
 
 
 class Solution {
 public:
     int minimumDeleteSum(string s1, string s2) {
-        int dp[1001][1001] = {0};
+        vector< vector< int> > dp(1001, vector<int>(1001,0));   // (0 to m (m+1)), (0 to n (n+1))
         //sum of ascii of both strings
         int n1 = accumulate(s1.begin(), s1.end(), 0);
         int n2 = accumulate(s2.begin(), s2.end(), 0);
         
-        
-        for ( int i = 0; i < s1.size(); ++i ) {
-            for ( int j = 0; j < s2.size(); ++j ) {
-                if ( s1[i] == s2[j] ) {
-                    dp[i+1][j+1] = dp[i][j] + int(s1[i]) * 2;       //bcz we need to subtract this from (n1 + n2), so lcs is common in both strings, so twice will be removed
+        for ( int i = 1; i <= s1.size(); ++i ) {
+            for ( int j = 1; j <= s2.size(); ++j ) {
+                if ( s1[i-1] == s2[j-1] ) {
+                    dp[i][j] = dp[i-1][j-1] + 2 * int(s1[i-1]);    //
                     
                     //here we dont need length, but we need the ascii sum 
                 } else {
-                    dp[i+1][j+1] = max(dp[i+1][j], dp[i][j+1]);
+                    dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
                 }
             }
         }
@@ -25,6 +24,6 @@ public:
         // dp[m][n] gives sum of lcs 
         
 
-        return n1 + n2 - dp[s1.size()][s2.size()];
+        return n1 + n2 - dp[s1.size()][s2.size()];  
     }
 };
