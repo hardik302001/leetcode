5a691c1411983b98750c53f8fcdc939aa714d4be
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-10-02 07:12:15 +0000

    Sync LeetCode submission - Longest Common Subsequence (cpp)

--- problems/longest_common_subsequence/solution.cpp
+++ problems/longest_common_subsequence/solution.cpp
@@ -11,7 +11,7 @@ public:
         for(int i=0;i<=m;i++)dp[0][i]=0;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(x[i-1]==y[j-1])dp[i][j]=1+dp[i-1][j-1];
+                if(x[i-1]==y[j-1]) dp[i][j]=1+dp[i-1][j-1];
                 else {
                     dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
                 }
