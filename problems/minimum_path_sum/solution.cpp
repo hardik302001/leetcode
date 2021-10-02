20c3b39de0fcd7e365cac47e66209015d0cf6c78
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-10-02 06:19:45 +0000

    Sync LeetCode submission - Minimum Path Sum (cpp)

--- problems/minimum_path_sum/solution.cpp
+++ problems/minimum_path_sum/solution.cpp
@@ -1,30 +1,29 @@
 //courtesy : codencode yt dp series, logic, the code is written by me.
+//see prev code(first one) also.
 
+//also see DUNGEOUN GAME :https://leetcode.com/problems/dungeon-game/submissions/
 
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int> > dp(m, vector<int> (n,0));
-        dp[m-1][n-1] = grid[m-1][n-1];
-        for(int j = n-2;j>=0;j--){
-            dp[m-1][j] = dp[m-1][j+1] + grid[m-1][j];
-        }
+        vector<vector<int> > dp(m+1, vector<int> (n+1,INT_MAX));
+    
+    
+        dp[m][n-1] = 0;
+        dp[m-1][n] = 0;
         
-        for(int i = m-2;i>=0;i--){
-            dp[i][n-1] = dp[i+1][n-1] + grid[i][n-1];
-        }
+
         
-        for(int i = m-2;i>=0;i--){
-            for(int j = n-2;j>=0;j--){
+        for(int i = m-1;i>=0;i--){
+            for(int j = n-1;j>=0;j--){
                 dp[i][j] = grid[i][j] + min(dp[i+1][j],dp[i][j+1]);
             }
         }
         
         
         return dp[0][0];
-
         
     }
 };
