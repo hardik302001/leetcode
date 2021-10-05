d3c31adf5bafc99a5ef2823fe503e26563345dd0
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-10-05 03:18:11 +0000

    Sync LeetCode submission - Shortest Palindrome (python3)

--- problems/shortest_palindrome/solution.py
+++ problems/shortest_palindrome/solution.py
@@ -9,8 +9,8 @@ we will optimise it in coming solutions!
 class Solution:
     def shortestPalindrome(self, s: str) -> str:
         n = len(s)
-        
+        if s=="":return s
         for i in range(n,0,-1):
             if s[:i]==s[:i][::-1]:
                 return s[i:n][::-1] + s
-        return s
+        # return s
