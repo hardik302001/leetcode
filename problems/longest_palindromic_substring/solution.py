dcad0318882b4a003f78778ee5db68d76e738e9f
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-10-05 03:28:43 +0000

    Sync LeetCode submission - Longest Palindromic Substring (python3)

--- problems/longest_palindromic_substring/solution.py
+++ problems/longest_palindromic_substring/solution.py
@@ -1,17 +1,17 @@
 class Solution:
-    def palindromeAt(self,s, l, r):    
-        while l >= 0 and r < len(s) and s[l] == s[r]:
-            l -= 1
-            r += 1
-        return s[l+1:r]
-    
+
     def longestPalindrome(self, s: str) -> str:
-            
+        def palindromeAt(s, l, r):    
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                l -= 1
+                r += 1
+            return s[l+1:r]
+    
         res = ""        
         for i in range(len(s)):        
-            odd  = self.palindromeAt(s, i, i)
-            even = self.palindromeAt(s, i, i+1)
-            res = max(res, odd, even, key=len)
+            odd  = palindromeAt(s, i, i)
+            even = palindromeAt(s, i, i+1)
+            res = max(res, odd, even, key=len)   #or update start and end according to lengthof odd and even...and then print final res as substring withs tart and end as corner indexes
         return res
 
  
