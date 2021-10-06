c0053c18132de9e66055092b484fb880c88c1b6c
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-10-06 03:12:40 +0000

    Sync LeetCode submission - Binary Subarrays With Sum (python3)

---
+++ problems/binary_subarrays_with_sum/solution.py
@@ -0,0 +1,22 @@
+# exactly same problem: https://leetcode.com/problems/subarray-sum-equals-k/
+# exactly sameproblem: https://leetcode.com/problems/count-number-of-nice-subarrays/
+class Solution:
+    def numSubarraysWithSum(self, p: List[int], k: int) -> int:
+        n = len(p)
+        d = {}
+        d[0] = 1
+        s = 0
+        count = 0
+        for i in range(len(p)):
+            s += p[i]
+            if s-k in d: # --- I
+                count += d[s-k]
+               # or return True
+               # or return indicies
+
+               # add sum to frq dict
+            if s in d:
+                d[s] += 1 # --- II
+            else:
+                d[s] = 1
+        return count
