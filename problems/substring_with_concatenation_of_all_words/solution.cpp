6c37a7df85d190bb44b8bbec344ab4be24efec4a
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-10-07 15:57:08 +0000

    Sync LeetCode submission - Substring with Concatenation of All Words (cpp)

---
+++ problems/substring_with_concatenation_of_all_words/solution.cpp
@@ -0,0 +1,48 @@
+//see https://www.youtube.com/watch?v=eebtefMRvjQ
+//see py code also
+
+
+//n = s.length()
+//len = length of each word(same for all)
+
+//TC: O(n*wl)
+//we visit each index atmost twice, for each index we may use substr
+
+
+
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        vector<int> res;
+        unordered_map<string, int> dict;
+        int len = words[0].size();
+        int n = s.length(), m = words.size();
+        for (const string& word : words) {
+            dict[word]++;
+        }
+        for (int i = 0; i < len; i++) {
+            int cnt = 0;
+            unordered_map<string, int> copy = dict;
+            for (int j = i; j <= n - len; j += len) {
+                string cur = s.substr(j, len);
+                copy[cur]--;
+                if (copy[cur] >= 0) {
+                    cnt++;
+                }
+                
+                int pop_start = j - m * len;
+                if (pop_start >= 0) {
+                    string pop_word = s.substr(pop_start, len);
+                    copy[pop_word]++;
+                    if (copy[pop_word] > 0) {
+                        cnt--;
+                    }
+                }
+                if (cnt == m) {
+                    res.push_back(pop_start + len);
+                }
+            }
+        }
+        return res;
+    }
+};
