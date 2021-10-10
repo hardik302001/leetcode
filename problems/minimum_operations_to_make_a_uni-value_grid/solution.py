7b3d57836cfacce971b03748a259b02f5d3f5357
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-10-10 05:52:19 +0000

    Sync LeetCode submission - Minimum Operations to Make a Uni-Value Grid (python3)

---
+++ problems/minimum_operations_to_make_a_uni-value_grid/solution.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minOperations(self, grid: List[List[int]], k: int) -> int:
+        s = 0
+        l = []
+        for i in grid:
+            l.extend(i)
+            s = s+ sum(i)
+        m = len(grid)
+        n = len(grid[0])
+        l.sort()
+        p = l.copy()
+        # print(l)
+        sz = m*n
+       
+        mid = l[sz//2]
+        ans1 = 0
+        for i in range(sz):
+            if l[i]<mid:
+                c = (mid-l[i])//k
+                ans1 = ans1 + c
+                l[i] = l[i] + c*k
+            elif l[i]>mid:
+                c = (l[i]-mid)//k
+                ans1 = ans1 +c
+                l[i] = l[i]-c*k
+        if len(set(l))!=1:
+            ans1 = -1
+                
+       
+        return ans1
