795cb3154ff2fe1210358e5b992ff8934ba51f43
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-10-13 13:11:02 +0000

    Sync LeetCode submission - Ways to Make a Fair Array (python3)

---
+++ problems/ways_to_make_a_fair_array/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def waysToMakeFair(self, nums: List[int]) -> int:
+        n = len(nums)
+        odd_prefix, even_prefix = [0] * (n + 1), [0] * (n + 1)
+        for i, num in enumerate(nums):
+            odd_prefix[i] += odd_prefix[i-1]
+            even_prefix[i] += even_prefix[i-1]
+            if i % 2 == 0:
+                even_prefix[i] += num
+            else:
+                odd_prefix[i] += num
+        result = 0
+        for i, num in enumerate(nums):
+            even_after_removed = even_prefix[i-1] + odd_prefix[n-1] - odd_prefix[i]
+            odd_after_removed = odd_prefix[i-1] + even_prefix[n-1] - even_prefix[i]
+            result += (even_after_removed == odd_after_removed)
+        return result
