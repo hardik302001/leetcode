615047b7e63dd6d6458e8731032e6b1c45f3d831
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-10-17 06:37:06 +0000

    Sync LeetCode submission - Path Sum II (cpp)

--- problems/path_sum_ii/solution.cpp
+++ problems/path_sum_ii/solution.cpp
@@ -1,29 +1,21 @@
-//Idea 1: https://leetcode.com/problems/path-sum/
-//Code idea: https://leetcode.com/problems/all-paths-from-source-to-target/
+//see prev solns
 
-//how to avoid backtracking, at every step we are taking care of every node, we are visiting
+
 class Solution {
 public:
     
-    void dfs(TreeNode* root, vector<vector<int>>& result, vector<int> path, int sum) {  //path is passed by value now(path)...
-        
+    void dfs(TreeNode* root, vector<vector<int>>& result, vector<int> &path, int sum) {
         if (root == NULL) return;
         path.push_back(root->val);
-        if (root->right == NULL and root->left ==  NULL){
+        if (root->right == NULL and root->left ==  NULL){            //bcz we need till leaf
             if(root->val == sum){
                 result.push_back(path);
-                
             }
         }
-        // for(auto it:path){
-        //     cout<<it<<" ";
-        // }
-        // cout<<endl;
-        
         dfs(root->left, result, path, sum - root->val);
         dfs(root->right, result, path , sum - root->val);
         
-        // path.pop_back();                                           //no backtracking needed 
+        path.pop_back();
     }
     
     
