52760dab20d7f6c1acdae7ddcb44938ebab76d7a
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-10-17 06:11:20 +0000

    Sync LeetCode submission - Subsets (cpp)

--- problems/subsets/solution.cpp
+++ problems/subsets/solution.cpp
@@ -1,24 +1,18 @@
-// Iterative
-
-// Using [1, 2, 3] as an example, the iterative process is like:
-
-// Initially, one empty subset [[]]
-// Adding 1 to []: [[], [1]];
-// Adding 2 to [] and [1]: [[], [1], [2], [1, 2]];
-// Adding 3 to [], [1], [2] and [1, 2]: [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]].
-
+//bitmanipulation
+//see prev submission also
 
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> subs = {{}};
-        for (int num : nums) {
-            int n = subs.size();
-            for (int i = 0; i < n; i++) {
-                subs.push_back(subs[i]); 
-                subs.back().push_back(num);
+        int n = nums.size(), p = 1 << n;
+        vector<vector<int>> subs(p);
+        for (int i = 0; i < p; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((i >> j) & 1) {
+                    subs[i].push_back(nums[j]);
+                }
             }
         }
         return subs;
     }
-}; 
+};
