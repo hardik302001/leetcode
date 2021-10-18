e2575428311def9ed1e1b494c44412b8b4550158
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-10-18 03:41:20 +0000

    Sync LeetCode submission - Count Number of Maximum Bitwise-OR Subsets (cpp)

--- problems/count_number_of_maximum_bitwise-or_subsets/solution.cpp
+++ problems/count_number_of_maximum_bitwise-or_subsets/solution.cpp
@@ -1,59 +1,21 @@
+//same as subsets
+
 class Solution {
 public:
-    int countMaxOrSubsets(vector<int>& nums) {
-        vector<vector<int> > res = subsets(nums);
-        int m= 0;
-        for(auto it:nums){
-            m = m | it;
-        }
-        int c = 0;
-        for(auto v: res){
-            int o = 0;
-            for(int i = 0;i<v.size();i++){
-                o = o|v[i];
+    int countMaxOrSubsets(vector<int>& a) {
+        int tot = 0;         //max or possible, taking all elements in account
+        
+        for(auto& i:a) tot |= i;
+        int n = a.size();
+        int ans = 0;
+        
+        for(int i=0;i<(1<<n);i++){
+            int res = 0;
+            for(int j=0;j<n;j++){
+                if((i>>j)&1) res |= a[j];
             }
-            if(o==m){
-                c++;
-            }
+            if(res==tot) ans++; 
         }
-        return c;
- 
+        return ans;
     }
-    
-    void subsetsUtil(vector<int>& A, vector<vector<int> >& res,
-                 vector<int>& subset, int index)
-{
-    res.push_back(subset);
-    for (int i = index; i < A.size(); i++) {
- 
-        // include the A[i] in subset.
-        subset.push_back(A[i]);
- 
-        // move onto the next element.
-        subsetsUtil(A, res, subset, i + 1);
- 
-        // exclude the A[i] from subset and triggers
-        // backtracking.
-        subset.pop_back();
-    }
- 
-    return;
-}
- 
-// below function returns the subsets of vector A.
-vector<vector<int> > subsets(vector<int>& A)
-{
-    vector<int> subset;
-    vector<vector<int> > res;
- 
-    // keeps track of current element in vector A;
-    int index = 0;
-    subsetsUtil(A, res, subset, index);
- 
-    return res;
-}
- 
-    
-    
-    
 };
