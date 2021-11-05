2196ff7bad4aa2e0eb021a8ae1561b7c5f1c95af
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-11-05 04:01:17 +0000

    Sync LeetCode submission - Arranging Coins (cpp)

--- problems/arranging_coins/solution.cpp
+++ problems/arranging_coins/solution.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        long long start = 0;
-        long long end = n;
-        //not that optimised..prevous soln is much  more optmised
         
-        while(start<=end)
-        {
-            long long mid = (start+end)/2;
+        long long l=1,r=n;
+        
+        while(l<r){
+           long long mid =l+(r-l)/2;
             
-            if((mid*(mid+1))/2==n)
-                return mid;
+           long long p=mid*(mid+1)/2;
             
-            if((mid*(mid+1))/2>n)
-                end = mid-1;
-            else
-                start = mid+1;
+            if(r-l==1){
+                if(r*(r+1)/2<=n) return r;
+                return l;
+            }
+            
+            
+            if(p==n) return mid;
+            if(p<n) {
+               
+                l=mid;
+            }
+            else 
+                r=mid-1;
+            
         }
+        return l;
         
-        return start-1;
         
-    
     }
 };
