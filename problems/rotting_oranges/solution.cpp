0c3b9c07caa61533bbc3fe804e6ae1ff3167e123
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-11-17 12:56:38 +0000

    Sync LeetCode submission - Rotting Oranges (cpp)

--- problems/rotting_oranges/solution.cpp
+++ problems/rotting_oranges/solution.cpp
@@ -1,13 +1,11 @@
 /*
     VISIT PREV BRUTE FROCE SOLUTION:FOR EVERY LEVEL WE TARVERSE THE WHOLE MATRIX AGAIN!    O(m*n*n*n) = O(m2n2)
     WE optimise it by BFS, level by level..
-    simple code : ans++ when puhed else ignore
+    simple code : ans++ when pushed else ignore
     TC = 0(m*n)
     
     
 */
-
-
 
 
 
