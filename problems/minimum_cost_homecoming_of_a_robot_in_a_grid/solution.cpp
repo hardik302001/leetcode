6f3394ee21de9a106e74538022f696be427affdd
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-11-27 16:11:58 +0000

    Sync LeetCode submission - Minimum Cost Homecoming of a Robot in a Grid (cpp)

---
+++ problems/minimum_cost_homecoming_of_a_robot_in_a_grid/solution.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int minCost(vector<int>& s, vector<int>& e, vector<int>& r, vector<int>& c) {
+        long long sum=0;
+        if(s[0]>e[0])
+        {
+            int idx=s[0];
+            while(idx!=e[0])
+            {
+                sum+=r[--idx];
+            }
+        }
+        else if(s[0]<e[0])
+        {
+            int idx=s[0];
+            while(idx!=e[0])
+            {
+                sum+=r[++idx];
+            }
+        }
+        
+        if(s[1]>e[1])
+        {
+            int idx=s[1];
+            while(idx!=e[1])
+            {
+                sum+=c[--idx];
+            }
+        }
+        else if(s[1]<e[1])
+        {
+            int idx=s[1];
+            while(idx!=e[1])
+            {
+                sum+=c[++idx];
+            }
+        }
+        
+        return sum;
+    }
+};
