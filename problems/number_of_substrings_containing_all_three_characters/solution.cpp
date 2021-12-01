4c52e653438da456e41906b3f65660f6681dff81
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-12-01 16:46:16 +0000

    Sync LeetCode submission - Number of Substrings Containing All Three Characters (cpp)

---
+++ problems/number_of_substrings_containing_all_three_characters/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int l = 0;
+        int ans = 0;
+        int n = s.size();
+        int a = 0, b = 0, c = 0;
+        for (int r = 0;r<n;r++){ 
+            if(s[r]=='a')a++;
+            else if(s[r]=='b')b++;
+            else c++;
+
+            
+            while(a>0 and b>0 and c>0){
+                ans = ans + (n-r);
+                if(s[l]=='a')a--;
+                else if(s[l]=='b')b--;
+                else c--;
+                l++;
+            }
+            
+            
+    
+        }
+        return ans;
+    }
+};
