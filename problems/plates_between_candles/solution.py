8a4a7ae958e3eea5131977838dea011789c692b6
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-12-03 07:32:22 +0000

    Sync LeetCode submission - Plates Between Candles (python3)

--- problems/plates_between_candles/solution.py
+++ problems/plates_between_candles/solution.py
@@ -2,8 +2,8 @@ class Solution:
     def platesBetweenCandles(self, s: str, queries: List[List[int]]) -> List[int]:
         n=len(s)
         pre=[0]*n
-        left=[-1]*n
-        right=[n+1]*n
+        left=[-1]*n          #indexes of nearest left candle
+        right=[n+1]*n        #indexs of nearest right cadle
         c=-1
         for i in range(n):
             if s[i]=="|":
@@ -18,21 +18,26 @@ class Solution:
                 right[i]=c
             else:
                 right[i]=c
-            
+        # print(left)
+        # print(right)
         c=0
-        star=[0]*n
+        star=[0]*n          #count of stars tille very index i (only stars no candles)
         for i in range(n):
             if s[i]=="*":
                 c+=1
                 star[i]=c
             else:
                 star[i]=star[i-1]
+        # print(star)
+        
+        
         res=[]
         for st,end in queries:
-            st1=right[st]
-            end1=left[end]
-            if st1==end1 or st1>end or end1<st1:
+            sti=right[st]      #nearest left candle from that (after or on that ) starting index
+            endi=left[end]     #nearest right candle from that (before or on that ) end index
+            # print(sti,endi)
+            if sti==endi or sti>end or endi<st: #if only single candle segment or out of bound segments then 0
                 res.append(0)
             else:
-                res.append(star[end1]-star[st1])
+                res.append(star[endi] - star[sti])   #else find count of stars (only stars , no candlles, so we explictly made star prefix array)
         return res
