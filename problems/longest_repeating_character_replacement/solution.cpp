bdde931613b0f2bb262fd716ff2d227acd811402
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-12-05 19:13:46 +0000

    Sync LeetCode submission - Longest Repeating Character Replacement (cpp)

--- problems/longest_repeating_character_replacement/solution.cpp
+++ problems/longest_repeating_character_replacement/solution.cpp
@@ -12,12 +12,15 @@ public:
         while(end<s.length()){
             char cur = s[end];
             mp[cur]++;                          //taking freq
-            max_freq = max(max_freq, mp[cur]);  //max freq of that window (window = end-start+1)
+            max_freq = max(max_freq, mp[cur]);  //max freq of that window (window = end-start+1), why mp[cur]?? bcz in line no 14, freq map is updated , so we need to update max_freq also
             
-            if(  ( end-start+1) - max_freq> k){   //we have only k choices to remove ..if window len with k replacement goes out of range then we remove from starting..
+            while(  ( end-start+1) - max_freq> k){   //we have only k choices to remove ..if window len with k replacement goes out of range then we remove from starting..
                 char startchar = s[start];
                 mp[startchar]--;   //dec char count for that char...bcz it will not be counted in that window
                 start++;
+                for(auto it: mp){
+                    max_freq = max(max_freq, it.second);
+                }
             }
             max_length = max(max_length, end-start+1);   //max length of that window
             end++;
