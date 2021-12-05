d06694d20c9b3d0c6c228574b19a19f7062b4503
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-12-05 04:33:36 +0000

    Sync LeetCode submission - Find if Path Exists in Graph (cpp)

--- problems/find_if_path_exists_in_graph/solution.cpp
+++ problems/find_if_path_exists_in_graph/solution.cpp
@@ -1,31 +1,23 @@
-//dfs givez tle
-
-//so source to target always, bfs
 class Solution {
 public:
+    vector<int> parent;     
+    int findParent(int node) {
+        return parent[node] == node ? node : (parent[node] = findParent(parent[node]));
+    }    
+    void makeSameGroup(int u , int v) {
+        int pu = findParent(u) ; 
+        int pv = findParent(v);
+        parent[pu] = pv; 
+    }
+    
     bool validPath(int n, vector<vector<int>>& edges, int start, int end) {
-        vector<vector<int>> graph(n+1); 
-        for(auto e : edges) {
-            graph[e[0]].push_back(e[1]);
-            graph[e[1]].push_back(e[0]);
-        }
-        vector<bool> visited(n,0);        
-        queue<int> q;
-        q.push(start);
-        visited[start] = 1; 
-        while(!q.empty()) {
-            int curr = q.front();
-            q.pop();
-            if(curr == end)
-                return true; 
-            for(auto &node : graph[curr]){
-                if(!visited[node]){
-                    visited[node] = 1; 
-                    q.push(node);
-                }
-            }
-        }
+        parent.resize(n);
+        for(int i=0;i<n;i++)
+            parent[i] = i; 
         
-        return false;
+        for(auto e : edges) {
+            makeSameGroup(e[0] , e[1]); 
+        }                         
+        return findParent(start) == findParent(end); 
     }
 };
