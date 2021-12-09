c8450da24dec368116a99027818f9986b7aaae36
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-12-09 05:49:56 +0000

    Sync LeetCode submission - Jump Game III (cpp)

---
+++ problems/jump_game_iii/solution.cpp
@@ -0,0 +1,18 @@
+//dfs
+//also see bfs soln
+
+class Solution {
+public:
+    
+    bool dfs(vector<int>& arr, vector<bool>& visited, int i) {
+        if (i < 0 or i >= arr.size() or visited[i]) return false;
+        if (arr[i] == 0) return true;
+        visited[i] = true;
+        return ( dfs(arr, visited, i + arr[i]) || dfs(arr, visited, i - arr[i]) );
+    }
+    
+    bool canReach(vector<int>& arr, int start) {
+        vector<bool> visited(arr.size(), false);
+        return dfs(arr, visited, start);
+    }
+};
