8e0581b5077d8fb0755e2d20ff23c60772610bea
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-12-09 14:55:29 +0000

    Sync LeetCode submission - Continuous Subarray Sum (python3)

---
+++ problems/continuous_subarray_sum/solution.py
@@ -0,0 +1,21 @@
+#for more similar ques see: https://leetcode.com/problems/subarray-sum-equals-k/
+
+class Solution:
+    def checkSubarraySum(self, A: List[int], K: int) -> bool:
+        
+        ans = 0
+        d = {}
+        d[0]  = -1         #for length we do -1 as index
+        len_A = len(A)
+        acc = 0        
+        for i in range(len_A):
+            acc += A[i]       
+            key = acc % K
+            
+            if key in d:
+                ans = max(ans, i - d[key])
+            else:
+                d[key] = i         #updating indexes
+            
+            
+        return ans>=2
