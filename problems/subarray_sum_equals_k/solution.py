2d543d945c9f01a2d02548d16eeacedbb044e910
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-12-09 13:31:02 +0000

    Sync LeetCode submission - Subarray Sum Equals K (python3)

--- problems/subarray_sum_equals_k/solution.py
+++ problems/subarray_sum_equals_k/solution.py
@@ -7,6 +7,12 @@ Single scan. Given the current sum and the k, we check if (sum-k) existsed befor
 
 '''
 
+
+'''
+also see: https://www.tutorialspoint.com/maximum-size-subarray-sum-equals-k-in-cplusplus
+also see: https://leetcode.com/problems/subarray-sums-divisible-by-k/
+'''
+
 #for more context search subarray sum equals k on yputube and watch pepcoding videos
 #also see : https://leetcode.com/problems/subarray-sums-divisible-by-k/
 class Solution:
