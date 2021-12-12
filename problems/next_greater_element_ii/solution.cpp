6e5b2ded17982a158195eb5a44094df170797bc7
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-12-12 16:40:27 +0000

    Sync LeetCode submission - Next Greater Element II (cpp)

--- problems/next_greater_element_ii/solution.cpp
+++ problems/next_greater_element_ii/solution.cpp
@@ -5,32 +5,30 @@
 2. check while updating value of nge[i], it shud be less than n
 3. i = i%n
 */
+
+
+//monotonic stack
+//similar to nge1
+
+
+
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        stack<int>s;
+        stack<int> s; 
         int n = nums.size();
-        vector<int>ans(n,-1);
-
-        for(int i = 2*n-1;i>=0;i--){
-            while(!s.empty() and s.top()<=nums[i%n]){
+        vector<int> res(n, -1);
+      
+        for(int i = 0; i < n * 2; i++) {
+            int idx = i % n;
+            while(!s.empty() && nums[s.top()] < nums[idx]) {
+                res[s.top()] = nums[idx];
                 s.pop();
-            }   
-            
-            if(i<n){                    //nge is of size n, so all value putting work will be done only when i is less than n
-                if(!s.empty()){          //now code is totally same as nge-i
-                    ans[i] = s.top();
-                }
-                else{
-                    ans[i] = -1;
-                }
-            }
-            
-            s.push(nums[i%n]);
-            
+            }            
+            s.push(idx); 
         }
         
-
-        return ans;
+        return res; 
     }
+    
 };
