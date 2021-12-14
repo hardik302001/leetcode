42dbcb41f6673f758693eafe1f61cb71971439ac
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-12-14 05:24:01 +0000

    Sync LeetCode submission - Remove K Digits (cpp)

--- problems/remove_k_digits/solution.cpp
+++ problems/remove_k_digits/solution.cpp
@@ -1,3 +1,14 @@
+
+
+// SEE PYTHON SOLUTION 
+// THAT IS MUCH BETTER, there we keep (n-k) elements
+// smalleest subsequence of length (n-k)
+// similar to https://leetcode.com/problems/find-the-most-competitive-subsequence/submissions/
+
+
+
+// here we try to remove only k elements, we aim to remove k in beginning only, bcz we want lexographically smallest string
+//this is also nice code
 class Solution {
 public:
     string removeKdigits(string num, int k) {
@@ -9,11 +20,11 @@ public:
                k--;                                             // remove one char
            }
            
-           if (ans.length() || c != '0') { ans.push_back(c); }  // can't have leading '0'
-       }
+           if (ans.length() || c != '0') { ans.push_back(c); }  // can't have leading '0', beautiful logic to find it! if ans is empty and  c=='0', then it wont push..else it would push irrespevtiv eof value of c
+        }
        
        while (ans.length() && k--) { ans.pop_back(); }          // make sure remove k digits in total
        
        return ans.empty() ? "0" : ans;
-}
+    }
 };
