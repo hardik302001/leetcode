dd15938ac4ad61c1376c7be4d4c3de5bc4f2f377
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-12-18 12:08:58 +0000

    Sync LeetCode submission - Count Special Quadruplets (python3)

--- problems/count_special_quadruplets/solution.py
+++ problems/count_special_quadruplets/solution.py
@@ -1,11 +1,22 @@
+# tc:      O(n2)
+
 class Solution:
     def countQuadruplets(self, nums: List[int]) -> int:
+		# We are looking for a < b < c < d that satisfies equation nums[a] + nums[b] == nums[d] - nums[c]
         n = len(nums)
-        c = 0
-        for i in range(n):
-            for j in range(i+1,n):
-                for k in range(j+1,n):
-                    for l in range(k+1,n):
-                        if nums[i]+nums[j]+nums[k]==nums[l]:
-                            c = c +1
-        return c
+        res = 0
+        d = {}
+        d[nums[0] + nums[1]] = 1 # intial count for i == 2 
+        for i in range(2, n - 1):
+            for j in range(i + 1, n):
+                # i < j, i => c and j => d
+                if nums[j] - nums[i] in d:
+                    res += d[nums[j] - nums[i]] # look for counts of result of right hand side of the equation with nums[i]
+            
+            for j in range(i):
+                # i > j, i => b and j => a
+                if nums[i] + nums[j] in d:
+                    d[nums[i] + nums[j]] += 1 # add new sums with nums[i] of left hand side to the dictionary
+                else:
+                    d[nums[i] + nums[j]] = 1
+        return res
