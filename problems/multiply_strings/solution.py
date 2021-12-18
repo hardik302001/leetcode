650f2bc8f606347eb61269e8fca11662751e493c
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-12-18 13:02:10 +0000

    Sync LeetCode submission - Multiply Strings (python3)

--- problems/multiply_strings/solution.py
+++ problems/multiply_strings/solution.py
@@ -1,3 +1,26 @@
 class Solution:
-    def multiply(self, num1: str, num2: str) -> str:
-        return str(int(num1)*int(num2))
+    def multiply(self, n1: str, n2: str) -> str:
+        if n1== "0" or n2=="0":
+            return "0"
+        
+        res = [0]*(len(n1) + len(n2))
+        n1 = n1[::-1]
+        n2 = n2[::-1]
+        
+        for i1 in range(len(n1)):
+            for i2 in range(len(n2)):
+                dig = int(n1[i1])*int(n2[i2]) + res[i1 + i2]
+                res[i1 + i2] = dig%10
+                res[i1 + i2 + 1] += dig//10
+        # print(res)
+        while res[-1]==0:
+            res.pop()
+            
+        
+        res = res[::-1]
+    
+        ans = ""
+        for i in res:
+            ans = ans + str(i)
+    
+        return ans
