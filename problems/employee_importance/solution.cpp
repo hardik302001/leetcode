08f5a0079bde72fbc467ff1f56906849d2710326
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-12-22 11:19:55 +0000

    Sync LeetCode submission - Employee Importance (cpp)

--- problems/employee_importance/solution.cpp
+++ problems/employee_importance/solution.cpp
@@ -1,15 +1,32 @@
+/*
+
+The Dot(.) operator is used to normally access members of a structure or union.
+as we do in dsu, we have class, and we access member using '.'.
+
+The Arrow(->) operator exists to access the members of the structure or the unions using pointers.
+as we do in link lists, we have pointer to ListNode pointer, so we use '->' to access next, prev member
+
+
+*/
+
 class Solution {
 public:
+    unordered_map<int, Employee*> emap;
+    
     int getImportance(vector<Employee*> employees, int id) {
-        unordered_map<int, Employee*>m;         //to find employee by employee id
-        for(auto x: employees) m[x->id] = x;
-        int sum = 0;
-        DFS(m, id, sum);
-        return sum;
+        for(auto& emp : employees){
+            emap[emp->id] = emp;
+        }
+        return dfs(id);
     }
     
-    void DFS(unordered_map<int, Employee*>& m, int id, int& sum){
-        sum += m[id]->importance;
-        for(auto x: m[id]->subordinates) DFS(m, x, sum);
+    
+    int dfs(int id){
+        Employee* emp = emap[id];
+        int result = emp->importance;
+        for(auto& s : emp->subordinates){
+            result += dfs(s);
+        }
+        return result;
     }
 };
