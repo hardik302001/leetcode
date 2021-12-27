a403916e17595e1f661134faa124b882ecb8d560
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-12-27 16:07:20 +0000

    Sync LeetCode submission - Finding the Users Active Minutes (python3)

---
+++ problems/finding_the_users_active_minutes/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findingUsersActiveMinutes(self, logs: List[List[int]], k: int) -> List[int]:
+        d = {}
+        for i in logs:
+            if i[0] not in d:
+                d[i[0]] = set()
+            d[i[0]].add(i[1])
+        
+        ans = [0]*k
+        for i in d:
+            l = len(d[i])
+            ans[l-1]+=1
+        return ans
