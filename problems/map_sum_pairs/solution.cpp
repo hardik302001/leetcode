2473bccbd105eb12ea856d19ebb6b3a9bd15ee27
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-12-29 18:03:48 +0000

    Sync LeetCode submission - Map Sum Pairs (cpp)

--- problems/map_sum_pairs/solution.cpp
+++ problems/map_sum_pairs/solution.cpp
@@ -1,21 +1,89 @@
-class MapSum {
+class TrieNode{
 public:
-    map<string, int> count;
-    MapSum() {
-        
-    }
-    
-    void insert(string key, int val) {
-        count[key] = val;
-    }
-    
-    int sum(string prefix) {
-        int answer = 0;
-        for(map<string, int>::iterator i = count.begin(); i != count.end(); i++)
-        {
-            if(i->first.find(prefix) == 0)
-                answer += i->second;
+    int endOfWord;
+    TrieNode* children[26];          //use array,better memory usage
+    int score;
+    TrieNode()
+    {
+        endOfWord = 0;
+        for(int i = 0;i<26;i++){
+            children[i] = NULL;   
         }
-        return answer;
+        score = 0;
     }
 };
+
+
+class MapSum {
+public:
+    unordered_map<string, int>m;
+    MapSum() {
+        root = new TrieNode();
+    }
+    
+    void insert(string word, int val) {
+        TrieNode* curr = root;
+
+        for(int i = 0; i<word.size(); i++)
+        {
+            int index = word[i] - 'a';
+            
+            if(curr->children[index] == NULL)
+            {
+                curr->children[index] = new TrieNode();
+            }
+            
+            curr = curr->children[index];
+
+        }
+        curr->score = val;    //bcz score is overriden, so we cant do prefix sum here one, we need to wait!
+        curr->endOfWord += 1;
+    }
+    
+    
+    int sum(string prefix) {
+        res = 0;
+        TrieNode* curr = root;
+        for(char c : prefix) {
+            if(curr->children[c-'a']==NULL) 
+                return 0;
+            curr = curr->children[c-'a'];
+        }
+        helper(curr);        //now i m at last char of prefix
+        return res;
+    }
+    
+    void helper(TrieNode* curr) {
+        res += curr->score;    //pref sum of all strings greater than curr string
+        for (int i = 0; i < 26; i++) {
+            if (curr->children[i] != NULL) {
+                helper(curr->children[i]);
+            }
+        }
+    }
+    
+    
+    //----------------------------------------------
+    
+        ~MapSum(){                         //destructor: to free memory
+        clear(root);
+    }
+    
+    void clear(TrieNode* root)
+    {
+        for(int i = 0; i<26; i++)
+        {
+            if(root->children[i] != NULL)
+            {
+                clear(root->children[i]);
+            }
+        }
+        
+        delete root;
+    }
+
+private:  //GOOD PRACTICE TO KEEP ROOT NODE PRIVATE
+    TrieNode* root;
+    int res;
+};
+
