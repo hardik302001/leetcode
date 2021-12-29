2aa4d47f874d39247f6df16e9027f30fc40ad752
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2021-12-29 01:56:06 +0000

    Sync LeetCode submission - Populating Next Right Pointers in Each Node (cpp)

---
+++ problems/populating_next_right_pointers_in_each_node/solution.cpp
@@ -0,0 +1,30 @@
+// we just need to define the new pointer "next" for the given tree, so we just need to tell that for every nide x, what will be x->next !
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        
+        if(root==NULL){
+            return root;
+        }
+        
+        queue<Node* >q;
+        q.push(root);
+        
+        while(!q.empty()){
+            int n = q.size();
+            for(int i=0;i<n;i++){
+                Node* x=q.front();
+                q.pop();
+                if(i!=n-1)x->next=q.front();
+                else x->next = NULL;
+                
+                if(x->left)q.push(x->left);
+                if(x->right)q.push(x->right);
+            }
+            
+        }
+        
+        return root;
+    }
+};
