a962db35936bae170bf7772c7418f675fb7a992d
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-01-03 13:31:52 +0000

    Sync LeetCode submission - Minimum Number of Arrows to Burst Balloons (python3)

---
+++ problems/minimum_number_of_arrows_to_burst_balloons/solution.py
@@ -0,0 +1,26 @@
+
+# same as counting number of non overlapping intervals
+# https://leetcode.com/problems/non-overlapping-intervals/
+
+class Solution:
+    def findMinArrowShots(self, intervals: List[List[int]]) -> int:
+        n = len(intervals)
+        c = 0
+        intervals.sort()
+        
+        begin = intervals[0]
+        for i in range(1, n):
+            if intervals[i][0] <= begin[1]:
+                c = c + 1
+                if intervals[i][1]>= begin[1]:
+                    begin = begin
+                else:
+                    begin = intervals[i]
+            
+            
+            else:
+                begin = intervals[i]
+
+                
+        
+        return n-c
