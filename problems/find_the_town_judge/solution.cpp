0bb15b3f20b7fbcf7bb8c1dd9d793d90602e8d2a
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-01-03 02:33:07 +0000

    Sync LeetCode submission - Find the Town Judge (cpp)

--- problems/find_the_town_judge/solution.cpp
+++ problems/find_the_town_judge/solution.cpp
@@ -27,7 +27,7 @@ public:
 		//if a node has outdegree==0 that means that node doesnot trust anyone and
 		//indegree ==n-1 that means all other nodes trusts this node except himself
         
-		for(int i=0;i<=n;i++){
+		for(int i=1;i<=n;i++){
             if(out[i]==0 && in[i]==n-1)
                 return i;
         }
