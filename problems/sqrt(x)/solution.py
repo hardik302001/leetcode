07028f965aac7ec9d45567f009bdc8542200aa07
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-01-09 06:35:17 +0000

    Sync LeetCode submission - Sqrt(x) (python3)

--- problems/sqrt(x)/solution.py
+++ problems/sqrt(x)/solution.py
@@ -1,11 +1,15 @@
 class Solution(object):
     def mySqrt(self, x):
         l, r = 0, x
-        while l <= r:
+        while l < r:
             mid = l + (r-l)//2
-            if mid * mid <= x < (mid+1)*(mid+1):
-                return mid
-            elif x < mid * mid:
-                r = mid - 1
+            if mid*mid==x:
+                return mid   
+            if x < mid * mid:
+                r = mid
             else:
                 l = mid + 1
+        if l*l>x:
+            return l-1
+        else:
+            return l
