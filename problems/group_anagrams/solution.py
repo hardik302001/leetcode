daa5ec5f62455d9adfed82d65c45bffbc78e7f82
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-01-11 13:23:01 +0000

    Sync LeetCode submission - Group Anagrams (python3)

--- problems/group_anagrams/solution.py
+++ problems/group_anagrams/solution.py
@@ -1,24 +1,30 @@
-# sort all strings, if string are anagrams, then after sorting they are same..
-# for optimization we will use dictionary having key as the sorted string, and we will add anagrams in list as values in that key..
+# https://leetcode.com/problems/group-anagrams/discuss/1398888/C%2B%2BPython-Group-by-sorted-string-group-by-count-characters-Solutions-Clean-and-Concise
+
+
+# class Solution:
+#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+#         def getKey(s):
+#             cnt = [0] * 26
+#             for c in s: cnt[ord(c) - ord('a')] += 1
+#             ans = []
+#             for i in range(26): ans.append(cnt[i] * chr(i + ord('a')))
+#             return "".join(ans)
+
+#         groups = defaultdict(list)
+#         for s in strs:
+#             groups[getKey(s)].append(s)
+#         return groups.values()
+
 
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        p = []
-        for i in strs:
-            ss = sorted(i)
-            q = ''
-            for sss in ss:
-                q = q + sss
-            p.append([q,i])
-            
-            
-        print(p)
-        d = {}
-        for i in p:
-            if i[0] in d:
-                d[i[0]].append(i[1])
-            else:
-                d[i[0]] = [i[1]]
-        ans = d.values()
-        return ans
+        def getKey(s):
+            cnt = [0] * 26
+            for c in s: cnt[ord(c) - ord('a')] += 1
+            return tuple(cnt)
+
+        groups = defaultdict(list)
+        for s in strs:
+            groups[getKey(s)].append(s)
+        return groups.values()
