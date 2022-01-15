ff5e8df1f68c61e4c350fb2277229fb3a2a26c17
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-01-15 12:54:56 +0000

    Sync LeetCode submission - Sliding Window Maximum (cpp)

--- problems/sliding_window_maximum/solution.cpp
+++ problems/sliding_window_maximum/solution.cpp
@@ -1,7 +1,14 @@
 //SLIDING WINDOW + DEQUE
 //SIMILAR PROBEM : https://leetcode.com/problems/shortest-subarray-with-sum-at-least-k/ 
 
-//also do: https://leetcode.com/problems/jump-game-vi/
+
+
+
+//also do: https://leetcode.com/problems/jump-game-vi/    , very similar , do chcek it out!
+
+
+
+
 //also do : https://leetcode.com/problems/constrained-subsequence-sum/
 
 class Solution {
@@ -10,26 +17,26 @@ public:
         int n=nums.size();
         vector<int> ans(n-k+1,0);
         
-        deque<int> dq;
+        deque<int> dq;       //max deque, dq.front(), gives max element
 
         
         for(int i=0;i<k;i++){
             while(!dq.empty() && nums[dq.back()]<=nums[i])
                 dq.pop_back(); //Pop indexes from rear/back end
-            dq.push_back(i);           //push indexes 
+            dq.push_back(i);           //push latest maximum index 
         }
         
         for(int i=k;i<n;i++){
             ans[i-k]=nums[dq.front()];
-            
-            while(!dq.empty() && dq.front()<=i-k) //If elements are out of range k
-                dq.pop_front(); //pop from front
-            
             //Pop from back rear end as we are concerned with max number only
             while(!dq.empty() && nums[dq.back()]<=nums[i]) 
                 dq.pop_back();          //pop indexes
-            
             dq.push_back(i);
+            
+            
+            if(!dq.empty() && dq.front() ==i-k) //If elements are out of range k
+                dq.pop_front(); //pop from front
+            
         }
         ans[n-k]=nums[dq.front()];
         return ans;
