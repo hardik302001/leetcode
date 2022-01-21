f48d0988809357f5d9397eb0c9270ffecfdebe95
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-01-21 04:07:42 +0000

    Sync LeetCode submission - Gas Station (cpp)

--- problems/gas_station/solution.cpp
+++ problems/gas_station/solution.cpp
@@ -9,46 +9,23 @@ Basically we make two passes to the array and if am able to reach my starting po
     */
 
 class Solution {
-
 public:
-
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-
         int diff = 0, n = gas.size(), count = 0;
-
-       
-
         for(int i=0; i<2*n; i++){
-
             diff += (gas[i % n] - cost[i % n]);
-
             count++;
-
             if( diff < 0 ){
-
                 count = 0;
-
                 diff = 0;
-
             }
-
-           
-
             if( count == n) {
-
                 cout << i;
-
                 return (i+1) % n;
-
             }
-
         }
-
         return -1;
-
     }
-
 };
-
-// this is also a solutuon you can check for interview 2 pass : https://leetcode.com/problems/gas-station/discuss/1237895/Easy-C++-solution-in-O(n)-time-complexity
+        
 
