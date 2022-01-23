4e6263e241c605b5c2fac0d1bf7a10918d75a88e
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-01-23 15:57:47 +0000

    Sync LeetCode submission - Number of Ways to Divide a Long Corridor (cpp)

---
+++ problems/number_of_ways_to_divide_a_long_corridor/solution.cpp
@@ -0,0 +1,31 @@
+// greedy : https://leetcode.com/problems/number-of-ways-to-divide-a-long-corridor/discuss/1709704/Greedy-Solution-or-C%2B%2B
+
+
+
+class Solution {
+public:
+    int mod = 1e9 + 7;
+    
+    int numberOfWays(string corridor) {
+        vector<int> pos;
+        for (int i = 0; i < corridor.size(); i++) {
+            if (corridor[i] == 'S') {
+                pos.push_back(i);
+            }
+        }
+        
+        if (pos.size() % 2 || pos.size() == 0)
+            return 0;
+        
+        long long ans = 1;
+        int prev = pos[1];
+        for (int i = 2; i < pos.size(); i += 2) {
+            int length = pos[i] - prev;
+            ans = (ans * length) % mod;
+            
+            prev = pos[i + 1];
+        }
+        
+        return ans;
+    }
+};
