dc8722bd2366336bda8eb6f3858a191af26d5d7b
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-01-28 08:26:15 +0000

    Sync LeetCode submission - Word Search (cpp)

--- problems/word_search/solution.cpp
+++ problems/word_search/solution.cpp
@@ -1,6 +1,8 @@
 //Trie implementation
 //code idea similar to : https://leetcode.com/problems/replace-words/
 //also see: https://leetcode.com/problems/implement-magic-dictionary/
+// // Time: O(mxn * 4) Worse case is traversing the entire grid 
+// Space: O(mxn + wordLength) I created a new matrix to track visited  + a trie
 
 class TrieNode{
 public:
@@ -126,6 +128,52 @@ public:
     
     
 };
+
+//backtracking + dfs solution for the same!
+// Space complexity O(Length of longest word) as space complexity = depth of recursion tree. Time complexity = O(n * m * 4^L) , where L = length of longest word. (as in every recursive call, we are calling the function 4 times, thus exponential complexity).
+
+/*
+class Solution {
+public:
+bool exist(vector<vector<char>>& board, string word) 
+{
+		if(board.empty())
+			return false;
+    
+		for(int i=0; i<board.size(); i++)
+		{
+			for(int j=0; j<board[0].size(); j++)
+			{
+				if(dfs(board, 0, i, j, word))
+					return true;
+			}
+		}
+    
+    return false;
+}
+
+bool dfs(vector<vector<char>>&board, int count, int i, int j, string& word)
+{
+    if(word.size() == count) //Signifies that we have reached the end of search
+        return true;
+    
+    if(i<0 || j<0 || i>=board.size() || j>=board[0].size() || board[i][j]!=word[count])
+	return false;
+    //We check if element is within bounds and then check if the character at that is the same as the corresponding character in string word
+        
+
+    char temp = board[i][j];
+    board[i][j] = ' '; //To show that we have visited this node
+    
+    bool res = dfs(board, count+1, i+1, j, word) || dfs(board, count+1, i-1, j, word) || dfs(board, count+1, i, j+1, word) ||dfs(board, count+1, i, j-1, word); //DFS in all 4 directions
+    
+    board[i][j] = temp; //Restore the element after checking   , backtracking..bcz after chenging the value(marking visited) the matrix would be changed
+    
+    return res;
+}
+};
+
+*/
 
 
 
