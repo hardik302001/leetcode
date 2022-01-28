fe29b0ee531a12443ebcfb739ebc97ebdf827052
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-01-28 06:31:39 +0000

    Sync LeetCode submission - Minimum Remove to Make Valid Parentheses (cpp)

--- problems/minimum_remove_to_make_valid_parentheses/solution.cpp
+++ problems/minimum_remove_to_make_valid_parentheses/solution.cpp
@@ -1,7 +1,7 @@
 
 //also see https://leetcode.com/problems/minimum-number-of-swaps-to-make-the-string-balanced/
 //https://www.geeksforgeeks.org/minimum-swaps-bracket-balancing/
-//SEE PREV SOLN
+//SEE PREV SOLN:below
 
 //in this approach we are avoiding stack, and doing 2 traversals, 
 //first from beginning 
@@ -29,3 +29,61 @@ public:
         return s;
     }
 };
+
+
+//also see https://leetcode.com/problems/minimum-number-of-swaps-to-make-the-string-balanced/
+//https://www.geeksforgeeks.org/minimum-swaps-bracket-balancing/
+
+
+/*
+APPROACH 1
+Here, we will use a stack for checking the validity of parentheses, and later remove the indexes of invalid parentheses from the string s. Thanks @harmxnkhurana for the suggestion of directly marking the invalid parentheses in string s rather than redundant storing it in a different vector.
+
+First, iterate the string s and mark the index of those characters which need to be removed to make it parentheses string using a special symbol '#'.
+Here, a stack is used for finding the valid pair of parentheses, and while doing so also mark the indexes of invalid parentheses in s.
+Finally, iterate s again and append non-marked symbol (#) to ans.
+COMPLETE CODE
+
+*/
+
+/*
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        
+        stack<int>st; // helper stack for finding matching parentheses
+        
+        for(int i=0;i<s.length();++i){
+            if(s[i]=='('){ // for open parentheses push into stack
+                st.push(i);
+            }
+            else if(s[i]==')'){ // for closing parentheses
+                // if no matching previous open parentheses found, we need to remove the index of that open parentheses from "s" so for now we are marking it with special character '#'
+                if(st.empty()){ 
+                    s[i]='#';
+                }
+                else{
+                    // if matching open parentheses found remove that from the stack
+                    st.pop();
+                }
+            }
+        }
+
+        // if stack is not empty, that means it contains open parentheses indexes which don't have any matching closing parentheses
+        while(!st.empty()){
+            s[st.top()]='#';
+            st.pop();
+        }
+        
+        string ans="";
+        for(int i=0;i<s.length();++i){
+            if(s[i]!='#'){ // append not marked character to the end of "ans"
+                ans.push_back(s[i]);
+            }
+        }
+        
+        return ans;
+    }
+};
+
+*/
