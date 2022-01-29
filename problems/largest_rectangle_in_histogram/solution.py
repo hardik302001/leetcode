5e62eb4e047b51b1dd68048e9e7769cc246e39c6
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-01-29 11:27:54 +0000

    Sync LeetCode submission - Largest Rectangle in Histogram (python3)

--- problems/largest_rectangle_in_histogram/solution.py
+++ problems/largest_rectangle_in_histogram/solution.py
@@ -1,21 +1,44 @@
+# very similar to : https://leetcode.com/problems/maximum-score-of-a-good-subarray/
+
+#nse 
+#pse
+#monotonic stack
+
 class Solution:
+    def largestRectangleArea(self, arr):
+        n = len(arr)
+        next_smaller_element = [-1]*n      #monotonic inc stack, left to right
+        prev_smaller_element = [-1]*n      #monotonic inc stack, right to left
     
-    #courtesy : apna college same title video.
-    
-    
-    #its good to have increasing heights...
-    
-    def largestRectangleArea(self, heights: List[int]) -> int:
-        heights.append(0)   #for safe case..bcz any of the height cant be 0..and always greater than zero..so it will a inc sequence
-        stack = [-1]   #stack.top = -1
+        stack1 = []
+        for i in range(n):
+            while len(stack1) and arr[stack1[-1]] > arr[i]:
+                next_smaller_element[stack1[-1]] = i
+                stack1.pop()   
+            stack1.append(i)
+
+            
+        stack2 = []
+        for i in range(n-1,-1,-1):
+            while len(stack2) and arr[stack2[-1]] >= arr[i]:
+                prev_smaller_element[stack2[-1]] = i
+                stack2.pop()
+            stack2.append(i)
+            
+        print(prev_smaller_element)    
+        print(next_smaller_element)
         ans = 0
-        for i in range(len(heights)):
-            while heights[i] < heights[stack[-1]]:  #decreasing height..so we need to remove..until we have again increasing heights in order again
-                p= stack.pop()  #stack array have indexes
-                print(p)
-                height = heights[p]
-                width = i - stack[-1] - 1 #i i s right point  , stack[-1] is left pointer
-                ans = max(ans, height * width)
-            stack.append(i) #index of height array
-        heights.pop()
+        for i in range(n):
+            if prev_smaller_element[i]== -1:
+                left_diff = i - 0
+            else:
+                left_diff = (i - prev_smaller_element[i]) - 1
+                
+            if next_smaller_element[i]== -1:
+                right_diff = n-1 - i
+            else:
+                right_diff = (next_smaller_element[i] - i) - 1
+            m = arr[i]* (left_diff +  right_diff+1)
+            ans = max(ans ,m)
+            
         return ans
