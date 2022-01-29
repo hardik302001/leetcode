74114bb1947037b46cf8807ce370a6c8d80be8f0
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-01-29 16:05:35 +0000

    Sync LeetCode submission - Maximal Rectangle (python3)

--- problems/maximal_rectangle/solution.py
+++ problems/maximal_rectangle/solution.py
@@ -1,19 +1,43 @@
 class Solution:
-    def largestRectangleArea(self, heights: List[int]) -> int:
-        heights.append(0)   
-        stack = [-1]   #stack.top = -1
-        ans = 0
-        for i in range(len(heights)):
-            while heights[i] < heights[stack[-1]]:  
-                p= stack.pop()
-                # print(p)
-                height = heights[p]
-                width = i - stack[-1] - 1 #i i s right point  , stack[-1] is left pointer
-                ans = max(ans, height * width)
-            stack.append(i) #index of height array
-        heights.pop()
-        return ans
+    def largestRectangleArea(self, arr):
+        n = len(arr)
+        next_smaller_element = [-1]*n      #monotonic inc stack, left to right
+        prev_smaller_element = [-1]*n      #monotonic inc stack, right to left
     
+        stack1 = []
+        for i in range(n):
+            while len(stack1) and arr[stack1[-1]] > arr[i]:
+                next_smaller_element[stack1[-1]] = i
+                stack1.pop()   
+            stack1.append(i)
+
+            
+        stack2 = []
+        for i in range(n-1,-1,-1):
+            while len(stack2) and arr[stack2[-1]] >= arr[i]:
+                prev_smaller_element[stack2[-1]] = i
+                stack2.pop()
+            stack2.append(i)
+            
+        # print(prev_smaller_element)    
+        # print(next_smaller_element)
+        ans = 0
+        for i in range(n):
+            if prev_smaller_element[i]== -1:
+                left_diff = i - 0
+            else:
+                left_diff = (i - prev_smaller_element[i]) - 1
+                
+            if next_smaller_element[i]== -1:
+                right_diff = n-1 - i
+            else:
+                right_diff = (next_smaller_element[i] - i) - 1
+                
+            # print(m)
+            m = arr[i]* (left_diff +  right_diff+1)
+            ans = max(ans ,m)
+            
+        return ans
     
     
     
@@ -21,21 +45,28 @@ class Solution:
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
         if not matrix:
             return 0
-        h, w = len(matrix), len(matrix[0])
-        m = [[0]*w for _ in range(h)]
-        for j in range(h):
-            for i in range(w):
-                if matrix[j][i] == '1': 
-                    m[j][i] = m[j-1][i] + 1            #m[j][i]  not m[i][j]
-        for i in range(h):
-            for j in range(w):
-                print(m[i][j],end = " ")
-            print()
+        row , cols = len(matrix), len(matrix[0])
+        m = [[0]*cols for _ in range(row)]
+        
+        for i in range(cols):
+            m[0][i] = 1 if matrix[0][i]=='1' else 0
+            
+        
+        for i in range(1 , row):
+            for j in range(cols):
+                if matrix[i][j] == '1': 
+                    m[i][j] = m[i-1][j] + 1            
+        
+        # for i in range(row):
+        #     for j in range(cols):
+        #         print(m[i][j],end = " ")
+        #     print()
+            
+            
         ma = 0
         for row in m:
-            print(row) #this row is itself a histogram
+            # print(row) #this row is itself a histogram
             x = self.largestRectangleArea(row)
             ma = max(ma,x)
-            print(x)
             
         return ma
