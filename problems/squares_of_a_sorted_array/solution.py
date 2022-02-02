5517026d0d0badb88d399afbccbc110615c3f60c
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-02-02 07:10:16 +0000

    Sync LeetCode submission - Squares of a Sorted Array (python3)

--- problems/squares_of_a_sorted_array/solution.py
+++ problems/squares_of_a_sorted_array/solution.py
@@ -1,4 +1,48 @@
+# one thing to notice here is that array can have multiple occurence of any element
+
+# O(nlogn)
+'''
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        p = [i*i for i in sorted(nums, key = lambda x: abs(x))]
-        return p
+        ans = []
+        for i in nums:
+            ans.append(i*i)
+        return sorted(ans)
+'''
+    
+    
+# O(n): counting sort , keeping track of multiple occrence , 2pass
+'''
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        d = [0]*10001
+        for i in nums:
+            d[abs(i)]+=1
+        
+        ans = []
+        for i in range(10001):
+            while d[i]>0:
+                ans.append(i*i)
+                d[i]-=1
+        return ans
+'''
+# O(n) 2 pointer, one pass
+
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        i = 0
+        j = n - 1
+        ans = [0]*n
+        
+        while (n-1)>=0:
+            if nums[i]*nums[i] > nums[j]*nums[j]:
+                ans[n-1] = nums[i]*nums[i]
+                n = n - 1
+                i = i +1
+            else:
+                ans[n-1] = nums[j]*nums[j]
+                n = n - 1
+                j = j - 1
+        return ans
