3603dc1b5de812f8c57412e4df01e431d047d7bb
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-02-03 04:36:12 +0000

    Sync LeetCode submission - 4Sum II (python3)

--- problems/4sum_ii/solution.py
+++ problems/4sum_ii/solution.py
@@ -1,12 +1,5 @@
 class Solution(object):
     def fourSumCount(self, A, B, C, D):
-        """
-        :type A: List[int]
-        :type B: List[int]
-        :type C: List[int]
-        :type D: List[int]
-        :rtype: int
-        """
         hashtable = {}
         for a in A:
             for b in B :
