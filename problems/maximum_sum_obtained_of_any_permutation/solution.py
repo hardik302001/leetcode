6d433a6fa12f00a766a05d19eb0867e4b075fe16
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-02-04 10:50:31 +0000

    Sync LeetCode submission - Maximum Sum Obtained of Any Permutation (python3)

--- problems/maximum_sum_obtained_of_any_permutation/solution.py
+++ problems/maximum_sum_obtained_of_any_permutation/solution.py
@@ -5,9 +5,9 @@ class Solution:
         for start, end in requests: # traverse the interval will be time-consuming
             overlap[start]+=1
             overlap[end+1]-=1
-        for i in range(len(overlap)): # we do it only once 
-            cum += overlap[i]
-            overlap[i] = cum
+        for i in range(1, len(overlap)): # we do it only once 
+            overlap[i] = overlap[i-1] + overlap[i]
+        # print(overlap)
         nums.sort() # sort the index and nums 
         overlap.sort()
         ans = sum([nums[i]*overlap[i+1] for i in range(len(nums))]) # multiply by index
