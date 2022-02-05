ef7e744faf83c8b089f2ca570800c09638ca4331
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-02-05 07:17:45 +0000

    Sync LeetCode submission - Merge k Sorted Lists (cpp)

--- problems/merge_k_sorted_lists/solution.cpp
+++ problems/merge_k_sorted_lists/solution.cpp
@@ -1,7 +1,106 @@
 
 
-//see prev soln also
+/*
+take starting 2 lists from vector..merge them and push at end ...
 
+do thsi until you have only on list left in the vector
+ tc : O(nlogk)
+ sc: O(1)
+
+
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists){
+        if(lists.size()==0){
+            return NULL;
+        }
+        
+        while(lists.size()>1){
+            ListNode* combine = merge2Lists(lists[lists.size() -1], lists[lists.size() - 2]);
+            lists.pop_back();
+            lists.pop_back();
+            lists.push_back(combine);
+        }
+        
+        return lists[0];
+    }
+    
+    
+    ListNode* merge2Lists(ListNode* l1 , ListNode* l2){
+        if(l1==NULL){
+            return l2;
+        }
+        
+        if(l2==NULL){
+            return l1;
+        }
+        
+        ListNode* c = NULL;
+        if(l1->val>l2->val){
+            c = l2;
+            c->next = merge2Lists(l1, l2->next);
+        }
+        else{
+            c = l1;
+            c->next = merge2Lists(l1->next, l2);
+        }
+        return c;
+    }
+    
+    
+};
+
+
+
+
+
+// check my c++ solution, without using push_back, erase, which is time consuming,
+// use only 34ms
+/*
+class Solution {
+public:
+    ListNode *mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (NULL == l1) return l2;
+        else if (NULL == l2) return l1;
+        if (l1->val <= l2->val) {
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        }
+        else {
+            l2->next = mergeTwoLists(l1, l2->next);
+            return l2;
+        }
+    }
+    ListNode *mergeKLists(vector<ListNode *> &lists) {
+        if (lists.empty()) return NULL;
+        int len = lists.size();
+        while (len > 1) {
+            for (int i = 0; i < len / 2; ++i) {
+                lists[i] = mergeTwoLists(lists[i], lists[len - 1 - i]);
+            }
+            len = (len + 1) / 2;
+        }
+        
+        return lists.front();
+    }
+};
+
+*/
+
+
+
+/*
 
 
 class Solution {
@@ -37,3 +136,5 @@ public:
         return result;
     }
 };
+
+*/
