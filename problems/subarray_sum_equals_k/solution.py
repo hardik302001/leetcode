33ac634895aa4acf0acf5f865cd0a9e86f4fa0a9
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-02-10 02:33:51 +0000

    Sync LeetCode submission - Subarray Sum Equals K (python3)

--- problems/subarray_sum_equals_k/solution.py
+++ problems/subarray_sum_equals_k/solution.py
@@ -11,10 +11,19 @@ Single scan. Given the current sum and the k, we check if (sum-k) existsed befor
 '''
 also see: https://www.tutorialspoint.com/maximum-size-subarray-sum-equals-k-in-cplusplus
 also see: https://leetcode.com/problems/subarray-sums-divisible-by-k/
+also see: https://leetcode.com/problems/continuous-subarray-sum/
 '''
 
 #for more context search subarray sum equals k on yputube and watch pepcoding videos
 #also see : https://leetcode.com/problems/subarray-sums-divisible-by-k/
+
+
+
+#here u want count, so d[0] = 1, the count of such subarrays
+#if u wanted length as in https://www.tutorialspoint.com/maximum-size-subarray-sum-equals-k-in-cplusplus
+#we say d[0] = -1, for index and update d[key] = i (index), rather we do d[i]+=1 in counting subarrays
+
+
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         d = {}
