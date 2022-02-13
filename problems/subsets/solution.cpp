b7dd24f478a4ee8cc5bab351b549568ce61513c6
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-02-13 15:04:31 +0000

    Sync LeetCode submission - Subsets (cpp)

--- problems/subsets/solution.cpp
+++ problems/subsets/solution.cpp
@@ -1,5 +1,4 @@
 //bitmanipulation
-//see prev submission also
 
 class Solution {
 public:
@@ -10,9 +9,50 @@ public:
             for (int j = 0; j < n; j++) {
                 if ((i) & (1<<j)) {
                     subs[i].push_back(nums[j]);
+                    
                 }
             }
         }
         return subs;
     }
 };
+
+/*
+Time complexity: O(n * 2^N)  to generate all subsets and then copy them into output list.
+
+Space complexity:  O(n * 2^N) to keep all the subsets of length NN, since each of NN elements could be present or absent.
+*/
+
+// ---------------------------------------------------------------------------------------------------
+
+// Iterative
+
+// Using [1, 2, 3] as an example, the iterative process is like:
+
+// Initially, one empty subset [[]]
+// Adding 1 to []: [[], [1]];
+// Adding 2 to [] and [1]: [[], [1], [2], [1, 2]];
+// Adding 3 to [], [1], [2] and [1, 2]: [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]].
+
+/*
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> subs = {{}};
+        for (int num : nums) {
+            int n = subs.size();
+            for (int i = 0; i < n; i++) {
+                subs.push_back(subs[i]); 
+                subs.back().push_back(num);
+            }
+        }
+        return subs;
+    }
+};
+*/
+
+/*
+Time complexity: O(n * 2^N)  to generate all subsets and then copy them into output list.
+
+Space complexity:  O(n * 2^N) to keep all the subsets of length NN, since each of NN elements could be present or absent.
+*/
