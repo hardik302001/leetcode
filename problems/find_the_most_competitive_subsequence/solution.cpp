e2703346967300c8a6bdf2cdf6f80f603547791e
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-02-18 05:22:50 +0000

    Sync LeetCode submission - Find the Most Competitive Subsequence (cpp)

--- problems/find_the_most_competitive_subsequence/solution.cpp
+++ problems/find_the_most_competitive_subsequence/solution.cpp
@@ -1,14 +1,40 @@
-// also see python solution
+// 1st approach!
 // size k = maintain size k
+// nse 
+
+class Solution {
+public:
+    vector<int> mostCompetitive(vector<int>& nums, int k) {
+        int n = nums.size();
+        stack<int> st;
+        for(int i = 0;i<n ; i++){
+            while(!st.empty() and st.top()>nums[i] and (n-i + st.size())>k){
+                st.pop();
+            }
+            if(st.size()<k){
+                st.push(nums[i]);
+            }
+        }
+        
+        vector<int>ans;
+        while(!st.empty()){
+            int p = st.top();
+            ans.push_back(st.top());
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
 
 
-
-
-
+// 2nd approach
+// cpp soln
 // size k == removing (n-k) elements
 // very similar to : https://leetcode.com/problems/remove-k-digits/submissions/            cpp solution
-
-
+// https://leetcode.com/problems/find-the-most-competitive-subsequence/
+// nse
+/*
 class Solution {
 public:
     vector<int> mostCompetitive(vector<int>& nums, int k) {
@@ -39,3 +65,5 @@ public:
         
     }
 };
+
+*/
