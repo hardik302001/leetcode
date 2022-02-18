cd408f58df5cfdc380b111328b2e24a4b82a0c3d
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-02-18 05:36:27 +0000

    Sync LeetCode submission - Remove K Digits (cpp)

--- problems/remove_k_digits/solution.cpp
+++ problems/remove_k_digits/solution.cpp
@@ -1,4 +1,4 @@
-
+/*
 
 // SEE PYTHON SOLUTION 
 // THAT IS MUCH BETTER, there we keep (n-k) elements
@@ -6,25 +6,81 @@
 // similar to https://leetcode.com/problems/find-the-most-competitive-subsequence/submissions/
 
 
+// 1st approach!
+// remove x elements == maintain (n-x) elements
+// nse 
 
-// here we try to remove only k elements, we aim to remove k in beginning only, bcz we want lexographically smallest string
-//this is also nice code
 class Solution {
 public:
-    string removeKdigits(string num, int k) {
-       string ans = "";                                         // treat ans as a stack in below for loop
-       
-       for (char c : num) {
-           while (ans.length() && ans.back() > c && k) {
-               ans.pop_back();                                  // make sure digits in ans are in ascending order
-               k--;                                             // remove one char
-           }
-           
-           if (ans.length() || c != '0') { ans.push_back(c); }  // can't have leading '0', beautiful logic to find it! if ans is empty and  c=='0', then it wont push..else it would push irrespevtiv eof value of c
+    string removeKdigits(string s, int remove) {
+        int n = s.size();
+        int k = n - remove;
+        stack<int> st;
+        for(int i = 0;i<n ; i++){
+            while(!st.empty() and st.top()>s[i] and (n-i + st.size())>k){
+                st.pop();
+            }
+            if(st.size()<k){
+                st.push(s[i]);
+            }
         }
-       
-       while (ans.length() && k--) { ans.pop_back(); }          // make sure remove k digits in total
-       
-       return ans.empty() ? "0" : ans;
+        
+        string ans;
+        while(!st.empty()){
+            int p = st.top();
+            ans.push_back(st.top());
+            st.pop();
+        }
+        while(ans.size() and ans.back() == '0'){
+            ans.pop_back();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans.size()?ans:"0";
     }
 };
+
+*/
+
+
+
+// 2nd approach
+// cpp soln
+// here we try to remove only k elements, we aim to remove k in beginning only, bcz we want lexographically smallest string
+// size k == removing (n-k) elements
+// very similar to : https://leetcode.com/problems/remove-k-digits/submissions/            cpp solution
+// https://leetcode.com/problems/find-the-most-competitive-subsequence/
+
+
+class Solution {
+public:
+    string removeKdigits(string s, int remove) {
+        int n = s.size();
+        stack<int> st;
+        for (auto &it :s ){
+            while(!st.empty() and remove and st.top() > it ){
+                st.pop();
+                remove--;
+            }
+            st.push(it);
+        }
+        
+        
+        while(remove--){
+            st.pop();
+        }
+        
+        string ans;
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
+        }
+        while(ans.size() and ans.back() == '0'){
+            ans.pop_back();
+        }
+        
+        reverse(ans.begin(),ans.end());
+        return ans.size()?ans:"0";
+        
+    }
+};
+
