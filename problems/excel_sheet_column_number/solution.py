d146b3113e0354d35bc2cc8e223e75ab5aaeb501
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-02-22 03:30:26 +0000

    Sync LeetCode submission - Excel Sheet Column Number (python3)

--- problems/excel_sheet_column_number/solution.py
+++ problems/excel_sheet_column_number/solution.py
@@ -1,3 +1,4 @@
+'''
 import string
 class Solution:
     def titleToNumber(self, col: str) -> int:
@@ -6,4 +7,13 @@ class Solution:
         ans = 0
         for i in range(len(col)):
             ans = ans + pow(26,i)*(alp.index(col[i])+1)
+        return ans
+'''    
+
+class Solution:
+    def titleToNumber(self, col: str) -> int:
+        alp = string.ascii_uppercase
+        ans = 0
+        for i in col:
+            ans = 26 * ans + alp.index(i)+1
         return ans
