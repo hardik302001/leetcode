015d7059723f4c629a34764c8a5265d018d973d6
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-02-24 04:09:24 +0000

    Sync LeetCode submission - Design Twitter (cpp)

---
+++ problems/design_twitter/solution.cpp
@@ -0,0 +1,58 @@
+class Twitter {
+public:
+    unordered_map<int , unordered_set<int>>following;
+    priority_queue<vector<int>>pq;
+    int t;
+    Twitter() {
+        t = 0;   // sorting on basis of time.
+    }
+    
+    void postTweet(int userId, int tweetId) {
+        pq.push({t , userId , tweetId});
+        t++;
+    }
+    
+    vector<int> getNewsFeed(int userId) {   
+        vector<int>ans;
+        priority_queue<vector<int>>tmp;    // to keep track of removed posts
+        while(pq.size()){
+            auto top = pq.top();
+            if(top[1]==userId){   // user will see his post
+                ans.push_back(top[2]);
+            }else if (following[userId].find(top[1])!=following[userId].end()){   // or the posts of person he follows
+                ans.push_back(top[2]);
+            }
+            
+            pq.pop();
+            tmp.push(top);
+            if(ans.size()==10){   // at max 10 posts
+                break;
+            }
+        }
+        
+        while(tmp.size()){    // add all removed posts back to pq!
+            auto top = tmp.top();
+            tmp.pop();
+            pq.push(top);
+        }
+        
+        return ans;
+    }
+    
+    void follow(int followerId, int followeeId) {
+        following[followerId].insert(followeeId);
+    }
+    
+    void unfollow(int followerId, int followeeId) {
+        following[followerId].erase(followeeId);
+    }
+};
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * Twitter* obj = new Twitter();
+ * obj->postTweet(userId,tweetId);
+ * vector<int> param_2 = obj->getNewsFeed(userId);
+ * obj->follow(followerId,followeeId);
+ * obj->unfollow(followerId,followeeId);
+ */
