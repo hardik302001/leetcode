e6e893e3a45e9da03c5012ad909ed8c37b729465
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-02-25 14:12:22 +0000

    Sync LeetCode submission - Queries on Number of Points Inside a Circle (cpp)

---
+++ problems/queries_on_number_of_points_inside_a_circle/solution.cpp
@@ -0,0 +1,15 @@
+// O(n^2)
+
+class Solution {
+public:
+    vector<int> countPoints(vector<vector<int>>& points, vector<vector<int>>& queries) {
+        vector<int> res;
+        for (auto &q : queries) {
+            int cnt = 0, rr = q[2] * q[2];
+            for (auto &p : points)
+                cnt += (q[0] - p[0]) * (q[0] - p[0]) + (q[1] - p[1]) * (q[1] - p[1]) <= rr;
+            res.push_back(cnt);
+        }
+        return res;
+    }
+};
