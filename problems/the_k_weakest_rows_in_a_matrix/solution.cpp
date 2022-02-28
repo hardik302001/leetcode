2a2ab225ed51d789caa2377cf0825307401656c1
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-02-28 14:02:00 +0000

    Sync LeetCode submission - The K Weakest Rows in a Matrix (cpp)

---
+++ problems/the_k_weakest_rows_in_a_matrix/solution.cpp
@@ -0,0 +1,26 @@
+
+class Solution {
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        vector<int> res;
+        
+        vector<pair<int,int>> vp;
+        
+        for(int i=0;i<mat.size();i++){
+            int count = 0;
+            for(int j=0;j<mat[i].size();j++){
+                if(mat[i][j] == 1)
+                    ++count;
+                        
+            }
+            vp.push_back(make_pair(count,i));
+        }
+        sort(vp.begin(),vp.end());
+         for(int i=0;i<k;i++){
+              res.push_back(vp[i].second);  
+         }
+        
+        return res;
+        
+    }
+};
