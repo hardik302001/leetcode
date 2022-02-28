f65f398a3b1e43c2df3c9ff13367fb7d60baf224
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-02-28 06:53:16 +0000

    Sync LeetCode submission - Maximum Running Time of N Computers (cpp)

--- problems/maximum_running_time_of_n_computers/solution.cpp
+++ problems/maximum_running_time_of_n_computers/solution.cpp
@@ -3,6 +3,7 @@
 // same as : https://leetcode.com/problems/minimum-number-of-days-to-make-m-bouquets/
 
 // https://leetcode.com/problems/maximum-running-time-of-n-computers/discuss/1692979/Java-Simple-Solution-w.-Explanation
+
 /*
 Observations
 
@@ -17,37 +18,63 @@ The running time would be O(m \lg n) where m = sum(batteries[i]) and n = batteri
 
 
 */
+
+
+// we want maximum here..
+/*
+
+# minimise maximum, so we return do hi = mid for valid case, and return low
+
+# if it was maximise minimum, then we will do li = mid +1 for valid case and return li - 1
+# keep something extra in r...bcz (l<r) and we are returning l-1, so we wont chcek ever on r, so keep extra
+#                        OR
+# keep a speerate variable ans , and equate ans to the value, whenever we get valid result for answer from function
+# see: https://leetcode.com/problems/maximum-running-time-of-n-computers/submissions/
+
+*/
+
 class Solution {
 public:
-    bool fun(vector<int>& a, long long x, long long k){
-        long long val = x*k;
+    bool fun(vector<int>& a, long long n, long long k){
+        long long val = n*k;
         long long curbatsum = 0;
         for(int i=0; i<a.size(); i++){
             curbatsum += min((long long)a[i],k);
         }
+        // why we are doing min here ??
+        /*
+         I was also confused why we are only considering k when we have bat>=k.
+The idea is that,
+
+Yeah, we would greedily want to use the left battery units (i.e. a[i] -k) to other pc, where battery is less than k.
+But one thing to notice here is that we can help others pc, only when we are done with our work. That is only when, we have used this current battery for k minutes on our current assigned pc.
+So , we would help other pc with our extra battery units ( a[i] - k) after k minutes. But , but , but, after k minutes our experiment is over. So, we cant use more than k units of battery where a[i]>=k.
+
+For bat<k, we can only use a[i] units of battery.
+        
+        */
+        
+        
         return val <= curbatsum;
     }
+    
+    
     long long maxRunTime(int n, vector<int>& a) {
         long long sum = 0;
         for(auto i : a){
             sum += i;
         }
-        long long ans = 0;
-        long long l = 0, r = sum;
-        
-        // we need maximum so we start from r!
-        if(fun(a, n , r)) return r;    // bcz while (l<r) will not check for r, (bcz when l = mid +1 [mid+1==r], then it will be l==r, so it will break!)
-        
+        long long l = 0, r = sum+1;  // always keep something extra in r, bcz we are considering l<r, so we will never see R
+
         while(l < r){
             long long mid = l + (r-l)/2;
             if(fun(a,n,mid)){
-                ans = mid;
                 l = mid + 1;
             }
             else{
                 r = mid;
             }
         }
-        return ans;
+        return l-1;
     }
 };
