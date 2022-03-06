ce192cefa4288086e591b0b84d43265ad6c9f807
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-03-06 13:08:43 +0000

    Sync LeetCode submission - All Nodes Distance K in Binary Tree (cpp)

---
+++ problems/all_nodes_distance_k_in_binary_tree/solution.cpp
@@ -0,0 +1,126 @@
+/*
+// BFS APPROACH : 
+// find parents and and left and right , and then traverse in bfs manner to find kth distannce nodes
+class Solution {
+public:
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int K) {
+        unordered_map<TreeNode*, TreeNode*> parent_track; // node -> parent
+        unordered_map<TreeNode*, bool> visited;
+        queue<TreeNode*> queue;
+        queue.push(root);
+        while(!queue.empty()) {   //First BFS to get a track of parent nodes
+            TreeNode* current = queue.front(); queue.pop();
+            if(current->left) {
+                parent_track[current->left] = current;
+                queue.push(current->left);
+            }
+            if(current->right) {
+                parent_track[current->right] = current;
+                queue.push(current->right);
+            }
+        }
+        
+        
+        queue.push(target);
+        visited[target] = true;
+        int curr_level = 0;
+        while(!queue.empty()) {   //Second BFS to go upto K level from target node and using our hashtable info
+            int size = queue.size();
+            if(curr_level == K) break;
+            while(size--) {
+                TreeNode* current = queue.front(); queue.pop();
+                if(current->left && !visited[current->left]) {   // left
+                    queue.push(current->left);
+                    visited[current->left] = true;
+                }
+                if(current->right && !visited[current->right]) {  // right
+                    queue.push(current->right);
+                    visited[current->right] = true;
+                }
+                if(parent_track[current] && !visited[parent_track[current]]) {   // parent
+                    queue.push(parent_track[current]);
+                    visited[parent_track[current]] = true;
+                }
+            }
+            curr_level++;
+        }
+        
+        vector<int> result;
+        while(!queue.empty()) {
+            TreeNode* current = queue.front(); queue.pop();
+            result.push_back(current->val);
+        }
+        return result;
+    }
+
+};
+
+*/
+
+
+// DFS
+
+class Solution {
+public:
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        unordered_map<TreeNode*, TreeNode*> parent_track; // node -> parent
+        unordered_map<TreeNode*, bool> visited;
+        queue<TreeNode*> queue;
+        queue.push(root);
+        while(!queue.empty()) { // First BFS to get a track of parent nodes 
+            TreeNode* current = queue.front(); queue.pop();
+            if(current->left) {
+                parent_track[current->left] = current;
+                queue.push(current->left);
+            }
+            if(current->right) {
+                parent_track[current->right] = current;
+                queue.push(current->right);
+            }
+        }
+        
+        
+        vector<int>ans;
+        visited[target] = true;
+        dfs(target , k, ans, parent_track , visited);
+        
+        
+        return ans;   
+    }
+    
+    void dfs(TreeNode* cur , int k , vector<int>&ans , unordered_map<TreeNode*, TreeNode*> &parent_track, unordered_map<TreeNode*, bool> &visited){
+        if(cur==NULL){
+            return;
+        }
+        if(k==0){
+            ans.push_back(cur->val);
+            return;
+        }
+        
+        if(cur->left){
+            if(!visited[cur->left]){
+                visited[cur->left] = true;
+                dfs(cur->left , k-1 , ans , parent_track , visited);
+                
+            }
+        }
+                
+        if(cur->right){
+            if(!visited[cur->right]){
+                visited[cur->right] = true;
+                dfs(cur->right , k-1 , ans , parent_track , visited);
+                
+            }
+        }
+        
+                
+        if(parent_track[cur]){
+            if(!visited[parent_track[cur]]){
+                visited[parent_track[cur]] = true;
+                dfs(parent_track[cur] , k-1 , ans , parent_track , visited);
+                
+            }
+        }
+    }
+    
+};
