eab848560b38f30b189fcc1f898e7ec2dcd98497
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-03-14 16:52:24 +0000

    Sync LeetCode submission - Fair Candy Swap (python3)

---
+++ problems/fair_candy_swap/solution.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/fair-candy-swap/solution/
+
+class Solution(object):
+    def fairCandySwap(self, A, B):
+        Sa, Sb = sum(A), sum(B)
+        setB = set(B)
+        for x in A:
+            if x + (Sb - Sa) / 2 in setB:
+                return [x, x + (Sb - Sa) / 2]
