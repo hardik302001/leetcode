1eabcbb0985596adfdb77f868dd20f6e9f2b9dd1
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-03-17 08:41:31 +0000

    Sync LeetCode submission - Score of Parentheses (cpp)

---
+++ problems/score_of_parentheses/solution.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/score-of-parentheses/discuss/1856519/JavaC%2B%2B-Visually-Explained!!
+
+class Solution {
+public:
+    int scoreOfParentheses(string s) {
+        stack<int> st;
+        int score = 0;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == '('){
+                st.push(score);
+                score = 0;
+            }
+            else {
+                score = st.top() + max(2 * score, 1);
+                st.pop();
+            }
+        }
+        return score;
+    }
+};
