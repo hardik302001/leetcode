75e28c1767e4fb7447146a04be632bfae4fce681
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-03-29 06:13:29 +0000

    Sync LeetCode submission - Letter Combinations of a Phone Number (cpp)

--- problems/letter_combinations_of_a_phone_number/solution.cpp
+++ problems/letter_combinations_of_a_phone_number/solution.cpp
@@ -12,7 +12,7 @@ vector<string> letterCombinations(string digits) {
         return res;
     }
     
-    void backtracking(vector<vector<char>>& table, vector<string>& res, string& local, int index, string& digits){
+    void backtracking(vector<vector<char>>& table, vector<string>& res, string &local, int index, string& digits){
         int digit = digits[index] - '0';
 
         if(index == digits.size())
@@ -26,3 +26,46 @@ vector<string> letterCombinations(string digits) {
     }
 
 };
+
+
+
+
+
+
+/*
+
+
+
+class Solution {
+public:
+    
+
+vector<string> letterCombinations(string digits) {
+        vector<string> res;
+        if(digits.size() == 0) return res;
+        string local;
+        vector<vector<char>> table{{'0'}, {'1'}, {'a','b','c'}, {'d','e','f'}, {'g','h','i'}, {'j','k','l'}, {'m','n','o'}, {'p','q','r','s'}, {'t','u','v'}, {'w','x','y','z'}};
+        
+        backtracking(table, res, local, 0, digits);
+        return res;
+    }
+    
+    void backtracking(vector<vector<char>>& table, vector<string>& res, string local, int index, string& digits){
+        int digit = digits[index] - '0';
+
+        if(index == digits.size())
+            res.push_back(local);
+        else
+            for(int i = 0; i < table[digit].size(); i++){
+                // local.push_back(table[digit][i]);
+                backtracking(table, res, local + table[digit][i], index+1, digits);
+                // local.pop_back();
+            }
+    }
+
+};
+
+
+
+
+*/
