2a201fc14a8b91ed28ea400875c4f99dad5f1dbb
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-04-06 05:14:23 +0000

    Sync LeetCode submission - 3Sum (python3)

--- problems/3sum/solution.py
+++ problems/3sum/solution.py
@@ -1,3 +1,6 @@
+# https://leetcode.com/problems/3sum-with-multiplicity/
+
+
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         k=len(nums)
@@ -7,13 +10,18 @@ class Solution:
     
         target = 0
         for i in range(0,k-2):
+            if(nums[i] > 0):
+                break
             start=i+1
             end=k-1
             
             while start < end:
-                cur = nums[i]
-                cur = cur + nums[start] + nums[end]
+                cur = nums[i] + nums[start] + nums[end]
                 if cur==target and [nums[i],nums[start],nums[end]] not in res: 
+                    while start < end and nums[start] == nums[start+1]:
+                        start += 1
+                    while start < end and nums[end] == nums[end-1]:
+                        end -= 1
                     res.append([nums[i],nums[start],nums[end]])
                     start+=1
                     end-=1
