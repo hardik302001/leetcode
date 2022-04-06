b52a27913f1c86086a32f19567f6034f19e7e36c
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-04-06 21:25:06 +0000

    Sync LeetCode submission - Combination Sum III (cpp)

---
+++ problems/combination_sum_iii/solution.cpp
@@ -0,0 +1,78 @@
+// backtracking
+
+class Solution {
+public:
+    vector<vector<int>>ans;
+    
+    void recur(int cur , int n , int k , vector<int>&temp){
+        if(k<0) return;
+        if(n<0) return;
+
+        if(k==0 and n==0){
+            ans.push_back(temp);
+            return;
+        }
+        if(cur>9) return;
+        
+        // consider current and move to next
+        temp.push_back(cur);
+        recur(cur+1 , n-cur , k-1, temp);
+        temp.pop_back();
+            
+        // ignore current and move to next        
+        recur(cur+1 , n , k, temp);
+                    
+        
+    }
+    
+    
+    vector<vector<int>> combinationSum3(int k, int n) {
+        if(n<k*(k+1)/2){return {};}
+ 
+        if (((9)*(10)/2 - (9-k)*(9-k+1)/2 ) <n){
+            return {};
+        }
+        vector<int>temp;
+        recur(1 , n , k , temp);
+            
+        return ans;
+    }
+};
+
+
+// similar soln
+/*
+class Solution {
+public:
+    vector<vector<int>>ans;
+    
+    void recur(int cur , int n , int k , vector<int>&temp){
+        if(k<0) return;
+        if(n<0) return;
+        if(k==0 and n==0){
+            ans.push_back(temp);
+            return;
+        }
+        for(int i = cur; i<10 ;i++ ){
+            temp.push_back(i);
+            recur(i+1 , n-i , k-1, temp);
+            temp.pop_back();
+            
+        }        
+        
+    }
+    
+    
+    vector<vector<int>> combinationSum3(int k, int n) {
+        if(n<k*(k+1)/2){return {};}
+ 
+        if (((9)*(10)/2 - (9-k)*(9-k+1)/2 ) <n){
+            return {};
+        }
+        vector<int>temp;
+         recur(1 , n , k , temp);
+            
+        return ans;
+    }
+};
+*/
