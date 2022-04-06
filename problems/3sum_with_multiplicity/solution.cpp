149f71ad213d74b9a0ecd2561d0ccf65e07745c5
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-04-06 05:22:10 +0000

    Sync LeetCode submission - 3Sum With Multiplicity (cpp)

---
+++ problems/3sum_with_multiplicity/solution.cpp
@@ -0,0 +1,17 @@
+// Use a map to count all sums of two numbers.
+// Then, if target - arr[i] is in map, it means we have a three-sum, so we add it to the result.
+
+class Solution {
+public:
+    int threeSumMulti(vector<int>& arr, int target) {
+        unordered_map<int, int> m;
+        int res = 0, mod = 1e9 + 7;
+        for (int i = 0; i < arr.size(); i++) {
+            res = (res + m[target - arr[i]]) % mod;
+            for (int j = 0; j < i; j++) {
+                m[arr[i] + arr[j]]++;
+            }
+        }
+        return res;
+    }
+};
