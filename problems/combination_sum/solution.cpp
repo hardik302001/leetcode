95850971a944e8867c1f7d403297c0e8ae304e5f
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-04-06 21:26:25 +0000

    Sync LeetCode submission - Combination Sum (cpp)

--- problems/combination_sum/solution.cpp
+++ problems/combination_sum/solution.cpp
@@ -1,36 +1,49 @@
-// BBACKTRACKING
+// BACK TRACKING
+// https://leetcode.com/submissions/detail/643040377/
+
+// see prev submissions also
 
 class Solution {
 public:
     
-    vector<vector<int>>ans;
-    vector<vector<int>> combinationSum(vector<int>& cand, int target) {
-        vector<int>temp = {};
-        recur(0 , target , temp , cand);
-        return ans;
-    }
-    
-    void recur(int ind , int target , vector<int> &temp , vector<int>&cand){
-        if(target <0 or ind==(cand.size())){
-            return;
-        }
-           
+    void recur(int i, int target , vector<int>&temp , vector<int>&cand, set<vector<int>>&ans){
+        if(target<0) return;
         if(target==0){
-            ans.push_back(temp);
+            ans.insert(temp);
             return;
         }
-           
-        // target is more than zero , so continue adding more elements
         
         
-        // choosing this current index element ( more than 1 times ) !
-        temp.push_back(cand[ind]);
-        recur(ind , target - cand[ind] , temp , cand);
-        temp.pop_back();
-           
-        // if we dont want to push current element, we go for next element !
-        recur(ind+1 , target, temp , cand);
+        if(i>=cand.size()) return;
+        
+        // conisider and go for next
+        
+            temp.push_back(cand[i]);
+            recur(i+1 , target-cand[i] , temp , cand , ans);
+            temp.pop_back();
+            
+        // consider and stay at current element 
+            
+            temp.push_back(cand[i]);
+            recur(i , target-cand[i] , temp , cand, ans);
+            temp.pop_back();
+            
+        // dont conisider and move for next
+            recur(i+1 , target , temp , cand, ans);
+            
+                    
         
     }
     
+    vector<vector<int>> combinationSum(vector<int>& cand, int target) {
+        set<vector<int>>ans;
+    
+        vector<int>temp;
+        recur(0 , target , temp , cand , ans);
+        vector<vector<int>> res;
+        for(auto v: ans){
+            res.push_back(v);
+        }
+        return res;
+    }
 };
