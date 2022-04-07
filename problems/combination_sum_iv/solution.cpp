5bc0ae2738dc306cb8dee35a482dff396146fa47
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-04-07 20:49:04 +0000

    Sync LeetCode submission - Combination Sum IV (cpp)

--- problems/combination_sum_iv/solution.cpp
+++ problems/combination_sum_iv/solution.cpp
@@ -1,8 +1,8 @@
 // same as coin combinations I cses
 //https://cses.fi/problemset/task/1635/
 
-
-
+// BOTTOM UP
+/*
 #define ll long long
 
 class Solution {
@@ -22,7 +22,7 @@ public:
           return dp[target];
     }
 };
-
+*/
 //follow up
 /*
 I think if there are negative numbers in the array, we must add a requirement that each number is only used one time, or either positive number or negative number should be used only one time, otherwise there would be infinite possible combinations.
@@ -31,3 +31,68 @@ For example, we are given:
 it's obvious to see as long as we choose n 1s and (n-1) -1s, it always sums up to 1, n can be any value >= 1.
 
 */
+
+// RECURSION (we dont need backtracking here)
+/*
+class Solution {
+public:
+    int ans = 0;
+    void recur(vector<int>& nums, int target){
+        if(target ==0){
+            ans++;
+            return;
+        }
+        
+        if(target<0){
+            return;
+        }
+        
+        for(int i = 0;i<nums.size();i++){
+            recur(nums , target - nums[i]);
+        }
+        
+    }
+    
+    long long int combinationSum4(vector<int>& nums, int target) {
+        recur(nums, target);
+        return ans;
+    }
+};
+
+
+*/
+
+// TOP DOWN  DP 
+
+class Solution {
+public:
+    int recur(vector<int>& nums, int target , vector<int>&dp){
+
+        
+        if(target == 0){
+            return 1;
+        }
+        
+        if(target<0){
+            return 0;
+        }
+        
+        if(dp[target]!=-1){
+            return dp[target];
+        }
+        
+        int res = 0;
+        for(int i = 0;i<nums.size();i++){
+            res+= recur(nums , target - nums[i], dp);
+        }
+        
+        return dp[target] = res;
+        
+    }
+    
+    int combinationSum4(vector<int>& nums, int target) {
+        
+        vector<int>dp(target + 1 , -1);
+        return recur(nums, target, dp);
+    }
+};
