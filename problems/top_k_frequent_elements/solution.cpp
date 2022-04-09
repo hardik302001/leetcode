d827f92695662619e46ec36cc9ee4b692a206a44
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-04-09 19:22:57 +0000

    Sync LeetCode submission - Top K Frequent Elements (cpp)

--- problems/top_k_frequent_elements/solution.cpp
+++ problems/top_k_frequent_elements/solution.cpp
@@ -4,47 +4,129 @@
 //similar to https://leetcode.com/problems/find-the-kth-largest-integer-in-the-array/
 
 
+//  ------------- MAX HEAP ---------------------------  O( N log n)
 
-class Comp {
-public:
-        bool operator()(const pair<int, int>& lhs, const pair<int, int>& rhs){
-              if(lhs.first<rhs.first){
-                    return true;
-                }
-                else if(rhs.first<lhs.first){
-                    return false;
-                }
-                else{
-                    if(lhs.second<rhs.second){
-                        return true;
-                    }
-                    else{
-                        return false;
-                    }
-                }
-        }
-};
+// class Comp {
+// public:
+//         bool operator()(pair<int, int>& lhs, pair<int, int>& rhs){
+//               if(lhs.first<rhs.first){    // true when a is smaller than b
+//                     return true;
+//                 }
+//                 else if(rhs.first<lhs.first){
+//                     return false;
+//                 }
+//                 else{
+//                     if(lhs.second<rhs.second){
+//                         return true;
+//                     }
+//                     else{
+//                         return false;
+//                     }
+//                 }
+//         }
+// };
 
 
+
+// class Solution {
+// public:
+//     vector<int> topKFrequent(vector<int>& nums, int k) {
+        
+//         unordered_map<int, int> cnt;
+//         for (auto& w : nums)
+//             ++cnt[w];
+        
+//         priority_queue<pair<int, int>, vector<pair<int, int>>, Comp> Q;  // max heap
+        
+//         for (auto it : cnt)
+//             Q.push({it.second, it.first});
+        
+//         vector<int> res;
+//         while (k-- and !Q.empty()) {
+//             res.push_back(Q.top().second);
+//             Q.pop();
+//         }
+        
+//         return res;
+//     }
+// };
+
+
+
+// ------------------- MIN HEAP --------------------    O(n logk)
+
+// class Comp {
+// public:
+//         bool operator()(pair<int, int>& lhs, pair<int, int>& rhs){
+//               if(lhs.first>rhs.first){    // true when a is greater than b
+//                     return true;
+//                 }
+//                 else if(rhs.first>lhs.first){
+//                     return false;
+//                 }
+//                 else{
+//                     if(lhs.second>rhs.second){
+//                         return true;
+//                     }
+//                     else{
+//                         return false;
+//                     }
+//                 }
+//         }
+// };
+
+
+
+// class Solution {
+// public:
+//     vector<int> topKFrequent(vector<int>& nums, int k) {
+        
+//         unordered_map<int, int> cnt;
+//         for (auto& w : nums)
+//             ++cnt[w];
+        
+//         priority_queue<pair<int, int>, vector<pair<int, int>>, Comp> Q;  // min heap
+        
+//         for (auto it : cnt){
+//             Q.push({it.second, it.first});
+//             if(Q.size()>k){  // keep only top k elements
+//                 Q.pop();
+//             }
+//         }
+        
+//         vector<int> res;
+//         while (!Q.empty()) {
+//             res.push_back(Q.top().second);
+//             Q.pop();
+//         }
+        
+//         return res;
+//     }
+// };
+
+
+
+// ------------------------- BUCKET SORT ----------------------------   O(n)
 
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        
-        unordered_map<int, int> cnt;
-        for (auto& w : nums)
-            ++cnt[w];
-        
-        priority_queue<pair<int, int>, vector<pair<int, int>>, Comp> Q;
-        for (auto it : cnt)
-            Q.emplace(it.second, it.first);
-        
         vector<int> res;
-        while (k-- > 0 && !Q.empty()) {
-            res.push_back(Q.top().second);
-            Q.pop();
+        if (!nums.size()) return res;
+        unordered_map<int, int> cnt;
+        for (auto num : nums) cnt[num]++;
+        vector<vector<int>> bucket(nums.size() + 1);
+        for (auto kv : cnt) {
+            bucket[kv.second].push_back(kv.first);
         }
-        
+
+        for (int i = bucket.size() - 1; i >= 0; --i) {
+            for (int j = 0; j < bucket[i].size(); ++j){
+                res.push_back(bucket[i][j]);
+                if (res.size() == k) return res;
+            }
+        }
+
         return res;
     }
 };
