16ddccd763f417a4a644e0eea335dd19d5577919
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-04-09 19:42:01 +0000

    Sync LeetCode submission - Backspace String Compare (cpp)

--- problems/backspace_string_compare/solution.cpp
+++ problems/backspace_string_compare/solution.cpp
@@ -1,46 +1,34 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        int k=0,p=0;
-        for(int i=0;i<s.size();i++)
-        {
-            if(s[i]=='#')
-            {
-                k--;
-                 k=max(0,k);
+        stack<char>s1, s2;
+        for(auto &i:s){
+            if(i=='#'){
+                if(!s1.empty()){
+                    s1.pop();
+                }
+            }else{
+                s1.push(i);
             }
-            
-           else
-           {
-               s[k]=s[i];
-               k++;
-           }
         }
-        for(int i=0;i<t.size();i++)
-        {
-            if(t[i]=='#')
-            {
-                p--;
-                 p=max(0,p);
+        for(auto &i:t){
+            if(i=='#'){
+                if(!s2.empty()){
+                    s2.pop();
+                }
+            }else{
+                s2.push(i);
             }
-            
-           else
-           {
-               t[p]=t[i];
-               p++;
-           }
         }
-        if(k!=p)
-            return false;
-        else
-        {
-            for(int i=0;i<k;i++)
-            {
-                if(s[i]!=t[i])
-                    return false;
-            }
-            return true;
+        if(s1.size()!=s2.size()) return false;
+        while(s1.size()){
+            auto x = s1.top();
+            s1.pop();
+            auto y = s2.top();
+            s2.pop();
+            // cout<<x<<" "<<y<<endl;
+            if(x!=y) return false;
         }
-        
+        return true;
     }
 };
