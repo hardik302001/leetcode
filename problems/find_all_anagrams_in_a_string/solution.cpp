9cace9bed98ef25bd0bcf50dd72b995bf7d6edc9
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-04-09 20:49:46 +0000

    Sync LeetCode submission - Find All Anagrams in a String (cpp)

--- problems/find_all_anagrams_in_a_string/solution.cpp
+++ problems/find_all_anagrams_in_a_string/solution.cpp
@@ -1,6 +1,15 @@
 //standard sliding window
 
 
+
+
+
+
+
+
+
+
+// O(26*len(s))
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) 
@@ -18,23 +27,24 @@ public:
         }
         
         int n=p.size();
-        int left=0;
-        for(int i=0;i<s.size();i++)
+        int l=0;
+        for(int r=0;r<s.size();r++)
         {
-            if(i<n-1){         //while size is less than size of susbtring
-                arr[s[i]-'a']++;
-            }
-            else
-            {
-                arr[s[i]-'a']++;       //now size is same, time to check if they are sam eor not!
-                if(arr==brr){
-                    v.push_back(i-n+1);   //if same then push the relative starting index of substring
-                } 
-                
-                arr[s[left]-'a']--;          //remove starting element of substring(now size of substring is one less than the                                                 //actual substring with which we will compare! )
-                left++;                      //move one posi ahead
+            
+            arr[s[r]-'a']++;       //now size is same, time to check if they are sam eor not!
+            
+            if(r-l+1>n){                             
+                arr[s[l]-'a']--;          //remove starting element of substring(now size of substring is one less than the actual substring with which we will compare! )
+                l++;                      //move one posi ahead
     
-            }        
+            }
+            
+            
+            if(arr==brr){
+                v.push_back(l);   //if same then push the relative starting index of substring                } 
+            }
+
+                  
         }
         return v;
     }
