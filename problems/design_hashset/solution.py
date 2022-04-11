c6d8b4ba20bccaf3101e7031b92ade38c6c67a70
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-04-11 18:35:26 +0000

    Sync LeetCode submission - Design HashSet (python3)

--- problems/design_hashset/solution.py
+++ problems/design_hashset/solution.py
@@ -1,27 +1,39 @@
+# USING SET
+
+
+# class MyHashSet:
+
+#     def __init__(self):
+#         """
+#         Initialize your data structure here.
+#         """
+#         self.s = set()
+        
+
+#     def add(self, key: int) -> None:
+#         self.s.add(key)
+
+#     def remove(self, key: int) -> None:
+#         self.s.discard(key)
+
+#     def contains(self, key: int) -> bool:
+#         """
+#         Returns true if this set contains the specified element
+#         """
+#         return (key in self.s)
+
+# using visited array
+
 class MyHashSet:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.s = set()
-        
+        self.arr = [False] * 1000001
 
-    def add(self, key: int) -> None:
-        self.s.add(key)
+    def add(self, key):
+        self.arr[key] = True
 
-    def remove(self, key: int) -> None:
-        self.s.discard(key)
+    def remove(self, key):
+        self.arr[key] = False
 
-    def contains(self, key: int) -> bool:
-        """
-        Returns true if this set contains the specified element
-        """
-        return (key in self.s)
-
-
-# Your MyHashSet object will be instantiated and called as such:
-# obj = MyHashSet()
-# obj.add(key)
-# obj.remove(key)
-# param_3 = obj.contains(key)
+    def contains(self, key):
+        return self.arr[key]
