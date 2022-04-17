e5d5b8aaa76e0edccb883e1998a22f0512bb8b2d
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-04-17 17:47:35 +0000

    Sync LeetCode submission - Two Sum II - Input Array Is Sorted (python3)

--- problems/two_sum_ii_-_input_array_is_sorted/solution.py
+++ problems/two_sum_ii_-_input_array_is_sorted/solution.py
@@ -1,18 +1,31 @@
 # previous code is better
 
+# MAP
+
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+#         ans = []
+#         h = {}
+#         for i in range(len(nums)):
+#             if (target - nums[i]) in h:
+#                 s,ss = i,h[target - nums[i]]
+#                 return [ss+1,s+1]
+#             else:
+#                 h[nums[i]] = i
+#             #print(h)
+        
+                
+# 2 PTR
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-
-        ans = []
-        h = {}
-        for i in range(len(nums)):
-            if (target - nums[i]) in h:
-                s,ss = i,h[target - nums[i]]
-                return [ss+1,s+1]
-            else:
-                h[nums[i]] = i
-            #print(h)
         
+        l, r = 0, len(nums) - 1
+        while l < r:
+            if nums[l] + nums[r] == target: return (l + 1,  r + 1)
+            if nums[l] + nums[r] > target: r -= 1
+            else: l += 1
                 
-        
+                
+# BINARY SEARCH CAN ALSO BE USED,
