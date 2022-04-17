8d6851a08ba484134043b58d41853021b982012d
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-04-17 17:38:40 +0000

    Sync LeetCode submission - Minimum Rounds to Complete All Tasks (python3)

--- problems/minimum_rounds_to_complete_all_tasks/solution.py
+++ problems/minimum_rounds_to_complete_all_tasks/solution.py
@@ -1,3 +1,5 @@
+# Intuition : Every number is a sum of 2s and 3s, except 1.
+
 class Solution:
     def minimumRounds(self, tasks: List[int]) -> int:
         d = Counter()
@@ -7,24 +9,13 @@ class Solution:
         c = 0
         for xx in d:
             i = d[xx]
-            # print(i)
             if i==1:
                 return -1
-            elif i==2:
-                c+=1
-            elif i==3:
-                c+=1
             else:
-                ma = 100000000000
-                for x in range(0 , i//3+1):
-                    p = i - 3*x
-                    if p%2==0:
-                        ma = min(ma , x + p//2)
-                
-                if ma==100000000000:
-                    return -1
+                if i%3==0:
+                    c+= (i)//3
                 else:
-                    c+=ma
+                    c+=(1+i//3)
             
         return c
                 
