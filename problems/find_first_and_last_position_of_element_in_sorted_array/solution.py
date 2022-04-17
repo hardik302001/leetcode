4040c90902174350a973efc5f4ee91a274931f26
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-04-17 18:04:55 +0000

    Sync LeetCode submission - Find First and Last Position of Element in Sorted Array (python3)

--- problems/find_first_and_last_position_of_element_in_sorted_array/solution.py
+++ problems/find_first_and_last_position_of_element_in_sorted_array/solution.py
@@ -1,34 +1,27 @@
 class Solution(object): 
-
-#### Main function
-
     def searchRange(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
         result = [-1, -1] #1
+        if len(nums)==0: return result
         
         result[0] = self.findStartingIndex(nums, target)  #2 
         result[1] = self.findEndingIndex(nums, target)  #3
-        
+        if len(nums)==1: return [-1, -1] if nums[0]!=target else [0,0]
         return result #4
 		
 #### Helper function 1 
 
     def findStartingIndex(self, nums, target):
         index = -1 #5 
-        low, high = 0, len(nums) -1 #6
-        
-        while low <= high: #7
+        low, high = 0, len(nums) #6
+        if nums[low]==target: return low
+        while low < high: #7
             mid = low + (high - low)//2 #8
 			
             if nums[mid] == target: #9
                 index = mid #10
-                high = mid - 1 #11
+                high = mid #11
             elif nums[mid] > target:  #12
-                high = mid - 1 #13
+                high = mid #13
             else:  #14
                 low = mid + 1 #15       
         
@@ -38,20 +31,16 @@ class Solution(object):
   
     def findEndingIndex(self, nums, target):
         index = -1
-        low, high = 0, len(nums) -1
-        
-        while low <= high:
-            
+        low, high = 0, len(nums)
+        if nums[high-1]==target: return high-1
+        while low < high:
             mid = low + (high - low)//2
-            
             if nums[mid] == target:
                 index = mid
-                low = mid + 1 #16
+                low = mid+1 #16
             elif nums[mid] > target: 
-                high = mid - 1
+                high = mid
             else:
-                 low = mid + 1
+                low = mid + 1
             
-            
-        
         return index
