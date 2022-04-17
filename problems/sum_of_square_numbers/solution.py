e79bb5ea543e1db6db940ae403bac08f9e26f9d3
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-04-17 17:54:08 +0000

    Sync LeetCode submission - Sum of Square Numbers (python3)

--- problems/sum_of_square_numbers/solution.py
+++ problems/sum_of_square_numbers/solution.py
@@ -1,9 +1,6 @@
 class Solution:
     def judgeSquareSum(self, c):
-        """
-        :type c: int
-        :rtype: bool
-        """
+    
         sq = set()
         count = int(math.sqrt(c))
         # use (count + 1) because first index is 0
