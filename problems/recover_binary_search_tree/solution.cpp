e02fcb4d139e31adf7c41724d3fafc849be34473
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-04-19 01:40:44 +0000

    Sync LeetCode submission - Recover Binary Search Tree (cpp)

---
+++ problems/recover_binary_search_tree/solution.cpp
@@ -0,0 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode *node_1 = NULL, *node_2 = NULL, *prev = NULL;
+
+void recoverTree(TreeNode* root) {
+
+    // write your code here
+
+    if (root == NULL) return;
+
+    
+
+    inorder(root);
+
+    if (node_1 && node_2)
+
+        swap(node_1->val, node_2->val);
+
+}
+
+void inorder(TreeNode *root) {
+
+    if (root == NULL) return;
+
+    inorder(root->left);
+
+    if (prev != NULL && root->val < prev->val) {
+
+        if (node_1 == NULL) {
+
+            node_1 = prev;
+
+        } 
+
+        if (node_1 != NULL) {
+
+            node_2 = root;
+
+        }
+
+    }
+
+    prev = root;
+
+    inorder(root->right);
+
+}
+        
+
+};
