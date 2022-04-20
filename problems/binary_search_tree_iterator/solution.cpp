5d00e02b2f89c8af13c3927b50cbe4bd3eebbbe2
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-04-20 01:50:17 +0000

    Sync LeetCode submission - Binary Search Tree Iterator (cpp)

---
+++ problems/binary_search_tree_iterator/solution.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/binary-search-tree-iterator/discuss/1430547/C++-Simple-Solution-using-Stack-O(h)-Time-Complexity-(-with-Diagrammatic-Explanation-)
+
+
+
+class BSTIterator {
+public:
+    stack<TreeNode*> s;
+    BSTIterator(TreeNode* root) {
+        partialInorder(root);
+    }
+    
+    void partialInorder(TreeNode* root){
+        while(root != NULL){
+            s.push(root);
+            root = root->left;
+        }
+    }
+    
+    int next() {
+        TreeNode* top = s.top();
+        s.pop();
+        partialInorder(top->right);
+        return top->val;
+    }
+    
+    bool hasNext() {
+        return !s.empty();
+    }
+};
