8cb8aa84c1dd00e38453cf9c4476d686f9c0adbd
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-04-25 03:57:49 +0000

    Sync LeetCode submission - Count Lattice Points Inside a Circle (python3)

--- problems/count_lattice_points_inside_a_circle/solution.py
+++ problems/count_lattice_points_inside_a_circle/solution.py
@@ -1,31 +1,9 @@
 class Solution:
     def countLatticePoints(self, circles: List[List[int]]) -> int:
-        def inn(circle_x, circle_y, rad, x, y):
-            if ((x - circle_x) * (x - circle_x) +
-                (y - circle_y) * (y - circle_y) <= rad * rad):
-                return True;
-            else:
-                return False;
-            
-            
-        s = set()
-        
-        for circle in circles:
-            
-            r = circle[2]
-            cx = circle[0]
-            cy = circle[1]
-
-            x1 = cx -r
-            y1 = cy - r
-            
-            x2 = cx + r
-            y2 = cy + r
-            
-            for i in range( x1 , x2+1):
-                for j in range( y1 , y2 +1):
-                    if((i-cx)**2 + (j-cy)**2<= r*r):
-                        s.add((i, j))
-
-        # print(s)
-        return len(s)
+        seen = set()
+        for x,y,r in circles:
+            for x0 in range(x-r, x+r+1):
+                for y0 in range(y-r,y+r+1):
+                    if (x-x0) ** 2+ (y-y0)**2 <= r*r:
+                        seen.add((x0, y0))
+        return len(seen)
