2255673c2473a9fcd915dbf2f1eeb783fe00400e
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-05-01 03:47:31 +0000

    Sync LeetCode submission - Backspace String Compare (cpp)

--- problems/backspace_string_compare/solution.cpp
+++ problems/backspace_string_compare/solution.cpp
@@ -1,34 +1,51 @@
+// https://leetcode.com/problems/backspace-string-compare/discuss/1469873/Python-2-solutions-O(1)-space-Clean-and-Concise
+
+// space:  O(1)
+
+
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        stack<char>s1, s2;
-        for(auto &i:s){
-            if(i=='#'){
-                if(!s1.empty()){
-                    s1.pop();
-                }
-            }else{
-                s1.push(i);
+        int k=0,p=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='#')
+            {
+                k--;
+                 k=max(0,k);
             }
+            
+           else
+           {
+               s[k]=s[i];
+               k++;
+           }
         }
-        for(auto &i:t){
-            if(i=='#'){
-                if(!s2.empty()){
-                    s2.pop();
-                }
-            }else{
-                s2.push(i);
+        for(int i=0;i<t.size();i++)
+        {
+            if(t[i]=='#')
+            {
+                p--;
+                 p=max(0,p);
             }
+            
+           else
+           {
+               t[p]=t[i];
+               p++;
+           }
         }
-        if(s1.size()!=s2.size()) return false;
-        while(s1.size()){
-            auto x = s1.top();
-            s1.pop();
-            auto y = s2.top();
-            s2.pop();
-            // cout<<x<<" "<<y<<endl;
-            if(x!=y) return false;
+        if(k!=p)
+            return false;
+        else
+        {
+            for(int i=0;i<k;i++)
+            {
+                if(s[i]!=t[i])
+                    return false;
+            }
+            return true;
         }
-        return true;
+        
     }
 };
