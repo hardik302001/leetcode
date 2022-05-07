c20c7ad861bfa8af73e49936c363b6c892e63fcd
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-05-07 08:06:21 +0000

    Sync LeetCode submission - Shortest Unsorted Continuous Subarray (cpp)

--- problems/shortest_unsorted_continuous_subarray/solution.cpp
+++ problems/shortest_unsorted_continuous_subarray/solution.cpp
@@ -2,50 +2,50 @@ class Solution {
 public:
     
     bool outOfOrder(vector<int> arr,int i){
-	int x = arr[i];
-	if(i==0){
-		return x > arr[1];
-	}
-	if(i==arr.size()-1){
-		return x < arr[i-1];
-	}
-	return x > arr[i+1] or x < arr[i-1];
+        int x = arr[i];
+        if(i==0){
+            return x > arr[1];
+        }
+        if(i==arr.size()-1){
+            return x < arr[i-1];
+        }
+        return x > arr[i+1] or x < arr[i-1];
 
     }
 
-   
+
     int findUnsortedSubarray(vector<int>& arr){
 
-	int smallest = 1000000;
-	int largest = -1000000;
-    if(arr.size()==1){return 0;}
-	for(int i=0;i<arr.size();i++){
-		int x = arr[i];
-		
-		if(outOfOrder(arr,i)){
-			smallest = min(smallest,x);
-			largest = max(largest,x);
-		}
-	}
+        int smallest = 1000000;
+        int largest = -1000000;
+        if(arr.size()==1){return 0;}
+        for(int i=0;i<arr.size();i++){
+            int x = arr[i];
 
-	//next step find the right index where smallest and largest lie (subarray) for out solution
-	if(smallest==1000000){
-		return 0;
-	}
+            if(outOfOrder(arr,i)){
+                smallest = min(smallest,x);
+                largest = max(largest,x);
+            }
+        }
 
-	int left = 0;
-	while(smallest >= arr[left]){
-		left++;
-	}
-	int right = arr.size() - 1;
-	while(largest <= arr[right]){
-		right--;
-	}
+        //next step find the right index where smallest and largest lie (subarray) for our solution
+        if(smallest==1000000 or largest ==-1000000){
+            return 0;
+        }
 
-	return right-left+1;
+        int left = 0;
+        while(smallest >= arr[left]){    // leftmost occurence of element greater than smallest
+            left++;
+        }
+        int right = arr.size() - 1;
+        while(largest <= arr[right]){    // rightmost occurence of element smaller than largest
+            right--;
+        }
+        
+        // cout<<smallest<<" "<<largest<<" "<<left<<" "<<right<<endl;
+        
+        return right-left+1;
 
-}
-    
-    
+    }
 
 };
