b1d4625ca070ac401442076bb8ab7dd2c82d383b
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-05-12 10:04:39 +0000

    Sync LeetCode submission - Subsets (cpp)

--- problems/subsets/solution.cpp
+++ problems/subsets/solution.cpp
@@ -1,21 +1,21 @@
 //bitmanipulation
 
-class Solution {
-public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        int n = nums.size(), p = 1 << n;
-        vector<vector<int>> subs(p);
-        for (int i = 0; i < p; i++) {
-            for (int j = 0; j < n; j++) {
-                if ((i) & (1<<j)) {
-                    subs[i].push_back(nums[j]);
+// class Solution {
+// public:
+//     vector<vector<int>> subsets(vector<int>& nums) {
+//         int n = nums.size(), p = 1 << n;
+//         vector<vector<int>> subs(p);
+//         for (int i = 0; i < p; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 if ((i) & (1<<j)) {
+//                     subs[i].push_back(nums[j]);
                     
-                }
-            }
-        }
-        return subs;
-    }
-};
+//                 }
+//             }
+//         }
+//         return subs;
+//     }
+// };
 
 /*
 Time complexity: O(n * 2^N)  to generate all subsets and then copy them into output list.
@@ -56,3 +56,28 @@ Time complexity: O(n * 2^N)  to generate all subsets and then copy them into out
 
 Space complexity:  O(n * 2^N) to keep all the subsets of length NN, since each of NN elements could be present or absent.
 */
+
+
+// ---------------------------------------------------------------------------------------------------
+
+// Recursive : backtracking
+
+class Solution {
+public:
+    vector<vector<int> > subsets(vector<int> &nums) {
+		sort(nums.begin(), nums.end());
+        vector< vector<int> > res;
+		vector<int> vec;
+		subsets(res, nums, vec, 0);
+		return res;
+    }
+private:
+	void subsets(vector<vector<int> > &res, vector<int> &nums, vector<int> &vec, int begin) {
+		res.push_back(vec);
+		for (int i = begin; i < nums.size(); ++i) {
+			vec.push_back(nums[i]);
+			subsets(res, nums, vec, i + 1);
+			vec.pop_back();
+		}
+	}
+};
