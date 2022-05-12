28b4fdfd9c34dcd42de01880f04adbda4fe62724
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-05-12 16:38:59 +0000

    Sync LeetCode submission - Minimum Cost For Tickets (cpp)

--- problems/minimum_cost_for_tickets/solution.cpp
+++ problems/minimum_cost_for_tickets/solution.cpp
@@ -10,22 +10,20 @@ where n is the last day.
 
 */
 
-
-
 class Solution {
 public:
         int mincostTickets(vector<int>& days, vector<int>& costs) {
             
             int n = days.back();
-            vector<int> dp(n + 1);
+            vector<int> dp(n + 1, 0);
             unordered_set<int> seen(days.begin(), days.end());
 
             // dynamic programming
             for (int i = 1; i <= n; i++) {
-                if (!seen.count(i))        //if it is not in days vector,, we need not do anything
+                if (!seen.count(i))        //if it is not in days vector, we need not do anything
                     dp[i] = dp[i - 1];
                 else
-                    dp[i] = min({dp[max(0, i -  1)] + costs[0],
+                    dp[i] = min({dp[max(0, i -  1)] + costs[0],  // all magic is done by dp[0], bcz dp[0]==0 we can extend  dp[i] to dp[i-1] , dp[i-7] , dp[i-30] dpeending upon situation...
                                  dp[max(0, i -  7)] + costs[1],
                                  dp[max(0, i - 30)] + costs[2]});
             }
