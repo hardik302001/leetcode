cbdacc75f0a20300a28ab3175b881b46794392c3
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-05-13 06:14:15 +0000

    Sync LeetCode submission - Redundant Connection (cpp)

--- problems/redundant_connection/solution.cpp
+++ problems/redundant_connection/solution.cpp
@@ -1,6 +1,4 @@
 //DSU
-//SEE PREV SOLN ALSO, dfs
-
 
 class Solution {
 public:
@@ -50,3 +48,49 @@ public:
     }
         
 };
+
+
+//SEE PREV SOLN ALSO, dfs  , add edge check if there is cycle or not..
+
+/*
+
+//FOR MORE INFO
+//https://leetcode.com/problems/redundant-connection/discuss/1295887/Easy-Solution-w-Explanation-or-All-Possible-Approaches-with-comments
+class Solution {
+public:
+    
+        vector<int> findRedundantConnection(vector<vector<int>>& e) {
+            int n = size(e);
+            vector<vector<int>> graph(n+1);
+            vector<bool> vis(n+1);                
+            for(auto& E : e) {
+                fill(begin(vis), end(vis), false);     // reset the vis array
+                graph[E[0]].push_back(E[1]), graph[E[1]].push_back(E[0]);
+                if(dfs(graph, vis, E[0])) return E;
+            }
+            return { };    // un-reachable
+        }
+    
+     
+        bool dfs(vector<vector<int>>& graph, vector<bool>& vis, int cur, int par = -1) { //check cycle
+            vis[cur] = true;
+            for(auto child : graph[cur]){
+                if(vis[child]==false){
+                    if(dfs(graph, vis, child, cur)==true){
+                        return true;
+                    }
+                }
+                else{
+                    if(child!= par){
+                        return true;
+                    }
+                }
+            
+            }    
+            return false;    // no cycle found
+        }
+
+
+    
+};
+*/
