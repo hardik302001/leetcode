9760c3a4213a92fe5e7a306ef8474210aa15902f
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-05-13 05:35:21 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array II (cpp)

--- problems/search_in_rotated_sorted_array_ii/solution.cpp
+++ problems/search_in_rotated_sorted_array_ii/solution.cpp
@@ -1,29 +1,43 @@
-// i dont know how it works
+// exactly same as : https://leetcode.com/problems/search-in-rotated-sorted-array/
+// just added some code for duplicates..
 
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        int left = 0, right =  nums.size()-1, mid;
+        int n = nums.size();
+        int l = 0;
+        int r= n-1;
         
-        while(left<=right)
-        {
-            mid = (left + right) >> 1;
-            if(nums[mid] == target) return true;
-
-            // the only difference from the first one, trickly case, just updat left and right
-            if( (nums[left] == nums[mid]) && (nums[right] == nums[mid]) ) {++left; --right;}
-
-            else if(nums[left] <= nums[mid])
-            {
-                if( (nums[left]<=target) && (nums[mid] > target) ) right = mid-1;
-                else left = mid + 1; 
+        while(l<r){
+            
+            // to avoid duplicates
+            while (l < r && nums[l] == nums[l + 1])
+                l++;
+            while (l < r && nums[r] == nums[r - 1])
+                r--;
+            
+            
+            int m = (l+r)/2;
+            
+            if(nums[m]>nums[r]){   // then first partitiion is monotonic and we will apply our search there!
+                if(target>=nums[l] and target<=nums[m]){
+                    r = m;
+                }else{
+                    l = m+1;
+                }
+            }else{          // then second partitiion is monotonic and we will apply our search there!
+                if(target>=nums[m] and target<=nums[r]){
+                    l = m+1;
+                }else{
+                    r = m;
+                }
             }
-            else
-            {
-                if((nums[mid] < target) &&  (nums[right] >= target) ) left = mid+1;
-                else right = mid-1;
+            
+            if(nums[m]==target){
+                return true;
             }
         }
-        return false;
+        
+        return nums[l]==target?true:false;
     }
 };
