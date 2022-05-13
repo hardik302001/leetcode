ca2150d96b1cb7f954906558896e5304beb4781b
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-05-13 18:42:18 +0000

    Sync LeetCode submission - Coin Change 2 (cpp)

--- problems/coin_change_2/solution.cpp
+++ problems/coin_change_2/solution.cpp
@@ -1,13 +1,16 @@
 //https://cses.fi/problemset/task/1636
-//coin combinations 2
-
+//coin combinations 2 = swap loops frpm coin comb 1
+// dice combination and coin combination 1 are exactly same...
 
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
         vector<int>dp(amount+1,0);
         dp[0] = 1;
-        for(int i = 0; i<coins.size(); i++){
+        
+        // we do coin array above and amount below to get ordered count...
+        // if we do its reverse then we would consider all cases...
+        for(int i = 0; i<coins.size(); i++){            
             for(int j = coins[i]; j<= amount; j++){
                 dp[j] += dp[j - coins[i]];
             }
