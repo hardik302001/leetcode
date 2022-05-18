b44e4f7cacb0b944658c7b8b0f069ed53c92d4e6
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-05-18 15:02:11 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock II (python3)

--- problems/best_time_to_buy_and_sell_stock_ii/solution.py
+++ problems/best_time_to_buy_and_sell_stock_ii/solution.py
@@ -1,24 +1,8 @@
+# see cpp recursive code also , 
+
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        i = 0
-        profit = 0
-        n = len(prices)-1
-        
-        while i<n:
-            while i<n and prices[i+1]<=prices[i]:
-                i = i +1
-            buy = prices[i]
-            
-            while i<n and prices[i+1]>prices[i]:
-                i = i +1
-            sell = prices[i]
-            
-            profit = profit + sell - buy
-        return profit
-        
-        
-        
-#         max_profit = 0
-#         for i in range(len(prices) - 1):
-#             max_profit += max(prices[i+1] - prices[i], 0)
-#         return max_profit
+    def maxProfit(self, prices: List[int]) -> int:        
+        max_profit = 0
+        for i in range(len(prices) - 1):
+            max_profit += max(prices[i+1] - prices[i], 0)
+        return max_profit
