dc0b5f92a4a1d883088632e51628f46a79646264
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-05-19 16:54:24 +0000

    Sync LeetCode submission - Find the Duplicate Number (cpp)

--- problems/find_the_duplicate_number/solution.cpp
+++ problems/find_the_duplicate_number/solution.cpp
@@ -1,28 +1,36 @@
-class Solution {
-public:
-    int findDuplicate(vector<int>& nums) {
-        
-    
-    	if (nums.size() > 1)
-        {
-            int slow = nums[0];
-            int fast = nums[nums[0]];
-            while (slow != fast)
-            {
-                slow = nums[slow];
-                fast = nums[nums[fast]];
-            }
+/*
 
-            fast = 0;
-            while (fast != slow)
-            {
-                fast = nums[fast];
-                slow = nums[slow];
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+*/
+
+class Solution{
+public:
+    int findDuplicate(vector<int> &nums){
+
+        int slow = nums[0];
+        int entry = nums[0];
+        int fast = nums[0];
+        while (true){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if(slow==fast){
+                while (slow != entry){
+                    entry = nums[entry];
+                    slow = nums[slow];
+                } 
+                return entry;
             }
-            return slow;
         }
+        
         return -1;
     }
 };
 
-//https://leetcode.com/problems/find-the-duplicate-number/discuss/72846/My-easy-understood-solution-with-O(n)-time-and-O(1)-space-without-modifying-the-array.-With-clear-explanation.
+
+
+// https://leetcode.com/problems/linked-list-cycle-ii/
+// also see solution section! 
+
+// https://leetcode.com/problems/find-the-duplicate-number/discuss/72846/My-easy-understood-solution-with-O(n)-time-and-O(1)-space-without-modifying-the-array.-With-clear-explanation.
