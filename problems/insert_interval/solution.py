70046522e5e467199fd6f5b47f60ef880a764baf
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-05-19 16:21:58 +0000

    Sync LeetCode submission - Insert Interval (python3)

--- problems/insert_interval/solution.py
+++ problems/insert_interval/solution.py
@@ -1,13 +1,22 @@
+# also see: merge interval
+
+
+
+# T.C. : = O(n) 
+
+
 class Solution:
     def insert(self, intervals: List[List[int]], new: List[int]) -> List[List[int]]:
         res = []
         for i in range(len(intervals)):
-            if new[1]<intervals[i][0]:
+            if new[1]<intervals[i][0]:    # before current interval
                 res.append(new)
                 return res + intervals[i:]
-            elif new[0]>intervals[i][1]:
+            elif new[0]>intervals[i][1]:   # after current interval
                 res.append(intervals[i])
             else:
-                new = [min(new[0],intervals[i][0]), max(new[1],intervals[i][1])]
-        res.append(new)
+                new = [min(new[0],intervals[i][0]), max(new[1],intervals[i][1])]  # very important we are updating new here..and not pushing it now , bcz its a case of overlapping intervals
+                
+        res.append(new) # we have not returned until now , it means new is still left so we push it into result , and thus we now return our res array
+        
         return res
