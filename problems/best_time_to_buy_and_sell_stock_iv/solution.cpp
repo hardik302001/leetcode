efca8243216dcf6403a5ac4322693c4914d64059
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-05-19 09:20:59 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock IV (cpp)

--- problems/best_time_to_buy_and_sell_stock_iv/solution.cpp
+++ problems/best_time_to_buy_and_sell_stock_iv/solution.cpp
@@ -1,29 +1,29 @@
-// (SAME AS BEST TIME TO BUY AND SEL STOCK 3)
-
+// (SAME AS BEST TIME TO BUY AND SELL STOCK 3)
+// there is some other soln also , with 2 state only , see striver video ...do that also later on..
 
 class Solution {
 public:
-    int dp[1001][2][101];            //just chnged the constrants accordingly
+    int dp[1001][2][101];            //just chnged the constriants accordingly
     int maxProfit(int transac, vector<int>& prices) {
         int n = prices.size();
         for(int i = 0;i<n;i++){
             for(int j = 0;j<2;j++){
-                for(int k = 0;k<transac;k++){
+                for(int k = 0;k<transac+1 ; k++){      // atmost transac = 0 , 1, 2 .. transac  = transac + 1
                     dp[i][j][k] = -1;
                 }
             }
         }
-        int ans = recur(prices, 0 , 1,transac-1);  //I deliberately started from tarnsac -1, because when if I start from transac , I will assign value,then dp[i][buying][transac] will give segmentation fault, because its index is from 0 to transac-1, not 0 yo transac, so to avoid it I started from transac -1 ..
+        int ans = recur(prices, 0 , 1,transac);  
         return ans;
     }
     
     
     int recur(vector<int>&prices, int i, int buying, int transac){
-        int buy, sell, cooldown;
+        int buy, sell, wait;
         if(i>=prices.size()){
             return 0;
         }
-        if (transac<0){
+        if (transac==0){  // transactions are finished, no more transactions, so no more profit
             return 0;
         }
         if (dp[i][buying][transac]!=-1){
@@ -32,13 +32,13 @@ public:
              
         if (buying){           //if in buying state
             buy = recur(prices, i+1, buying^1,transac) - prices[i];   //if buy , then relative profit would be curr profit + (-price[i]) 
-            cooldown = recur(prices, i+1, buying,transac); 
-            dp[i][buying][transac] = max(cooldown, buy);  //either you will buy or cooldown, 
+            wait = recur(prices, i+1, buying,transac); 
+            dp[i][buying][transac] = max(wait, buy);  //either you will buy or cooldown, 
         }
         else{
             sell = recur(prices, i+1, buying^1,transac-1) + prices[i]; //if sell , then relative profit would be curr profit + (+price[i]) 
-            cooldown = recur(prices, i+1, buying,transac); 
-            dp[i][buying][transac] = max(cooldown, sell);
+            wait = recur(prices, i+1, buying,transac); 
+            dp[i][buying][transac] = max(wait, sell);
         }
             
         return dp[i][buying][transac];
