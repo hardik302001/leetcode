64bd3be04679ddf32a324956d8775075174c5d2e
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-05-19 17:29:00 +0000

    Sync LeetCode submission - Two Sum (python3)

--- problems/two_sum/solution.py
+++ problems/two_sum/solution.py
@@ -1,3 +1,7 @@
+# also see : https://takeuforward.org/data-structure/two-sum-check-if-a-pair-with-given-sum-exists-in-array/
+
+# see sorting + 2 pointer nlogn approach also..
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         d = {}
