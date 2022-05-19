f0f45b8b525969a83224088a29651e150f46f35c
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-05-19 18:06:46 +0000

    Sync LeetCode submission - 4Sum (python3)

--- problems/4sum/solution.py
+++ problems/4sum/solution.py
@@ -1,30 +1,43 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        n=len(nums)
         nums.sort()
+
         res = []
-        n = len(nums)
-        for i in range(n):
-            for j in range(i+1,n):
-                cur = nums[i] + nums[j]
-                s = j+1
-                e = n-1
-                need = target - cur
-                while s<e:
-                    if nums[s] + nums[e]==need:
-                        x = []
-                        x.append(nums[i])
-                        x.append(nums[j])
-                        x.append(nums[s])
-                        x.append(nums[e])
-                        if x in res:
-                            pass
-                        else:
-                            res.append(x)
-                        s = s +1
-                        e = e -1
-                    elif nums[s] + nums[e]>need:
-                        e = e -1
+        i = 0
+        j = i+1
+        while i< n-3:
+            j = i+1
+            while j<n-2:
+                start=j+1
+                end=n-1
+            
+                while start < end:
+                    cur = nums[i] + nums[j] + nums[start] + nums[end]
+                    
+                    if cur==target and [nums[i],nums[j],nums[start],nums[end]] not in res:
+                        res.append((nums[i],nums[j],nums[start],nums[end]))
+                        
+                        # repetition check only on start and end 
+                        while start < end and nums[start] == nums[start+1]:   
+                            start += 1
+                        start+=1
+                        while start < end and nums[end] == nums[end-1]:
+                            end -= 1
+                        end-=1
+                        
+                    elif cur > target:
+                        end-=1
                     else:
-                        s = s +1
-        return res
+                        start+=1
                 
+                
+                # repetion of 2nd number
+                while j+1 < n-2 and nums[j] == nums[j+1]:   
+                    j += 1
+                j+=1
+            # repetition of first number
+            while i+1 < n-3 and nums[i] == nums[i+1]:   
+                i += 1
+            i+=1
+        return res
