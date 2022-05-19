20c139570402edb65853470144b870567291f1b2
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-05-19 17:11:22 +0000

    Sync LeetCode submission - Pow(x, n) (cpp)

--- problems/pow(x,_n)/solution.cpp
+++ problems/pow(x,_n)/solution.cpp
@@ -1,10 +1,28 @@
-class Solution {
-public:
-    double myPow(double x, int n) {
-        long n2 = (long)n;
-        if (n2 < 0) {n2 = -n2; x = 1/x;}
-        if (n2 == 0)
-            return 1;
-        return (n2 % 2 == 0) ? myPow(x*x, n2/2) :  myPow(x*x, n2/2)*x;
-    }
+// TC : O(logn)
+
+
+class Solution
+{
+    public:
+        double myPow(double x, int n)
+        {
+            double ans = 1.0;
+            long long nn = n;
+            if (nn < 0) nn = -1 * nn;
+            while (nn)
+            {
+                if (nn % 2)           // odd
+                {
+                    ans = ans * x;     // updating ans
+                    nn = nn - 1;
+                }
+                else                 // even
+                {
+                    x = x * x;       // updating x , not ans
+                    nn = nn / 2;
+                }
+            }
+            if (n < 0) ans = (double)(1.0) / (double)(ans);
+            return ans;
+        }
 };
