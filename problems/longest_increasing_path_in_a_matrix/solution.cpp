b2b500e7434bfe2097fb8b67a197b50fe281ab56
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-05-19 00:33:22 +0000

    Sync LeetCode submission - Longest Increasing Path in a Matrix (cpp)

--- problems/longest_increasing_path_in_a_matrix/solution.cpp
+++ problems/longest_increasing_path_in_a_matrix/solution.cpp
@@ -2,11 +2,6 @@
 // dp soln with memoization easy peasy!
 // TC: O(n*m)
 
-// start from all points and find longest increasing path considering current point as beginning of path, recursively it will be O(m*n*4^(m*n))
-
-// with dp it will be just O(m*n)
-
-
 class Solution {
 public:
     int solve(int i , int j , int m , int n , vector< vector< int> > &dp, vector< vector< int> > &matrix){
@@ -16,7 +11,7 @@ public:
         
         vector<int> dx = {1, 0 , -1, 0};
         vector<int> dy = {0 , 1 , 0 , -1};
-        int res = 1;     // path will be atleast of size 1 ( suppose all 4 directions are invalid[ 3rd sample TC] )
+        int res = 1;     // path will eb atleast of size 1c ( suppose all 4 directions are invalid[ 3rd sample TC] )
         for(int k = 0;k<4;k++){
             int newx = i + dx[k];
             int newy = j + dy[k];
@@ -26,8 +21,9 @@ public:
                 // 1 + ...bcz we know current cell is surely included in longest inc path!
             } 
         }
-        return dp[i][j] = res;
+        dp[i][j] = res;
         
+        return res;
     }
 
     
@@ -47,6 +43,10 @@ public:
         return ans;
     }
 };
+
+
+
+
 
 
 /*
