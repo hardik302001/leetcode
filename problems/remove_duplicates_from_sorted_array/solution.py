a2aa2c642df80f636075eebfe6eda5df6b9ffbcd
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-05-20 09:24:15 +0000

    Sync LeetCode submission - Remove Duplicates from Sorted Array (python3)

--- problems/remove_duplicates_from_sorted_array/solution.py
+++ problems/remove_duplicates_from_sorted_array/solution.py
@@ -1,7 +1,30 @@
-'''
-Since the array is already sorted, we can keep two pointers ii and jj, where ii is the slow-runner while j is the fast-runner. As long as nums[i] = nums[j] , we increment jj to skip the duplicate.
+# see https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
+# then move ahead...
 
-When we encounter nums[j] != nums[i] , the duplicate run has ended so we must copy its value to nums[i + 1]. ii is then incremented and we repeat the same process again until jj reaches the end of array.
+
+    
+    
+    
+    
+class Solution:
+    def removeDuplicates(self, nums):
+        slow, fast = 1, 1
+
+        while fast < len(nums):
+            if nums[slow - 1] != nums[fast]:
+                nums[slow] = nums[fast]
+                slow += 1
+            fast += 1
+        return slow
+    
+    
+    
+    
+    
+'''
+Since the array is already sorted, we can keep two pointers i and j, where i is the slow-runner while j is the fast-runner. As long as nums[i] = nums[j] , we increment j to skip the duplicate.
+
+When we encounter nums[j] != nums[i] , the duplicate run has ended so we must copy its value to nums[i + 1]. i is then incremented and we repeat the same process again until j reaches the end of array.
 
 '''
 
@@ -17,15 +40,5 @@ When we encounter nums[j] != nums[i] , the duplicate run has ended so we must co
 #         return count
     
 
-#THIS CODE IS GENERALISED VERSION FOR ANY K, rather than atmost  one occurence or rather than atmost 2 occurence, for more info visit remove-duplicates-from-sorted-array-ii
-    
-class Solution:
-    def removeDuplicates(self, nums):
-        slow, fast = 1, 1
-
-        while fast < len(nums):
-            if nums[slow - 1] != nums[fast]:
-                nums[slow] = nums[fast]
-                slow += 1
-            fast += 1
-        return slow
+# THIS CODE IS GENERALISED VERSION FOR ANY K, rather than atmost one occurence or rather than atmost 2 occurence, for more info visit remove-duplicates-from-sorted-array-ii
+  
