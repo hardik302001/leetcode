77ae9313f8ae8859a6ca5531ce1c6f2e2b619998
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-05-21 02:23:38 +0000

    Sync LeetCode submission - Coin Change (cpp)

--- problems/coin_change/solution.cpp
+++ problems/coin_change/solution.cpp
@@ -1,21 +1,22 @@
 //minimizing coin cses
 
+
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
         long long int INF = 10000000000;
-        vector<long long > dp(amount +1,INF);
+        vector < long long > dp(amount +1,INF);
         dp[0] = 0;
-        
+
         for(int i = 1;i<=amount;i++){
             long long int ans = INF;
-            for(int j = 0;j<coins.size();j++){
+            for(int j = 0;j < coins.size();j++){
                 if(coins[j]<=i){
                     ans = min(ans, dp[i-coins[j]]);
                 }
             }
-            
+
             if(ans==INF){
                 dp[i] = INF;
             }
@@ -23,15 +24,13 @@ public:
                 dp[i] = ans +1;
             }
         }
-        
-        
+
         if(dp[amount]==INF){
             return -1;
         }
         else{
             return dp[amount];
         }
-        
-        
     }
 };
+
