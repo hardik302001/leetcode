7807a27ac1b4660b6876d475fc47ebb3aacc7b9b
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-05-22 14:03:52 +0000

    Sync LeetCode submission - Palindrome Partitioning (cpp)

--- problems/palindrome_partitioning/solution.cpp
+++ problems/palindrome_partitioning/solution.cpp
@@ -16,7 +16,7 @@ public:
     
     void backtrack(int start, vector<string> &temp, string s){
         int n = s.length();
-        if (start >= n) {
+        if (start == n) {
             res.push_back(temp);
             return;
         } 
@@ -39,6 +39,16 @@ public:
             }
         }
         return true;
-    }
-    
+    }    
 };
+
+
+/*
+
+Time Complexity: O( (2^n) *k*(n/2) )
+Reason: O(2^n) to generate every substring and O(n/2)  to check if the substring generated is a palindrome. O(k) is for inserting the palindromes in another data structure, where k  is the average length of the palindrome list.
+
+Space Complexity: O(k * x)
+Reason: The space complexity can vary depending upon the length of the answer. k is the average length of the list of palindromes and if we have x such list of palindromes in our final answer. The depth of the recursion tree is n, so the auxiliary space required is equal to the O(n).
+*/
+
