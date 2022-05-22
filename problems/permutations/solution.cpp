e73589cf8558afdb48b8f24aad545dbf2af8dbd8
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-05-22 16:56:06 +0000

    Sync LeetCode submission - Permutations (cpp)

--- problems/permutations/solution.cpp
+++ problems/permutations/solution.cpp
@@ -1,30 +1,84 @@
 //total n! permutuations
-//see prev submission, for backtracking by copy
 
 // here it is backtracking by refernce
+// also see : https://leetcode.com/problems/permutations-ii/submissions/
+
+// class Solution {
+// public:
+//    // method 1: standard backtracing solution
+//     vector<vector<int> > permute(vector<int> &num) {  // we are passing num array only ...
+//         vector<vector<int> > ans;
+//         permutation(num, 0, ans);
+//         return ans;
+//     }
+
+//     void permutation(vector<int>& num, int begin, vector<vector<int> > &ans) {        
+//         //now we are taking nums vector by value, so we need to backtrack it
+//         if (begin == num.size()) {
+//             ans.push_back(num);
+//             return;
+//         }
+
+//         // every number chosen to be the begin once
+//         for (int i = begin; i < num.size(); ++i) {
+//             swap(num[begin], num[i]);
+//             permutation(num, begin+1, ans);
+//             swap(num[begin], num[i]);            //backtracking step
+//         }
+//     }
+// };
+
+/*
+Time complexity: O(N! * N)
+
+Reason:  The recursion takes O(N!)  time because we generate every possible permutation and another O(N)  time is required to make a deep copy and store every sequence in the data structure. 
+
+Space complexity: O(N) 
+
+Reason: Result stored in a vector, we are auxiliary space taken by recursion
+*/
 
 
 class Solution {
 public:
-   // method 1: standard backtracing solution
-    vector<vector<int> > permute(vector<int> &num) {  // we are passing num array only ...
-        vector<vector<int> > ans;
-        permutation(num, 0, ans);
-        return ans;
-    }
+    vector<vector<int> > res;
+    
+    vector<vector<int> > permute(vector<int> &num) {
+        int n = num.size();
+        map<int , int>m;
+        for(auto x: num) m[x]++; 
+        vector<int>cur = {};   // keep a new array to keep track of it
+        permutation(cur , m , n);
 
-    void permutation(vector<int>& num, int begin, vector<vector<int> > &ans) {        
-        //now we ar etaking nums vector by value, so we need to backtrack it
-        if (begin >= num.size()) {
-            ans.push_back(num);
+        return res;
+    }
+    
+    
+     void permutation(vector<int>&cur , map<int, int>&m , int n) {        
+        if(cur.size()==n){
+            res.push_back(cur);
             return;
         }
-
-        // every number chosen to be the begin once
-        for (int i = begin; i < num.size(); ++i) {
-            swap(num[begin], num[i]);
-            permutation(num, begin+1, ans);
-            swap(num[begin], num[i]);            //backtracking step
+        
+         
+        for(auto &x: m){
+            if(x.second>0){
+                cur.push_back(x.first);
+                m[x.first]-=1;
+                    
+                permutation(cur , m , n);
+                
+                m[x.first]+=1;
+                cur.pop_back();
+            }
         }
+         
+         
     }
+
 };
+/*
+Time Complexity:  N! x N
+
+Space Complexity:  O(N)
+*/
