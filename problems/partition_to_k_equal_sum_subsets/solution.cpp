46491633cb239777a53318f54256f72a84992e62
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-05-24 11:48:33 +0000

    Sync LeetCode submission - Partition to K Equal Sum Subsets (cpp)

---
+++ problems/partition_to_k_equal_sum_subsets/solution.cpp
@@ -0,0 +1,53 @@
+// same as : https://leetcode.com/problems/matchsticks-to-square
+// pseudo code same as : sudoku solver
+// backtracking
+
+
+
+
+class Solution {
+public:
+    int n;
+    int visited[16];
+    
+    bool possible(vector<int>& nums, int numsIndex, int currSum, int& subsetSum, int k) {
+        if(k == 1)
+            return true;
+        if(currSum == subsetSum) {
+            return possible(nums, n-1, 0, subsetSum, k-1);
+        }
+        
+        for(int i = numsIndex; i>=0; i--) {
+            if(visited[i] || currSum + nums[i] > subsetSum)
+                continue;
+            
+            visited[i] = 1;
+            currSum   += nums[i];
+            
+            if(possible(nums, i-1, currSum, subsetSum, k))
+                return true;
+            
+            visited[i] = 0;
+            currSum   -= nums[i];
+        }
+        
+        return false;
+    }
+    
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        int sum = accumulate(begin(nums), end(nums), 0);
+        
+        if(sum%k != 0)
+            return false;
+        
+        memset(visited, 0, sizeof(visited));
+        
+        n                   = nums.size();
+        int subsetSum       = sum/k;
+        int numsIndex       = n-1;
+        int currSum         = nums[numsIndex];
+        visited[numsIndex]  = 1;
+        
+        return possible(nums, numsIndex, currSum, subsetSum, k);
+    }
+};
