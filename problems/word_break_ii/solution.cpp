5691898cc0cebc2fdfbee00016b82176fc61ad88
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-05-24 10:35:26 +0000

    Sync LeetCode submission - Word Break II (cpp)

--- problems/word_break_ii/solution.cpp
+++ problems/word_break_ii/solution.cpp
@@ -1,9 +1,50 @@
-//RECURSION
+
+
+// backtracking
+
+class Solution {
+public:
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        //insert all the words in the set
+        unordered_set<string> set;
+        vector<string> res;
+        for(auto word:wordDict)
+            set.insert(word);
+        //to store the current string 
+        string curr="";
+        findHelper(0,s,curr,set,res);
+        return res;
+    }
+    
+    void findHelper(int ind,string s,string curr,unordered_set<string> set,vector<string>& res)
+    {
+        if(ind==s.length())
+        {
+            //we have reached end
+            curr.pop_back(); //remove the trailing space
+            res.push_back(curr);
+        }
+        string str="";
+        for(int i=ind;i<s.length();i++)
+        {
+            //get every substring and check if it exists in set
+            str.push_back(s[i]);
+            if(set.count(str))
+            {
+                //we have got a word in dict 
+                //explore more and get other substrings
+                findHelper(i+1,s,curr+str+" ",set,res);
+            }
+        }
+    }
+};
 
 
 
+// ------------------------------------------------------------------------------------------------------
 
-
+// RECURSION + dp type
+/*
 class Solution {
 public:
     unordered_map<string, vector<string>> m;
@@ -44,7 +85,7 @@ public:
     
 };
 
-
+*/
 /*
 Paraphrasing the solution in my words
 
@@ -64,3 +105,4 @@ and // then comes and which is present so now again wordbreak called on remainde
 
 Hope this helps.
 */
+
