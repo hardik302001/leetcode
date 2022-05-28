12e955aeb64f792c97c8bd7216dc1ea16afe9bdb
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-05-28 06:43:04 +0000

    Sync LeetCode submission - Maximum Depth of Binary Tree (cpp)

--- problems/maximum_depth_of_binary_tree/solution.cpp
+++ problems/maximum_depth_of_binary_tree/solution.cpp
@@ -1,47 +1,22 @@
-/*
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        if(root==NULL){
-            return 0;
-        }
-        int lh = 1 + maxDepth(root->left);
-        int rh = 1 + maxDepth(root->right);
-        return max(lh, rh);
-    }
-};
-*/
-/*
-Although the time complexity is same as O(n), but the space complexities are different.
-the DFS is O(logN) while the BFS is O(N), the reason is that for a balanced binary search tree, the last row of leaves is equal to half of the total node number.
-*/
-
-
-class Solution {
-public:
-    int maxDepth(TreeNode *root)
-    {
-        if(root == NULL)
-            return 0;
-
-        int res = 0;
-        queue<TreeNode *> q;
-        q.push(root);
-        while(!q.empty())
-        {
-            for(int i = 0, n = q.size(); i < n; ++ i)
-            {
-                TreeNode *p = q.front();
-                q.pop();
-
-                if(p -> left != NULL)
-                    q.push(p -> left);
-                if(p -> right != NULL)
-                    q.push(p -> right);
-            }
-            res++;
-        }
-
-        return res;
+        if(root== NULL) return 0;
+        
+        int lh = maxDepth(root->left);
+        int rh = maxDepth(root->right);
+        
+        return 1 + max(lh , rh);
     }
 };
