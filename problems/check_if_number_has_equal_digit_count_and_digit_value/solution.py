84e6d943eb7ee75f5530898b7ca9a08a871f6fca
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-05-28 16:31:30 +0000

    Sync LeetCode submission - Check if Number Has Equal Digit Count and Digit Value (python3)

---
+++ problems/check_if_number_has_equal_digit_count_and_digit_value/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def digitCount(self, num: str) -> bool:
+        for i in range(len(num)):
+            if int(num[i]) != num.count(str(i)):
+                return False
+        return True
