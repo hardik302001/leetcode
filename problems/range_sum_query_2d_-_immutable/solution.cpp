f618d6665ef33bd7232042e32bf379b5e7a8966c
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-06-03 03:11:07 +0000

    Sync LeetCode submission - Range Sum Query 2D - Immutable (cpp)

--- problems/range_sum_query_2d_-_immutable/solution.cpp
+++ problems/range_sum_query_2d_-_immutable/solution.cpp
@@ -1,60 +1,27 @@
-// also see: https://leetcode.com/problems/matrix-block-sum/submissions/
-
-
-
-/*
-Construct a 2D array sums[row+1][col+1]
-
-(notice: we add additional blank row sums[0][col+1]={0} and blank column sums[row+1][0]={0} to remove the edge case checking), so, we can have the following definition
-
-sums[i+1][j+1] represents the sum of area from matrix[0][0] to matrix[i][j]
-
-To calculate sums, the ideas as below
-
-+-----+-+-------+     +--------+-----+     +-----+---------+     +-----+--------+
-|     | |       |     |        |     |     |     |         |     |     |        |
-|     | |       |     |        |     |     |     |         |     |     |        |
-+-----+-+       |     +--------+     |     |     |         |     +-----+        |
-|     | |       |  =  |              |  +  |     |         |  -  |              |
-+-----+-+       |     |              |     +-----+         |     |              |
-|               |     |              |     |               |     |              |
-|               |     |              |     |               |     |              |
-+---------------+     +--------------+     +---------------+     +--------------+
-
-   sums[i][j]      =    sums[i-1][j]    +     sums[i][j-1]    -   sums[i-1][j-1]   +  
-
-                        matrix[i-1][j-1]
-So, we use the same idea to find the specific area's sum.
-
-+---------------+   +---------+----+   +---+-----------+   +---------+----+   +---+----------+
-|               |   |         |    |   |   |           |   |         |    |   |   |          |
-|   (r1,c1)     |   |         |    |   |   |           |   |         |    |   |   |          |
-|   +------+    |   |         |    |   |   |           |   +---------+    |   +---+          |
-|   |      |    | = |         |    | - |   |           | - |      (r1,c2) | + |   (r1,c1)    |
-|   |      |    |   |         |    |   |   |           |   |              |   |              |
-|   +------+    |   +---------+    |   +---+           |   |              |   |              |
-|        (r2,c2)|   |       (r2,c2)|   |   (r2,c1)     |   |              |   |              |
-+---------------+   +--------------+   +---------------+   +--------------+   +--------------+
-And we can have the following code
-*/
-
 class NumMatrix {
-private:
-    int row, col;
-    vector<vector<int>> sums;
 public:
-    NumMatrix(vector<vector<int>> &matrix) {
-        row = matrix.size();
-        col = matrix[0].size();
-        sums = vector<vector<int>>(row+1, vector<int>(col+1, 0));
-        for(int i=1; i<=row; i++) {
-            for(int j=1; j<=col; j++) {
-                sums[i][j] = matrix[i-1][j-1] + sums[i-1][j] + sums[i][j-1] - sums[i-1][j-1] ;
+    
+    vector<vector<int>>dp;
+    NumMatrix(vector<vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
+        dp = vector<vector<int>>(m+1 , vector<int>(n+1 , 0));
+        for(int i= 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                dp[i+1][j+1] = dp[i][j+1] + dp[i+1][j] - dp[i][j] + mat[i][j];
             }
         }
-    }
+        
 
+    }
+    
     int sumRegion(int row1, int col1, int row2, int col2) {
-        return sums[row2+1][col2+1] - sums[row2+1][col1] - sums[row1][col2+1] + sums[row1][col1];
+        return (dp[row2+1][col2+1] - (dp[row1][col2+1] + dp[row2+1][col1]) + dp[row1][col1]);
     }
 };
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ */
