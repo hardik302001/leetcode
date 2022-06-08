4900899912004be7c3bfcc78184b66b485eaa740
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-06-08 06:11:48 +0000

    Sync LeetCode submission - All Paths From Source to Target (cpp)

--- problems/all_paths_from_source_to_target/solution.cpp
+++ problems/all_paths_from_source_to_target/solution.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     
-    //I  cant do &path in function arguement bcz, I want my path to have different values in different dfs...
     
     void dfs(vector<vector<int>>& graph, vector<vector<int>>& result, vector<int>& path, int src, int end) {
-        
-        path.push_back(src);
         if(src == end) {
+            path.push_back(src);
             result.push_back(path);
             path.pop_back();
             return;
         }
         
-        for(auto node : graph[src])
+        for(auto node : graph[src]){
+            path.push_back(src);
             dfs(graph, result, path, node, end);
-        path.pop_back();
+            path.pop_back();
+        }
         
     }
 
