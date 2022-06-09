3883ac378c44338942b630c5847031268d5ab22c
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-06-09 05:17:53 +0000

    Sync LeetCode submission - Number of Operations to Make Network Connected (cpp)

--- problems/number_of_operations_to_make_network_connected/solution.cpp
+++ problems/number_of_operations_to_make_network_connected/solution.cpp
@@ -1,3 +1,7 @@
+// you need not count extra edges (or cycles),
+// you only need to count total number of components... bcz if we have sufficient edges, then we need minimum of k-1 edges for k components..
+// ans = component_count - 1
+
 class Solution {
 public:
      class UnionFind {
@@ -31,17 +35,53 @@ public:
     };
     
     
-    int makeConnected(int n, vector<vector<int>>& connections) {
+    int makeConnected(int n, vector<vector<int>>& con) {
         UnionFind uf(n);
-        for(auto &p: connections){
+        if (n-1>con.size()) return -1;
+        int c = 0;
+        for(auto &p: con){
             uf.Union(p[0],p[1]);
         }
         
-        if(connections.size()>=(n-1)){
-            return uf.getCount()-1;
-        }
-        
-        return -1;
+        return uf.getCount()-1;
+
     
     }
 };
+
+// dfs
+
+/*
+class Solution {
+private:
+    void dfs(vector<vector<int>> &adj, vector<bool> &visited, int src)
+    {
+        visited[src] = true;
+        for(int i : adj[src])
+            if(!visited[i])
+                dfs(adj, visited, i);
+    }
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        
+        if(connections.size() < n - 1)
+            return -1;
+        vector<vector<int>> adj(n);
+        for(auto v : connections)
+        {
+            adj[v[0]].push_back(v[1]);
+            adj[v[1]].push_back(v[0]);
+        }
+        vector<bool> visited(n, false);
+        int components = 0;
+        for(int i=0; i<n; i++)
+            if(!visited[i])
+            {
+                dfs(adj, visited, i);
+                components++;
+            }
+        return components - 1;
+    }
+};
+
+*/
