3dd1bda456ddef46baa63ca1e7861a6d29823a28
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-06-11 02:28:56 +0000

    Sync LeetCode submission - Minimum Operations to Reduce X to Zero (cpp)

--- problems/minimum_operations_to_reduce_x_to_zero/solution.cpp
+++ problems/minimum_operations_to_reduce_x_to_zero/solution.cpp
@@ -1,32 +1,18 @@
-//pref sum + map 
-//O(n)
-// https://leetcode.com/problems/minimum-operations-to-reduce-x-to-zero/discuss/1353917/C%2B%2B-Binary-Search-to-Hash-Map
-
-
-//also see prev code
-
-
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
-        int n = nums.size(), ans = INT_MAX, suffix_sum = 0, prefix_sum = 0;
-        unordered_map<int, int> suffix;
-        for (int i = n - 1; i >= 0; --i) {
-            suffix_sum += nums[i];
-            if (suffix_sum == x)
-                ans = min(n - i, ans);
-            suffix[suffix_sum] = i;
+        int n = nums.size(), j = 0, sum = 0, maxlen = -1;
+        int total = accumulate(begin(nums), end(nums), 0);
+        if(total < x) return -1;
+        for(int i = 0; i < n; i++) {
+            sum += nums[i];
+            while (sum > total - x)
+                sum -= nums[j++];
+            if(sum == total - x) 
+                maxlen = max(maxlen, i - j + 1);
         }
-        for (int i = 0; i < n; ++i) {
-            prefix_sum += nums[i];
-            if (prefix_sum == x)
-                ans = min(i + 1, ans);
-            int target = x - prefix_sum;
-            if (suffix.find(target) != suffix.end() && suffix[target] > i)
-                ans = min(ans, i + 1 + n - suffix[target]);
-        }
-        if (ans == INT_MAX)
-            return -1;
-        return ans;
+        return maxlen == -1 ? -1 : n - maxlen;
     }
+        
+    
 };
