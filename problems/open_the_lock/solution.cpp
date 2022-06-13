41cbef3e0bfdc85e20e6b433c9df51483538fd6c
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-06-13 04:17:33 +0000

    Sync LeetCode submission - Open the Lock (cpp)

--- problems/open_the_lock/solution.cpp
+++ problems/open_the_lock/solution.cpp
@@ -1,50 +1,50 @@
-//BFS
-
 class Solution {
 public:
-    int openLock(vector<string> &deadends, string target) {
-        unordered_set<string> deadendsSet(deadends.begin(), deadends.end());
-        unordered_set<string> visited;
-        int result = 0;
-        if (deadendsSet.find("0000") != deadendsSet.end()) {
+    int openLock(vector<string>& deadends, string target) {
+        unordered_set<string>ss(deadends.begin() , deadends.end());
+        if (ss.count("0000")) {
             return -1;
         }
-        queue<string> wheelQueue;
-        wheelQueue.push("0000");
-        visited.insert("0000");
-        while (!wheelQueue.empty()) {
-            int levelSize = wheelQueue.size();
-            while (levelSize--) {
-                string up;
-                string down;
-                string currentWheel = wheelQueue.front();        // 3 cases
-                wheelQueue.pop();
-                if (currentWheel == target) { //curr wheel
-                    return result;
+        queue<string>q;
+        q.push("0000");
+        
+        int ans = 0;
+        while(q.size()){
+            int sz = q.size();
+            while(sz--){
+                auto s = q.front();
+                q.pop();
+                
+                if(s==target){
+                    return ans;
                 }
                 
-                //now 2 cases for up and down
-
-            
-                for (int i = 0; i < 4; i++) {
-                    // every time,we can change atmost one wheel, fro up and down, and get a new combination, so we always reset up and down to curr!
-                    down = currentWheel;
-                    up = currentWheel;
-                    char upCh = up[i], downCh = down[i];
-                    up[i] = (upCh == '9' ? '0' : upCh + 1);
-                    down[i] = (downCh == '0' ? '9' : downCh - 1);
-                    if (visited.find(up) == visited.end() && deadendsSet.find(up) == deadendsSet.end()) {
-                        wheelQueue.push(up);
-                        visited.insert(up);
+                for(int i = 0;i<4;i++){
+                    string temp = s;
+                    char next = (((temp[i]-'0')+1)%10+'0');
+                    char prev = (((temp[i]-'0')-1+10)%10+'0');
+                    temp[i] = next;
+                    if(temp!=s and ss.count(temp)==0){
+                        q.push(temp);
+                        ss.insert(temp);
                     }
-                    if (visited.find(down) == visited.end() && deadendsSet.find(down) == deadendsSet.end()) {
-                        wheelQueue.push(down);
-                        visited.insert(down);
+                    
+                    temp[i] = prev;
+                    if(temp!=s and ss.count(temp)==0){
+                        q.push(temp);
+                        ss.insert(temp);
                     }
                 }
+                
             }
-            result++;
+            ans++;
         }
+        
+        
+        
         return -1;
+        
+        
+        
     }
 };
