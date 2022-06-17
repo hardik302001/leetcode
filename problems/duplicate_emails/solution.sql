3d5ec95f04715040af55c051781cb59d104adf53
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-06-17 16:38:34 +0000

    Sync LeetCode submission - Duplicate Emails (mysql)

--- problems/duplicate_emails/solution.sql
+++ problems/duplicate_emails/solution.sql
@@ -3,15 +3,16 @@
 
 
 -- group by + having
-/*
+
 SELECT email as 'Email'
 FROM Person
 GROUP BY email
-HAVING COUNT(email)>1;
-*/
+HAVING COUNT(id)>1;
 
 
+/*
 -- self join
 SELECT DISTINCT(p1.email) as 'Email'
 FROM Person as p1 , Person as p2
 WHERE p1.email = p2.email AND p1.id<>p2.id
+*/
