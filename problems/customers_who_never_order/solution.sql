1af103ec95d3d21d30fc10409c2b37bb8cb23879
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-06-17 05:54:35 +0000

    Sync LeetCode submission - Customers Who Never Order (mysql)

--- problems/customers_who_never_order/solution.sql
+++ problems/customers_who_never_order/solution.sql
@@ -1,2 +1,17 @@
 # Write your MySQL query statement below
-SELECT name as 'Customers' FROM Customers WHERE id NOT IN (SELECT customerId from Orders ) 
+
+
+# subquery
+
+# SELECT name as 'Customers' FROM Customers WHERE id NOT IN (SELECT customerId from Orders ) 
+
+
+
+
+# left join
+
+select name as 'Customers'
+from Customers
+left join Orders
+on Customers.id = Orders.customerId
+where (Orders.id) IS NULL    -- always use IS with NULL ,  = NULL wont work here
