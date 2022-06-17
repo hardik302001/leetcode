81636bfacc57cb1e80a8646c028268009e74b5e8
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-06-17 05:52:04 +0000

    Sync LeetCode submission - Customer Placing the Largest Number of Orders (mysql)

---
+++ problems/customer_placing_the_largest_number_of_orders/solution.sql
@@ -0,0 +1,10 @@
+SELECT customer_number
+FROM orders
+GROUP BY customer_number
+HAVING COUNT(order_number) = (
+	SELECT COUNT(order_number) as cnt
+	FROM orders
+	GROUP BY customer_number
+	ORDER BY cnt DESC
+	LIMIT 1
+)
