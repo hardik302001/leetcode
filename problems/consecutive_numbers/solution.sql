24f4f64cc1e3bb8cb676d82e663a48e14c7984cf
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-06-18 09:36:56 +0000

    Sync LeetCode submission - Consecutive Numbers (mysql)

---
+++ problems/consecutive_numbers/solution.sql
@@ -0,0 +1,16 @@
+# also see : https://leetcode.com/problems/human-traffic-of-stadium/submissions/
+
+# 3 self join
+
+SELECT DISTINCT
+    l1.Num AS ConsecutiveNums
+FROM
+    Logs l1,
+    Logs l2,
+    Logs l3
+WHERE
+    l1.Id = l2.Id - 1
+    AND l2.Id = l3.Id - 1
+    AND l1.Num = l2.Num
+    AND l2.Num = l3.Num
+;
