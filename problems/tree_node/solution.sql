84aec616fb67fd750825076b81edd893c2fbce7b
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-06-18 04:23:22 +0000

    Sync LeetCode submission - Tree Node (mysql)

---
+++ problems/tree_node/solution.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select id , (case 
+ when p_id is null then 'Root'
+ when id in (select p_id from tree where p_id is not null) then 'Inner'
+ else 'Leaf' end) 
+ as Type
+from Tree
