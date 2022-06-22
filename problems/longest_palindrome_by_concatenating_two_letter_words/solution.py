aa8a1a133f0e47a2cc1f1f57140dc69417ca1130
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-06-22 05:19:47 +0000

    Sync LeetCode submission - Longest Palindrome by Concatenating Two Letter Words (python3)

--- problems/longest_palindrome_by_concatenating_two_letter_words/solution.py
+++ problems/longest_palindrome_by_concatenating_two_letter_words/solution.py
@@ -1,37 +1,31 @@
 class Solution:
     def longestPalindrome(self, words: List[str]) -> int:
         d = {}
-        samee = []
-        sameo = []
-        diff = []
-        
-        ans = 0
         for i in words:
             if i in d:
                 d[i]+=1
             else:
-                d[i] = 1
-        f = 0
-        for i in d:
-            if i[::-1] in d and d[i[::-1]]>0:
-                if i!=i[::-1]:
-                    p = min(d[i], d[i[::-1]])
-                    ans = ans + 4*p     
-                    d[i]= d[i] - p
-                    d[i[::-1]]= d[i[::-1] ]- p
-                else:
-                    if d[i]&1:
-                        sameo.append(d[i])
-                    else:
-                        ans = ans + 2*d[i]
-        # print(sameo)
-        for i in sameo:
-            f = 1
-            ans = ans + (i-1)*2
-            
-        if f:
-            ans = ans + 2
-        
+                d[i] =1 
+        ans = 0
+        ext = 0  # len of word that will come in middle
+        # this word will be of form "xx"
+        for cur in d:
+            rev = cur[::-1]
+            if rev!=cur:
+                if rev in d and d[rev]>0:
+                    cnt = min(d[cur], d[rev])
+                    ans+= cnt*len(cur)*2
+                    d[cur]-=cnt
+                    d[rev]-=cnt
+            else:
+                cnt = d[cur]
+                pairs = cnt//2
+                ans+= pairs*2*len(cur)
+                if cnt%2==1:
+                    ext = max(ext , len(cur))  # maximum extra length possible
+                    # the fact that ques has only 2 length word, so we can relace it by 2
                     
-        return ans
-            
+                d[cur] = 0
+        ans+=ext
+        
+        return ans 
