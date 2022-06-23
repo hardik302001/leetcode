95c0eee88d5b268348c760beeaba1ea957ff82fa
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-06-23 03:32:11 +0000

    Sync LeetCode submission - Game Play Analysis I (mysql)

--- problems/game_play_analysis_i/solution.sql
+++ problems/game_play_analysis_i/solution.sql
@@ -3,6 +3,8 @@
 
 
 
+# see how beuatiifully group by works and min
+# similar logic used in :https://leetcode.com/problems/delete-duplicate-emails/
 
 select player_id , min(event_date) as 'first_login'
 from Activity
