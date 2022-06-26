53063c1d817a77a6795060e2b6d384f052ff94c6
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-06-26 03:31:41 +0000

    Sync LeetCode submission - Maximum Points You Can Obtain from Cards (cpp)

--- problems/maximum_points_you_can_obtain_from_cards/solution.cpp
+++ problems/maximum_points_you_can_obtain_from_cards/solution.cpp
@@ -143,3 +143,30 @@ public:
         
     }
 };
+
+
+/*
+same thing , sliding window
+
+# minimise (n-k)
+
+class Solution:
+    def maxScore(self, arr: List[int], k: int) -> int:
+        
+        l = 0
+        s = 0
+        n = len(arr)
+        if n==k:
+            return sum(arr)
+        
+        ans = 10000000000000
+        for r in range(n):
+            s+=arr[r]
+            if r-l+1==(n-k):
+                ans = min(ans , s)
+                s-=arr[l]
+                l+=1
+        # print(ans)
+        return sum(arr)-ans
+
+*/
