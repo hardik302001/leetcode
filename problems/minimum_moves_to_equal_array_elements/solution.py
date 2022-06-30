878ce17dfda85cf40f4b4f2b0a400e72022fb739
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-06-30 04:01:22 +0000

    Sync LeetCode submission - Minimum Moves to Equal Array Elements (python3)

--- problems/minimum_moves_to_equal_array_elements/solution.py
+++ problems/minimum_moves_to_equal_array_elements/solution.py
@@ -1,20 +1,17 @@
 '''
-Visualize the nums array as a bar graph where the value at each index is a bar of height nums[i]. Sort the array such that the bar at index 0 is minimum height and the bar at index N-1 is highest.
-Now in the first iteration, make a sequence of moves such that the height at index 0 is equal to height at index N-1. Clearly this takes nums[N-1]-nums[0] moves. After these moves, index N-2 will be the highest and index 0 will still be the minimum and nums[0] will be same as nums[N-1].
-In the next iteration, lets do nums[N-2]-nums[0] moves. After this iteration, nums[0], nums[N-2], and nums[N-1] will be the same.
+This is a really great math question that should have been categorized as "Medium". The real trick lies in this - "Increasing n - 1 smaller elements by 1 = decreasing only the max element by 1". 
+decrease 1 to reach min element,
+so ans = summation(for every element to reach min)
+O(n)
+
 '''
 
 class Solution(object):
     def minMoves(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        nums.sort()
+        m = min(nums)
         c = 0
-        for i in range(len(nums)-1, -1, -1):
-            if nums[i] == nums[0]:
-                break
-            c += nums[i] - nums[0]
+        n = len(nums)
+        for i in range(n):
+            c += nums[i] - m
         return c
 
