4c48c7f7a28281d84c524949a3d5d4e16ed3ee36
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-01 02:56:16 +0000

    Sync LeetCode submission - Maximum Units on a Truck (python3)

--- problems/maximum_units_on_a_truck/solution.py
+++ problems/maximum_units_on_a_truck/solution.py
@@ -1,7 +1,7 @@
 class Solution:
     def maximumUnits(self, box: List[List[int]], truckSize: int) -> int:
-        box.sort(key=lambda x:x[1],reverse=True)        #we want maximum noofunits only..so our priority to maximise it..
-        print(box)
+        box.sort(key=lambda x:-x[1])        #we want maximum noofunits only..so our priority to maximise it..
+        
         s=0
         for i,j in box:
             i=min(i,truckSize)
