a0ee6a31dede4cabae7fe24f813df25c5ef7c323
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-01 10:16:41 +0000

    Sync LeetCode submission - Partition Equal Subset Sum (cpp)

--- problems/partition_equal_subset_sum/solution.cpp
+++ problems/partition_equal_subset_sum/solution.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     
+    int dp[201][20001];
     bool canPartition(vector<int>& nums) {
         int sum = 0;
         for (auto a : nums) // Sum up the array
@@ -10,39 +11,20 @@ public:
             return false;
         
         sum /= 2;
-        
-        bool ans = isSubsetSum(nums, nums.size(), sum);
+        memset(dp , -1 , sizeof dp);
+        bool ans = isSubsetSum(0 , 0 , nums, nums.size(), sum);
         return ans;
     }
     
-    bool isSubsetSum(vector<int>&set, int n, int sum)
+    bool isSubsetSum(int idx ,  int cur , vector<int>&nums, int n, int sum)
     {
-        // The value of subset[i][j] will be true if
-        // there is a subset of set[0..j-1] with sum
-        // equal to i
-        bool subset[n + 1][sum + 1];
-
-        // If sum is 0, then answer is true
-        for (int i = 0; i <= n; i++)
-            subset[i][0] = true;
-
-        // If sum is not 0 and set is empty,
-        // then answer is false
-        for (int i = 1; i <= sum; i++)
-            subset[0][i] = false;
-
-        // Fill the subset table in bottom up manner
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= sum; j++) {
-                if (j < set[i - 1])
-                    subset[i][j] = subset[i - 1][j];
-                if (j >= set[i - 1])
-                    subset[i][j] = subset[i - 1][j]
-                                   || subset[i - 1][j - set[i - 1]];
-            }
-        }
-
-
-        return subset[n][sum];
+        if(idx>=n) return false;
+        if(cur>sum) return false;
+        
+        if(cur==sum) return true;
+        if(dp[idx][cur]!=-1) return dp[idx][cur];
+        bool ans =  isSubsetSum(idx+1, cur , nums , n , sum) or isSubsetSum(idx+1 , cur + nums[idx] , nums, n,  sum);
+        
+        return dp[idx][cur] = ans;
     }
 };
