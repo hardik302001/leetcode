6ee7cd1f256efe6d1cd569e6d4c9f801b21b40aa
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-03 18:42:19 +0000

    Sync LeetCode submission - Sliding Window Maximum (cpp)

--- problems/sliding_window_maximum/solution.cpp
+++ problems/sliding_window_maximum/solution.cpp
@@ -1,45 +1,30 @@
 //SLIDING WINDOW + DEQUE
 //SIMILAR PROBEM : https://leetcode.com/problems/shortest-subarray-with-sum-at-least-k/ 
-
-
-
-
-//also do: https://leetcode.com/problems/jump-game-vi/    , very similar , do chcek it out!
-
-
-
+// also see: https://leetcode.com/problems/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/
+//also do: https://leetcode.com/problems/jump-game-vi/    , very similar , do check it out!
 
 //also do : https://leetcode.com/problems/constrained-subsequence-sum/
+
+
 
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int n=nums.size();
-        vector<int> ans(n-k+1,0);
-        
-        deque<int> dq;       //max deque, dq.front(), gives max element
-
-        
-        for(int i=0;i<k;i++){
-            while(!dq.empty() && nums[dq.back()]<=nums[i])
-                dq.pop_back(); //Pop indexes from rear/back end
-            dq.push_back(i);           //push latest maximum index 
-        }
-        
-        for(int i=k;i<n;i++){
-            ans[i-k]=nums[dq.front()];
-            //Pop from back rear end as we are concerned with max number only
-            while(!dq.empty() && nums[dq.back()]<=nums[i]) 
-                dq.pop_back();          //pop indexes
+        deque<int> dq;
+        vector<int> ans;
+        for (int i=0; i<nums.size(); i++) {
+            // add current
+            while (!dq.empty() && nums[dq.back()] < nums[i])
+                dq.pop_back();
             dq.push_back(i);
             
+            // remove extra
+            if (!dq.empty() && dq.front() == i-k) dq.pop_front();  // aubarray size shud be k only
             
-            if(!dq.empty() && dq.front() ==i-k) //If elements are out of range k
-                dq.pop_front(); //pop from front
+            // update answer
+            if (i>=k-1) ans.push_back(nums[dq.front()]);  // ==(k-1) , we got subarray of size k
             
         }
-        ans[n-k]=nums[dq.front()];
         return ans;
     }
 };
-
