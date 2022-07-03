a0823efb0b270a59b7e96104560ed8a0908efb0c
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-03 19:03:56 +0000

    Sync LeetCode submission - Constrained Subsequence Sum (cpp)

--- problems/constrained_subsequence_sum/solution.cpp
+++ problems/constrained_subsequence_sum/solution.cpp
@@ -1,13 +1,16 @@
 // same as sliding window maximum
 // same as jump game 6 
 
+// O(n): deque solution
+// O(nlogn): pq soln can also be done
+
 class Solution {
 public:
     int constrainedSubsetSum(vector<int>& nums, int k) {
         
         int n=nums.size();
         vector<int> dp = nums;  // dp[i], gives maximum constrainted subseq sum till ith index
-        int ans = dp[0];
+        int ans = dp[0];  
         deque<int> dq;       //max deque, dq.front(), gives max element
         
         for(int i=0;i<n;i++){
@@ -20,6 +23,8 @@ public:
             
             if(!dq.empty() && dq.front() ==i-k) //If elements are out of range k
                 dq.pop_front(); //pop from front
+            
+            
             ans = max(ans , dp[i]);    // taking max of all dp[i]
         }
         
