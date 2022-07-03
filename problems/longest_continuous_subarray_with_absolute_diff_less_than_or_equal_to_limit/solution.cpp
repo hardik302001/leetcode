4beb85bc644e5f1746107ee12a489b7acd146d76
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-03 18:44:52 +0000

    Sync LeetCode submission - Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit (cpp)

--- problems/longest_continuous_subarray_with_absolute_diff_less_than_or_equal_to_limit/solution.cpp
+++ problems/longest_continuous_subarray_with_absolute_diff_less_than_or_equal_to_limit/solution.cpp
@@ -13,13 +13,15 @@ public:
         int ans = 0;
         
         while ( i < nums.size()) {
+            
+            // add current
             while (!maxdq.empty() && nums[i] >= nums[maxdq.back()]) maxdq.pop_back();
             while (!mindq.empty() && nums[i] <= nums[mindq.back()]) mindq.pop_back();
             
             maxdq.push_back(i);
             mindq.push_back(i);
             
-            // reduce the window size until condition is fulfilled
+            // update , reduce the window size until condition is fulfilled
             if (nums[maxdq.front()] - nums[mindq.front()] > limit) {
                 j++;
                 if (j > maxdq.front()) maxdq.pop_front();
