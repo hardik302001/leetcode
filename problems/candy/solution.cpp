9ca441fa96eb63c4a1627bb215424308470541d6
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-04 02:33:37 +0000

    Sync LeetCode submission - Candy (cpp)

---
+++ problems/candy/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int candy(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> l(n, 1);
+        vector<int> r(n, 1);
+        
+        for (int i = 1; i < n; i++)
+            if (nums[i] > nums[i - 1]) l[i] = l[i - 1] + 1;
+        for (int i = n - 2; i >=0; i--)
+            if (nums[i] > nums[i + 1]) r[i] = r[i + 1] + 1;
+        
+        
+        int res = 0;
+        for (int i =0;i<n;i++){
+            res+= max(l[i], r[i]);
+        }
+        return res;
+    }
+};
