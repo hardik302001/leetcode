d3a5a8c24e9bc950968ef260d17b094637314657
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-06 12:04:39 +0000

    Sync LeetCode submission - Trapping Rain Water (python3)

--- problems/trapping_rain_water/solution.py
+++ problems/trapping_rain_water/solution.py
@@ -1,37 +1,32 @@
+
+# see cpp for O(1) pace solution , more optimised
+
+
+# TC: O(n)
+# SC: O(n)
+
+
+
 class Solution:
-
     def trap(self, height: List[int]) -> int:
-
-        left = []   #max from left
-
-        right = []  #max from right   (dont forget to reverse it)
-
+        left = []   # max from left
+        right = []  # max from right   (dont forget to reverse it)
         ma1 = 0
-
         ma2 = 0
-
+        
         for i in range(len(height)):
-
             ma1 = max(ma1, height[i])
-
             ma2 = max(ma2, height[len(height)-1-i])
-
             left.append(ma1)
-
-            right.append(ma2) #we are pushing max from right side at starting(left) so we need to reverse it
-
+            right.append(ma2) # we are pushing max from right side at starting(left) so we need to reverse it
         print(left)
-
         print(right)
-
         print(height)
-
         s = 0
-
         for i in range(len(height)):
-
-            s = s + min(left[i],right[len(height) - i - 1]) - height[i]      #reversing the right array while considering min
-
+            s = s + min(left[i],right[len(height) - i - 1]) - height[i]      # reversing the right array while considering min
+            
+            
         return s
 
             
