53acd0b46ca648696d50846573ba44baa29c64c9
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-06 12:11:38 +0000

    Sync LeetCode submission - Longest Consecutive Sequence (python3)

--- problems/longest_consecutive_sequence/solution.py
+++ problems/longest_consecutive_sequence/solution.py
@@ -1,15 +1,21 @@
 
+# do read all solutions from SOLUTION SECTION!
+# T.C. =  O(n)
+
+
 class Solution:
     def longestConsecutive(self, nums):
         longest_streak = 0
         num_set = set(nums)
 
         for num in num_set:
-            if num - 1 not in num_set:          #we are looking for starting of sequence...
+            
+            # if num is starting of the sequence then ( num - 1) should not exist in the set
+            if num - 1 not in num_set:          # we are looking for starting of sequence...
                 current_num = num
                 current_streak = 1
 
-                while current_num + 1 in num_set:
+                while current_num + 1 in num_set: # start looking for (num+1)'s and update num
                     current_num += 1
                     current_streak += 1
 
@@ -19,33 +25,5 @@ class Solution:
     
     
 # sort and find longest streak , nlogn
-# do read all solutions from SOLUTION SECTION!
-
-# T.C. =  O(n)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
