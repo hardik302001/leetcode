2b43b5b466c9755650b4e2cb3434162d14a72822
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-07 06:41:11 +0000

    Sync LeetCode submission - Longest Binary Subsequence Less Than or Equal to K (cpp)

--- problems/longest_binary_subsequence_less_than_or_equal_to_k/solution.cpp
+++ problems/longest_binary_subsequence_less_than_or_equal_to_k/solution.cpp
@@ -11,10 +11,11 @@ public:
             if(s[i]=='1'){
                 if (!f or (n-i)>31) continue;   // (n-i) = count of digits seen , so 1 based indexing
                 cur+= (1<<(n-1-i));            // (1<<(n-1-i)) = pow( 2 , (n-i-1)) , 0 based indexing 
-                if(cur>k){
+                if(cur>k){  // if it cant handle it now, means it also cant handle it in future, so flag = 0, no more 1's needed 
+                                // it means time up, so we need to only conisder 0 from now..
                     f = 0;
                 }else{
-                    c++;
+                    c++;   // we can add current 1, 
                 }
             }else{
                 c++;
