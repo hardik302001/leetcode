ec39d18cdbb19ef707e5754582ebdebddfd3ccad
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-07 06:15:19 +0000

    Sync LeetCode submission - Find All Numbers Disappeared in an Array (cpp)

--- problems/find_all_numbers_disappeared_in_an_array/solution.cpp
+++ problems/find_all_numbers_disappeared_in_an_array/solution.cpp
@@ -1,6 +1,5 @@
 //very similar to : https://leetcode.com/problems/find-all-duplicates-in-an-array/
 
-
 /*
 Since, each element in the array is in range [1,n], it means we can use array elements as index for the same array.
 
@@ -8,6 +7,7 @@ So the idea is to use array element as indexes, and mark those indexes, somehow,
 
 In the end, unmarked indexes will indicate that these elements are missing in the array.
 */
+
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
@@ -16,7 +16,7 @@ public:
         for( int i=0; i < nums.size(); i++)
         {
             int n = abs(nums[i]); 
-            if(nums[n-1] > 0)
+            if(nums[n-1] > 0)   // bcz if this condition do not exist then at end we will get nums[i]>0 for those numbers that are removed and repeated also, but we only want that are removed..
                 nums[n-1] = -nums[n-1]; // marking -ve
         }
         for( int i=0; i < nums.size(); i++)
