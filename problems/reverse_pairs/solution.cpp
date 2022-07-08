4fb69c3e41e352a0850ede7e115f6e93ead42895
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-08 13:12:03 +0000

    Sync LeetCode submission - Reverse Pairs (cpp)

--- problems/reverse_pairs/solution.cpp
+++ problems/reverse_pairs/solution.cpp
@@ -1,13 +1,14 @@
 // similar to : https://leetcode.com/problems/count-of-smaller-numbers-after-self/
 // see striver video, inversion count , reverse pairs
+// https://takeuforward.org/data-structure/count-inversions-in-an-array/
 
+// TC = O(nlogn)
+// SC = O(n)
 
 class Solution {
 public:
 
     int merge(vector<pair<int, int>>&vp , int start , int mid , int end){
-        
-        
         // CALCULATING 
         int c = 0;
         int j = mid+1;
@@ -16,13 +17,11 @@ public:
             while(j<=end and vp[i].first>2LL*vp[j].first){
                 j++;
             }
-            c += (j-(mid+1));
+            c += (j-(mid+1));  // bcz [start, mid] and [mid+1, end] are sorted
+            // so for start<=x<=end, x>2*(some y number of elements in [mid+1, end]) , then we can say start<= x+1<=end , will surely be greater than atleast y elements, and we will continue to look for new elements in [mid+1, end] holding nums[x+1]<2*nums[j] from the same index we left before...
         }
         
-        
-        
-        // doing the merge sort part
-         
+        // ----------------- doing the merge sort part ---------------------
         vector<pair<int, int>>temp(end-start+1);     // to join the 2 sorted partitions
         i = start; // start of left partition
         j = mid+1; // start of right partition
@@ -43,8 +42,7 @@ public:
         // now adding remaining elements to our sorted temp..
         
         while(i<=mid) temp[k++] = vp[i++]; 
-        while(j<=end) temp[k++] = vp[j++];
-        
+        while(j<=end) temp[k++] = vp[j++];  
     
         // now we update our value-pair(vp) to temp, why because we will pass vp for our next recursion only..
         i = start;
@@ -58,7 +56,7 @@ public:
     
     int mergesort(vector<pair<int , int>>&vp, int l , int r){
         int inv_count = 0;
-        if(l<r){
+        if(l<r){             // if , not while   (l<r) not equal
             int m = (l+r)/2;
             inv_count+= mergesort(vp, l , m);
             inv_count+= mergesort(vp , m+1 , r);
