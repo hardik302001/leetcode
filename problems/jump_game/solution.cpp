849154a0ad99fe15a30732771c2b604b6e33805d
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-09 03:55:07 +0000

    Sync LeetCode submission - Jump Game (cpp)

--- problems/jump_game/solution.cpp
+++ problems/jump_game/solution.cpp
@@ -1,46 +1,13 @@
-// greedy
-
-class Solution{
-public: 
-    bool canJump(vector<int>& nums){
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int m = 0;  // bcz initially it is at nums[0]
         int n = nums.size();
-        int canreach = 0;
-        
+        if(n==1) return true;
         for(int i = 0;i<n;i++){
-            if(i>canreach){
-                return false;
-            }
-            canreach = max(canreach , i + nums[i]);
+            if(i>m) return false; 
+            m = max( m , i+ nums[i]);
         }
-        
         return true;
-        
-    }   
+    }
 };
-
-
-
-// // brute force
-
-// class Solution {
-// public:
-//     bool canJump(vector<int>& nums) {
-//         int n = nums.size();
-//         vector<bool> jump(n,false);
-//         jump[n-1]=true;
-        
-//         for(int i=n-2;i>=0;i--)
-//         {
-//             for(int j=0;j<=nums[i] && j<n;j++)
-//             {
-//                 if(jump[i+j]==true)     
-//                 {
-//                     jump[i]=true; 
-//                     break;
-//                 }
-//             }
-//         }
-
-//         return jump[0];
-//     }
-// };
