49ff723fa1c5bd4423b7831f88a118e5490d0cce
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-09 04:22:37 +0000

    Sync LeetCode submission - Jump Game II (cpp)

--- problems/jump_game_ii/solution.cpp
+++ problems/jump_game_ii/solution.cpp
@@ -4,19 +4,15 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         int n = nums.size();
-        vector<int>steps(n, INT_MAX);
-        steps[0] = 0;
-        for(int i = 0;i<n-1;i++){
-            for(int j = i+1; j<min(i + 1 +  nums[i], n);j++){
-                steps[j] = min(steps[j] , 1 + steps[i]);
-        //      for(int i = 0;i<n;i++){
-        //          cout<<steps[i]<<" ";
-        //      }
-        //      cout<<endl;
-            }
+        vector<int>dp(n+1, INT_MAX);
+        dp[0] = 0;      // we are standing at 0th index
+        
+        for(int i = 0 ;i<n;i++){
+            int next = i + nums[i];
+            for(int j = i; j<=min(n , next);j++) dp[j] = min(dp[j], 1 + dp[i]);
         }
-
-        return steps[n-1];
+        
+        return dp[n-1];
     }
 };
 
@@ -24,14 +20,12 @@ public:
 
 
 // greedy
-/*
-#  Solution 2: Greedy
 
-# The main idea is based on greedy.
-# Step 1: Let's say the range of the current jump is [left, right], farthest is the farthest position that all positions in [left, right] can reach.
-# Step 2: Once we reach to right, we trigger another jump with left = right + 1, right = farthest, then repeat step 1 util we reach at the end.
-*/
+// The main idea is based on greedy.
+// Step 1: Let's say the range of the current jump is [left, right], farthest is the farthest position that all positions in [left, right] can reach.
+// Step 2: Once we reach to right, we trigger another jump with left = right + 1, right = farthest, then repeat step 1 util we reach at the end.
 // BFS LIKE SOLUTION
+
 class Solution{
 public:
     int jump(vector<int>&nums){
@@ -40,8 +34,8 @@ public:
         int right = 0;
         int farthest = 0;
         int jump = 0;
-        while( right < n-1 ) {
-            for(int i = left; i<=right;i++){
+        while( right < n-1 ) { // we break as it reaches (n-1) index
+            for(int i = left; i<=right;i++){ // from out current position we can jump from [left, right] only. 
                 farthest = max(farthest , i + nums[i]);
             }
             left = right +  1;
