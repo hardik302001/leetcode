4379f9ca8b0b6eda4bb0e68f990779fc9c5a2277
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-09 05:34:11 +0000

    Sync LeetCode submission - Jump Game VI (cpp)

--- problems/jump_game_vi/solution.cpp
+++ problems/jump_game_vi/solution.cpp
@@ -1,41 +1,58 @@
-// grreedy wont work  (WA)
-// [0,-1,-2,-3,1]
-// 2
+/*
+greedy wont work  (WA)
 
-// expected -1
-// got -2
+[0,-1,-2,-3,1]
+2
 
-// recursion gives tle
+expected -1
+got -2
+
+*/
+
+// ------------------------------------------------------------------
+
+/*
+recursion gives tle    O(n^k)
+*/
+
+// --------------------------------------------------------------
+
 // dp + memo gives tle too O(n*k) = 10*9
 
-// class Solution {
-// public:
+/*
+class Solution {
+public:
+  // dp[i] will denote the maximum score that we can obtain starting from ith index. 
+    vector<int>dp = vector<int>(100001, INT_MIN);  // maximum sum till index i      
+    // recursive solver which finds max score to reach n-1 starting from ith index
     
-//     vector<int>dp = vector<int>(100001, INT_MIN);      
-//     // recursive solver which finds max score to reach n-1 starting from ith index
-    
-//     int recur(vector<int>& nums, int k, int i){
-//         if(dp[i] != INT_MIN) return dp[i];   // already calculated result for index i
-// 	    for(int j = 1; j <= k; j++)          // try jumps of all length and choose the one which maximises the score
-// 		    if(i + j < nums.size())
-// 			    dp[i] = max(dp[i], nums[i] + recur(nums, k, i + j));
-// 	    return dp[i];
-//     }
+    int recur(vector<int>& nums, int k, int i){
+        if(dp[i] != INT_MIN) return dp[i];   // already calculated result for index i
+	       for(int j = 1; j <= k; j++)          // try jumps of all length and choose the one which maximises the score
+		        if(i + j < nums.size())
+			        dp[i] = max(dp[i], nums[i] + recur(nums, k, i + j));
+	    return dp[i];
+    }
     
 
-//     int maxResult(vector<int>& nums, int k) {
-//         int n = nums.size();
-//         dp[n-1] = nums[n-1];
-//         return recur(nums , k , 0 );
-//     }
+    int maxResult(vector<int>& nums, int k) {
+        int n = nums.size();
+        dp[n-1] = nums[n-1];   // maximum sum from index (n-1) is nums[n-1].
+        return recur(nums , k , 0 );
+    }
  
-// };
+};
 
+*/
 
+// ----------------------------------------------------------
 
 //so we use priority queue or deque for the same 
 // ULTIMATE EXPLANATION : https://leetcode.com/problems/jump-game-vi/discuss/1261207/Dry-Run-Picture-oror-Dp-with-Explanation-oror-C%2B%2B-oror-Deque-oror-PriorityQueue
 
+// O(nlogk)
+// priority solution
+/*
 class Solution {
 public:
     int maxResult(vector<int>& nums, int k) {
@@ -53,5 +70,42 @@ public:
          }
          return dp[nums.size()-1];
                
+    }
+};
+*/
+
+
+//----------------------------------------------------------------------
+
+
+// O(n)
+// deque solution
+// sliding window maximum
+// similar to : https://leetcode.com/problems/constrained-subsequence-sum/
+// there we have choice to choose or not choose, but here we have to choose
+
+class Solution {
+public:
+    int maxResult(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int> dp(n, 0);  // dp[i], gives maximum constrainted subseq sum till ith index
+        int ans = dp[0];  
+        deque<int> dq;       //max deque, dq.front(), gives max element
+        // dec deque
+        
+        for(int i=0;i<n;i++){
+            dp[i] = nums[i]+(dq.empty()?0:dp[dq.front()]);   // we have to choose from front() as dq.front() gives maximum
+            
+            // Pop from back rear end as we are concerned with max number only
+            while(!dq.empty() && dp[dq.back()]<=dp[i]) 
+                dq.pop_back();          //pop indexes
+            dq.push_back(i);
+            
+            if(!dq.empty() && dq.front() ==i-k) //If elements are out of range k
+                dq.pop_front(); //pop from front
+                        
+        }
+
+        return dp.back();  // we need maximum answer till last index
     }
 };
