6c069068d9349620152deb1f8285ba0f1782dc4a
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-11 17:24:28 +0000

    Sync LeetCode submission - Cheapest Flights Within K Stops (cpp)

--- problems/cheapest_flights_within_k_stops/solution.cpp
+++ problems/cheapest_flights_within_k_stops/solution.cpp
@@ -2,7 +2,7 @@
 
 // we dont need to consider cost here.. our priority is k nodes first..., we dont want nodes with small cost first here...
 
-
+/*
 #define INF 1000000000
 
 class Solution {
@@ -40,5 +40,38 @@ public:
         }
 
         return dis[dst]==INF?-1:dis[dst];
+    }
+};
+*/
+
+
+// Running K iterations would give the lowest cost given up to K flights.
+// However, the question is asking for the lowest cost given up to K transfers
+// so we will do (k+1) relaxing iterations
+
+#define INF 1000000000
+
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        
+        vector<int>dis(n+1, INF);
+        dis[src] = 0;
+        
+        for(int i = 1;i<=(k+1);i++){   // bcz k+1 relaxing iteration allowed
+            vector<int>tmp(dis);  // very important to use seperate array , bcz if we are updating dis while traversing, it will not give correct answer uptil kth iterations, but eventually it will give correct answer for nth iterations, bcz in total we have (n-1) iterations .. for (n-1) edges..
+            // see for more info: https://leetcode.com/problems/network-delay-time/
+            
+            for(auto edge: flights){
+                if(dis[edge[0]]!=INF and dis[edge[0]] + edge[2] < tmp[edge[1]]){
+                    tmp[edge[1]] = dis[edge[0]] + edge[2];
+                }
+            }
+            
+            dis = tmp;
+        }
+        
+        return dis[dst]==INF?-1:dis[dst];
+            
     }
 };
