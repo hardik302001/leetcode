da34d4034aca2de8dd26208dbec346e8e42ae7c6
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-12 10:57:32 +0000

    Sync LeetCode submission - Network Delay Time (cpp)

--- problems/network_delay_time/solution.cpp
+++ problems/network_delay_time/solution.cpp
@@ -1,3 +1,9 @@
+// also see: https://leetcode.com/problems/cheapest-flights-within-k-stops/
+
+
+// o(n+elogn)
+// pure djisktra
+/*
 #define INF 1000000000000
 #define ll long long
 class Solution {
@@ -20,10 +26,9 @@ public:
             for(auto edge:graph[node]){
                 if(d + edge.second<dis[edge.first]){
                     dis[edge.first] = d + edge.second;
-                    pq.push({dis[edge.first] , edge.first});
+                    pq.push({dis[edge.first] , edge.first});  // push edge
                 }
-            }
-            
+            }       
         }
         
         ll ma = 0;
@@ -33,5 +38,41 @@ public:
     
         return ma==INF?-1:ma;
     
+    }
+};
+
+*/
+// Dijkstra's Algorithm takes O(ElogN). Finding the minimum time required in signalReceivedAt takes O(N).
+// tc : O(n+elogn)  
+
+
+
+
+
+// O(ne)
+// bellman ford :  fucking constraints, giving TLE
+// hardly passed 
+
+
+#define INF 1000000000000
+#define ll long long
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<ll> dist(n+1, INF);   // bcz 1 based indexing
+        dist[k] = 0; 
+        for(int i = 1;i<=n-1;i++) {                     // do relaxing (n-1) times
+            for(auto it: times) {
+                if(dist[it[0]]!=INF and dist[it[0]] + it[2] < dist[it[1]]) {
+                    dist[it[1]] = dist[it[0]] + it[2]; 
+                }
+            }
+        }
+        ll ma = 0;
+        for(int i = 1;i<=n;i++){
+            ma=max(ma , dist[i]);
+        }
+    
+        return ma==INF?-1:ma;
     }
 };
