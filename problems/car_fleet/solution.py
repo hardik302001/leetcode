e2cb2b6778570a129f2b948a9b4db5fddcc9185e
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-13 14:23:06 +0000

    Sync LeetCode submission - Car Fleet (python3)

--- problems/car_fleet/solution.py
+++ problems/car_fleet/solution.py
@@ -1,14 +1,13 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-
             cars = []
             for i in range(len(position)):
                 cars.append((position[i], speed[i]))
 
-            cars.sort(key=lambda x: (x[0], x[1]), reverse=True) # desc order : closer to traget comes first
+            cars.sort(key=lambda x: (x[0], x[1]), reverse=True) # desc order : closer to target comes first
             stack = []
             for x, v in cars:
-                dist = target - x # remaning distance to tagret
+                dist = target - x # remaning distance to target
                 if not stack:
                     stack.append(dist/v) # arrivalTime = dist/v
                 elif dist/v > stack[-1]: # car is away from target as compared to prev car and it takes more time than prev car, so it will never cross it, so it will make a seperate fleet..car arrives late -> thus does not join previous fleet and forms its own fleet
