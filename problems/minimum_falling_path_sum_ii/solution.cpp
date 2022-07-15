96e232a2c5f19393ef768c2942208f49572f4579
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-15 05:54:07 +0000

    Sync LeetCode submission - Minimum Falling Path Sum II (cpp)

--- problems/minimum_falling_path_sum_ii/solution.cpp
+++ problems/minimum_falling_path_sum_ii/solution.cpp
@@ -1,11 +1,13 @@
-//we need not find minium for every elemenet in row, for each row, we will only find the first two min elements, and can use them alternately while considering the min path sum ..
+// we need not find minium for every elemenet in row, for each row, we will only find the first two min elements, and can use them alternately while considering the min path sum ..
 
-//secondly, we ust need path sum, irrespective of the way, so we could have gone from top to bottom , but in this codde we wen from botoom to top, filling the dp array.. dp[curip[curj] = prev dp[alpha][beta] + curr_element
-
-
-
+// secondly, we must need path sum, irrespective of the way, so we could have gone from top to bottom , but in this codde we went from botoom to top, filling the dp array.. 
+// dp[curi][curj] = prev dp[alpha][beta] + curr_element
 
 
+
+// very similar idea to : paint house 2 and paint house 3
+
+// tc: O(n*n)
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& mat) {
@@ -21,6 +23,8 @@ public:
         for(int i = m-2;i>=0;i--){
             int mini1 = INT_MAX, mini2 = INT_MAX;
             int ind = -1;      //index of mini1
+            
+            // find min of prev row
             for(int j = 0;j<n;j++){
                if(dp[i+1][j]<=mini1){
                    mini2 = mini1;
@@ -30,33 +34,21 @@ public:
                 else if(dp[i+1][j]<mini2){
                     mini2 = dp[i+1][j];
                 }            
-            
             }
-            
-            
-            
-            //minimum of prev dp row  (above)
-           
             
             //now prev dp min + cur cell
             for(int j = 0;j<n;j++){
                 if(ind==j){
-                    dp[i][j] = mat[i][j] + mini2;
+                    dp[i][j] = mat[i][j] + mini2;  // go with mini2
                 }
                 else{
-                    dp[i][j] = mat[i][j] + mini1; 
+                    dp[i][j] = mat[i][j] + mini1;  // else with mini1
                 }
             }
-        }
-        
-        for(int i = 0;i<m;i++){
-            for(int j= 0;j<n;j++){
-                cout<<dp[i][j]<<" ";
-            }
-            cout<<endl;
         }
         
       
+        // res = minimum of first row
         int res = INT_MAX;
         for(int j = 0;j<n;j++){
             res = min(res, dp[0][j]);
@@ -64,7 +56,3 @@ public:
         return res;
     }
 };
-
-
-
-
