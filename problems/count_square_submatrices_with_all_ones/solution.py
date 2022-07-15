6d145d8dc56875d1c65f635ab8bf50b28ba22744
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-15 11:28:40 +0000

    Sync LeetCode submission - Count Square Submatrices with All Ones (python3)

--- problems/count_square_submatrices_with_all_ones/solution.py
+++ problems/count_square_submatrices_with_all_ones/solution.py
@@ -5,7 +5,7 @@
 class Solution:
     def countSquares(self, matrix: List[List[int]]) -> int:
         
-        # DP[y][x] denotes the largest edge length of square, whose bootom right point is [y][x]
+        # DP[y][x] denotes the largest edge length of square, whose bottom right point is [x][y]
         dp_table = matrix
         
         h, w = len(matrix), len(matrix[0])
@@ -14,12 +14,14 @@ class Solution:
         for y in range(1, h):
             for x in range(1, w):
                 
-                if matrix[y][x]:
+                if matrix[y][x]==1:
                     
                     # Make sure matrix[y][x] is 1, thus square can cover current matrix cell.
                     # Update largest edge length of sqaure, whose bottom right point is [y][x]
                     
                     dp_table[y][x] = 1 + min(dp_table[y][x-1],dp_table[y-1][x-1],dp_table[y-1][x])
+        
+        print(dp_table)
         
         s = 0
         for i in dp_table:
