a31fc9fd7ce8b72b5a39339450ee3d0b6f12386e
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-15 11:14:49 +0000

    Sync LeetCode submission - Time Based Key-Value Store (cpp)

--- problems/time_based_key-value_store/solution.cpp
+++ problems/time_based_key-value_store/solution.cpp
@@ -13,28 +13,24 @@ public:
     
     string get(string key, int time) {
         if(um.count(key)){
+            // we need upper bound
             int start = 0, end = um[key].size();
             while(start < end) {
                 int mid = start + (end-start)/2;
-                if(um[key][mid].second > time)
-                    end = mid;
+                
+                if(um[key][mid].second <= time)   // cause we need upper bound
+                    start = mid+1;
                 else
-                    start = mid + 1;
+                    end = mid;
             }
             
             
-            //m[key][start].first would return the first occurrence of "next" value. To get the last occurrence we want to                   //previous value hence m[key][start-1].first.
+            //m[key][start].first would return the first occurrence of "next" value. To get the last occurrence we want to                   
+            //previous value hence m[key][start-1].first.
             //  if((start - 1) >= 0 and (start - 1) < m[key].size()) is to make sure we are not out of bounds.
             
-            return start > 0 and start <= um[key].size() ? um[key][start-1].first : "";
+            return (start> 0) ? um[key][start-1].first : "";
         }
         return "";
     }
 };
-
-/**
- * Your TimeMap object will be instantiated and called as such:
- * TimeMap* obj = new TimeMap();
- * obj->set(key,value,timestamp);
- * string param_2 = obj->get(key,timestamp);
- */
