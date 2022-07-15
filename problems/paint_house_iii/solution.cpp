828a68cc1c2e281ee3c0a4e1604494fa49d0273b
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-15 05:53:49 +0000

    Sync LeetCode submission - Paint House III (cpp)

--- problems/paint_house_iii/solution.cpp
+++ problems/paint_house_iii/solution.cpp
@@ -168,49 +168,61 @@ public:
 };
 
 OPTIMISED VERSION
+idea similar to : https://leetcode.com/problems/minimum-falling-path-sum-ii/
+
 Currently this solution takes O(nk^2). Iterating all combinations takes O(nk). Finding the minimum among colors takes O(k). The optimization here is to reduce finding minimum to O(1). I do not need to find minimum for every color, because all I need is the minimum and the next minimum. If current color is the same as the previous color, which happens to have the minimum cost, I can just use the second minimum
 
 TC: O(nk)
 // for more info : http://buttercola.blogspot.com/2015/09/leetcode-paint-house-ii.html
 class Solution {
 public:
-    int minCostII(vector<vector<int>> &costs) {
+    int minCostII(vector<vector<int>> &mat) {
 
-        int n  = costs.size();
+        int n  = mat.size();
         if(n==0) return 0;
         int ind = 0;
-        int k = costs[0].size();
-        vector<int>dp(k+1, 0); 
-        int min1 = 0;
-        int min2 = 0;
- 
-        for (int i = 0; i < n; i++) {
-            int oldMin1 = min1;
-            int oldMin2 = min2;
-             
-            min1 = INT_MAX;
-            min2 = INT_MAX;
-             
-            for (int j = 0; j < k; j++) {
-                if (dp[j] != oldMin1 || oldMin1 == oldMin2) {
-                    dp[j] = oldMin1 + costs[i][j];
-                } else {
-                    dp[j] = oldMin2 + costs[i][j];
+        int k = mat[0].size();
+        
+        vector<vector<int> > dp(n, vector<int>(k,0));
+        for(int j = 0;j<k;j++){
+            dp[n-1][j] = mat[n-1][j];     //last row, no variation
+        }
+        
+        for(int i = n-2;i>=0;i--){
+            int mini1 = INT_MAX, mini2 = INT_MAX;
+            int ind = -1;      //index of mini1
+            
+            // find min of prev row
+            for(int j = 0;j<k;j++){
+               if(dp[i+1][j]<=mini1){
+                   mini2 = mini1;
+                   mini1 = dp[i+1][j];
+                   ind = j;
+               }
+                else if(dp[i+1][j]<mini2){
+                    mini2 = dp[i+1][j];
+                }            
+            }
+            
+            //now prev dp min + cur cell
+            for(int j = 0;j<k;j++){
+                if(ind==j){
+                    dp[i][j] = mat[i][j] + mini2;  // go with mini2
                 }
-                 
-                if (min1 <= dp[j]) {
-                    min2 = min(min2, dp[j]);
-                } else {
-                    min2 = min1;
-                    min1 = dp[j];
+                else{
+                    dp[i][j] = mat[i][j] + mini1;  // else with mini1
                 }
             }
-             
         }
-         
-        return min1;
+        
+      
+        // res = minimum f first row
+        int res = INT_MAX;
+        for(int j = 0;j<k;j++){
+            res = min(res, dp[0][j]);
+        }
+        return res;
     }
 };
-
 
 */
