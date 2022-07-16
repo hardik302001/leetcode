1ec60d69b2626165a6dd7dc3dbb58f1d117201f7
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-16 18:20:21 +0000

    Sync LeetCode submission - Target Sum (cpp)

--- problems/target_sum/solution.cpp
+++ problems/target_sum/solution.cpp
@@ -1,23 +1,22 @@
-//see previous accepted code alos, that is based on rrecursion
-//this code is based on dp!
-//see aditya verma dp notes for more info
+// see previous accepted code alos, that is based on rrecursion
+// this code is based on dp!
+// see aditya verma dp notes for more info
 
-
+/*
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
         //basically target is difference of subsets
         //so we need to ocunt no of possible subsetwith given difference..
         
-        /*
-        S1 - S2 = target
-        S1 + S2  = sum(nums)
-        so S1 = [ target + sum(nums) ]//2
+       
+//         S1 - S2 = target
+//         S1 + S2  = sum(nums)
+//         so S1 = [ target + sum(nums) ]//2
         
         
         //now we need to find no of subsets with given sum S1..
-        
-        */
+
         
         int sum = accumulate(nums.begin(),nums.end(),0);
         int s1 = (target + sum)/2;
@@ -30,7 +29,7 @@ public:
         }
         
         
-        //secondly s1 should be positive bcz we are considering array of isze s1, what if s1<0.
+        //secondly s1 should be positive bcz we are considering array of size s1 for bottom up method, what if s1<0.
         
         if(s1<0){
             s1 = abs(s1);
@@ -59,14 +58,69 @@ public:
             
             }
         }
-        // for(int i = 0;i<m;i++){
-        //     for(int j = 0;j<n;j++){
-        //         cout<<dp[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
         
         return dp[m-1][n-1];
         
+    }
+};
+
+*/
+
+
+/*
+class Solution {
+public:
+    
+    int dp[21][2001];
+    int findTargetSumWays(vector<int>& nums, int target) {
+        //basically target is difference of subsets
+
+        int sum = accumulate(nums.begin(),nums.end(),0);
+        int s1 = (sum+ target)/2;
+        
+        
+        //s1 should not in fraction it shud be integer.
+        
+        if((target + sum)%2!=0){
+            return 0;
+        }
+
+
+        int n = nums.size();
+        memset(dp , - 1, sizeof dp);
+        return recur(0 , 0 , nums , n ,  s1);
+    }
+    
+     
+    int recur(int idx ,  int cur , vector<int>&nums, int n, int sum)
+    {
+        if(idx>n) return 0;
+        if(cur>sum) return 0;
+        if(idx==n) return (cur==sum)? 1 :0;
+        if(dp[idx][cur]!=-1) return dp[idx][cur];
+        int ans =  recur(idx+1, cur , nums , n , sum) + recur(idx+1 , cur + nums[idx] , nums, n,  sum);
+        
+        return dp[idx][cur] = ans;
+    }
+    
+    
+};
+
+*/
+
+// tc: O(n*s)
+
+class Solution {
+public:
+
+    int findTargetSumWays(vector<int>& nums, int S) {
+        vector<unordered_map<int,int>> mem(nums.size());
+        return find(0,nums,S,mem);    
+    }
+    int find(int p, vector<int>& nums, int sum, vector<unordered_map<int,int>>& mem) {
+        if(p==nums.size()) return sum==0;
+        auto it = mem[p].find(sum);
+        if(it != mem[p].end()) return it->second;
+        return mem[p][sum]=find(p+1,nums,sum+nums[p],mem)+find(p+1,nums,sum-nums[p],mem);
     }
 };
