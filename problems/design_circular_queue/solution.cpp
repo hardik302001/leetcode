20e29c91870fb08326c676705df669fe934a305f
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-18 06:43:35 +0000

    Sync LeetCode submission - Design Circular Queue (cpp)

--- problems/design_circular_queue/solution.cpp
+++ problems/design_circular_queue/solution.cpp
@@ -1,33 +1,20 @@
 class MyCircularQueue {
 public:
-    
-    int head, tail, cur_size, max_size;
-    vector<int> q;
-    
-    MyCircularQueue(int k) {
-        q.resize(k);
-        for(int i = 0;i<k;i++){
-            q[i] = 0;
-        }
-        head = 0;
-        tail = 0;
-        max_size = k;
-        cur_size = 0;
-    }
+    MyCircularQueue(int k) : head(0), tail(0), capacity(k), size(0), q(vector<int>(k, -1)) {}
     
     bool enQueue(int value) {
         if (isFull()) return false;
-        q[tail] = value;
-        tail = (tail + 1) % max_size;
-        cur_size++;
+        q[tail++] = value;
+        tail %= capacity;
+        size++;
         return true;
     }
     
     bool deQueue() {
         if (isEmpty()) return false;
-        q[head] = 0;
-        head = (head + 1) % max_size;              
-        cur_size--;
+        q[head++] = -1;
+        head %= capacity;
+        size--;
         return true;
     }
     
@@ -35,18 +22,20 @@ public:
         return isEmpty() ? -1 : q[head];
     }
     
-    int Rear() {
-        return isEmpty() ? -1 : tail == 0 ? q[max_size-1]: q[tail-1];
+    int Rear() { // tail = 0, means queue is full, so we return cap-1, else we do tail-1
+        // why index -1? bcz we are moving one step ahead after enqueue operation! 
+        return isEmpty() ? -1 : tail == 0 ? q[capacity-1]: q[tail-1];
     }
     
     bool isEmpty() {
-        return cur_size == 0;
+        return size == 0;
     }
     
     bool isFull() {
-        return cur_size == max_size;
+        return size == capacity;
     }
+    
+private:
+    int head, tail, size, capacity;
+    vector<int> q;
 };
-
-
-
