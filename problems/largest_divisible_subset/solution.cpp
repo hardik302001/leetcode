58cfac7ee791f7c016fb95f4f98181e6d99e4c82
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-20 17:28:40 +0000

    Sync LeetCode submission - Largest Divisible Subset (cpp)

--- problems/largest_divisible_subset/solution.cpp
+++ problems/largest_divisible_subset/solution.cpp
@@ -1,4 +1,4 @@
-//similar to :  https://leetcode.com/problems/longest-increasing-subsequence/
+// similar to :  https://leetcode.com/problems/longest-increasing-subsequence/
 
 // https://leetcode.com/problems/largest-divisible-subset/discuss/1578685/C%2B%2B-EASY-Solution-oror-Standard-LIS-Variation-oror-INTUITIVE-approach
 
@@ -8,33 +8,35 @@ public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int n = nums.size();
-        vector<int>dp(n,1);
-        vector<int>prev(n, -1); //previous index array to track for previous element in the dp before i
-        int end = 0;   //bcz if only one ;eement, we cam consider anyways
-        int m = 0;
+        int lastidx = 0;
+        int ans = 0;
+        vector<int>dp(n+1, 1), hash(n+1, -1);
         for(int i = 0;i<n;i++){
+            hash[i] = i;
             for(int j = 0;j<i;j++){
-                if(nums[i]%nums[j]==0 and dp[i]<dp[j]+1){
-                    dp[i] = dp[j]+1;
-                    prev[i] = j;
+                if(nums[i]%nums[j]==0 and dp[i]<1+dp[j]){
+                    dp[i] = 1 + dp[j];
+                    hash[i] = j;
                 }
-                
             }
-            
-            
-            if(dp[i]>m){        // we cant update m, anytime.. we need to update m to the max of dp vector
-                end = i;
-                m = dp[i];
+            if(ans<dp[i]){
+                ans = max(ans, dp[i]);
+                lastidx  = i;                
             }
+
         }
         
-        
-        vector<int>ans;
-        while(end!=-1){
-            ans.push_back(nums[end]);
-            end = prev[end];
+        vector<int>lis;
+        while(hash[lastidx]!=lastidx){
+            lis.push_back(nums[lastidx]);
+            lastidx =  hash[lastidx];
         }
         
-        return ans;
-    }
+        lis.push_back(nums[lastidx]);
+        
+        reverse(lis.begin(), lis.end());
+        return lis;        
+        
+    }	
 };
+
