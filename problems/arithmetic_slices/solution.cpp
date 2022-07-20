380ab9bd6d183ac36b2de08b6f34e29c64070ed1
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-20 15:03:54 +0000

    Sync LeetCode submission - Arithmetic Slices (cpp)

--- problems/arithmetic_slices/solution.cpp
+++ problems/arithmetic_slices/solution.cpp
@@ -1,53 +1,71 @@
+
 /*
-We find the sub problem:
-Assume A[i:j] (both include A[i] and A[j]) is an arithmetic slice, then we have:
+TIME COMPLEXITY
+O(N * N)
 
-if A[i]-A[i-1] = = A[i+1]-A[i], then A[i-1:j] is an arithmetic slice;
-if A[j+1]-A[j] = = A[j]-A[j-1], then A[i:j+1] is an arithmetic slice.
-use dp[i][j] to memorize whether A[i:j] is an arithmetic slice, and count to count the num of arithmetic slices:
+SPACE COMPLEXITY
+O(1)
+*/
+/*
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+		// if nums size is less than 3 return false
+        if(nums.size() < 3)
+            return 0;
+        
+        int cnt = 0, diff;
+        
+        for(int i = 0; i<nums.size()-2; ++i)
+        {
+			// storing diff of first 2 elements
+            diff = nums[i+1] - nums[i];
+			
+			// checking for consecutive elements with same difference.
+            for(int j = i+2; j<nums.size(); ++j)
+            {
+				// if we find the same diff of next 2 elements
+				// this means we  find consecutive elements
+				// increase the Count
+                if(nums[j] - nums[j-1] == diff)
+                    ++cnt;
+                else
+				// break as we need to cnt for consecutive diff elements
+                    break;
+            }
+        }
+		// return cnt
+        return cnt;
+    }
+};
 
-
-2nd DP Solution: time O(n), space O(n)
-We can find another sub problem: assume dp[i] is the number of arithmetic slices which are end with A[i]. then we have:
-dp[i]=(A[i]-A[i-1] = = A[i-1]-A[i-2])? 1+dp[i-1] : 0, the code:
 */
 
 
-// class Solution{
-// public:
-//     int numberOfArithmeticSlices(vector<int>&A) {
-//     int n=A.size();
-//     if(n<3){return 0;}
-//     vector<int>dp(n);
-//     dp[0]=0;
-//     dp[1]=0;
-//     int sum=0;
-//     for(int i=2;i<n;i++){
-//         if((A[i]-A[i-1])==(A[i-1]-A[i-2])){
-//             dp[i]=dp[i-1]+1;
-//         }else{
-//             dp[i]=0;
-//         }
-//         sum+=dp[i];
-//     }
-//     return sum;
-// }
-// };
 
-// up to now, time complexity is O(n), but space complexity is also O(n). In fact, we only need a curr to memorize the num of arithmetic slices which end with current A[i] and a sum to memorize num of all curr. that is @icl7722's solution, which time complexity is O(n) and space complexity is O(1).
+// --------------------------------------------------------
 
 
-class Solution{
+//  TC: O(n)
+
+// very similar to : https://leetcode.com/problems/number-of-smooth-descent-periods-of-a-stock/
+
+class Solution {
 public:
-    int numberOfArithmeticSlices(vector<int>& A) {
-	int curr = 0, sum = 0;
-	for (int i=2; i<A.size(); i++)
-		if (A[i]-A[i-1] == A[i-1]-A[i-2]) {
-			curr += 1;
-			sum += curr;
-		} else {
-			curr = 0;
-		}
-	return sum;
-}
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        if(nums.size() <= 2)return 0;
+        int count = 0, ans = 0;
+        for(int i = 0; i < nums.size() - 2; i++){
+            if(nums[i+1] - nums[i] == nums[i+2] - nums[i+1]){
+                count++;
+            }
+            else{
+                ans += ((count * (count+1)) / 2);
+                count = 0;
+            }
+            
+        }
+        if(count) ans += ((count * (count+1)) / 2);
+        return ans;
+    }
 };
