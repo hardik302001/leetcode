316da711eadba0f580e2d2f57de65e7095899f76
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-21 16:51:46 +0000

    Sync LeetCode submission - Word Ladder (cpp)

--- problems/word_ladder/solution.cpp
+++ problems/word_ladder/solution.cpp
@@ -1,48 +1,48 @@
 // same as: https://leetcode.com/problems/minimum-genetic-mutation/
-
+//  TC: O(26 * length of beginWord * number of words in wordList)
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-         set<string> dictionary;
-    for(auto word : wordList) {
-        dictionary.insert(word);
-    }
-    if(dictionary.find(endWord) == dictionary.end()) {
-        return 0;
-    }
-    
-    queue<string> bfs;
-    int result = 1;   // bcz we want length of sequence , not no. of transformations
-    bfs.push(beginWord);
-    
-    while(!bfs.empty()) {
-        int size = bfs.size();
-        while(size--) {
-            string temp = bfs.front();
-            bfs.pop();
-            if(temp == endWord)
-                return result;
-            
-            //comparing is done in O(26)*size of word here!
-            // see prev TLE pproachh there it was count of word of size(size of word)* size of word
-            for(int i = 0; i < temp.size(); i++) {
-                string current = temp;
-                for(char j = 'a'; j <= 'z'; j++) {
-                    current[i] = j; 
+        set<string> dictionary;   // similar to vis
+        for(auto word : wordList) {
+            dictionary.insert(word);
+        }
+        if(dictionary.find(endWord) == dictionary.end()) {
+            return 0;
+        }
 
-                    if(current == temp)
-                        continue;
+        queue<string> bfs;
+        int result = 1;   // bcz we want length of sequence , not no. of transformations
+        bfs.push(beginWord);
 
-                    if(dictionary.find(current) != dictionary.end()) {
-                        bfs.push(current);
-                        dictionary.erase(current);
+        while(!bfs.empty()) {
+            int size = bfs.size();
+            while(size--) {
+                string temp = bfs.front();
+                bfs.pop();
+                if(temp == endWord)
+                    return result;
+
+                //comparing is done in O(26)*size of word here!
+                // see prev TLE pproachh there it was count of word of size(size of word)* size of word
+                for(int i = 0; i < temp.size(); i++) {
+                    string current = temp;
+                    for(char j = 'a'; j <= 'z'; j++) {
+                        current[i] = j; 
+
+                        if(current == temp)
+                            continue;
+
+                        if(dictionary.find(current) != dictionary.end()) {
+                            bfs.push(current);
+                            dictionary.erase(current);
+                        }
                     }
                 }
             }
+
+            result++;
         }
-        
-        result++;
-    }
-    return 0;
+        return 0;
     }
 };
