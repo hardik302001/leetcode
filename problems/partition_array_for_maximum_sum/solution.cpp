efd322b6bbaf20dfef648824e809d5743c75d2ad
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-21 17:51:35 +0000

    Sync LeetCode submission - Partition Array for Maximum Sum (cpp)

---
+++ problems/partition_array_for_maximum_sum/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int dp[501];
+    int recur(int start , vector<int>&arr, int k){
+        if(start==arr.size()) return 0;
+        if(dp[start]!=-1) return dp[start];
+        
+        int mi = INT_MIN;
+        int ma = INT_MIN;
+        int len = 0;
+        for(int i = start ; i<min(int(arr.size()) ,start+k ); i++){
+            len++;
+            ma = max( ma, arr[i]);
+            int res = len*ma + recur(i+1, arr, k);
+            mi = max(mi , res);
+        }
+        return dp[start] = mi;
+        
+    }
+    
+    
+    int maxSumAfterPartitioning(vector<int>& arr, int k) {
+        memset(dp , -1 , sizeof dp);
+        return recur(0 , arr, k);  // max sum frm arr(i:n)
+    }
+};
