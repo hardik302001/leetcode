89ce2a3cd58d723b2bc5b713bd7a81147a1d851a
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-21 17:31:05 +0000

    Sync LeetCode submission - Palindrome Partitioning II (cpp)

--- problems/palindrome_partitioning_ii/solution.cpp
+++ problems/palindrome_partitioning_ii/solution.cpp
@@ -1,40 +1,35 @@
-/*
-Took me a while to understand. I'd like to help further explain it.
-
-The definition of 'cut' array is the minimum number of cuts of a sub string. More specifically, cut[n] stores the cut number of string s[0, n-1].
-
-Here is the basic idea of the solution:
-
-Initialize the 'cut' array: For a string with n characters s[0, n-1], it needs at most n-1 cut. Therefore, the 'cut' array is initialized as cut[i] = i-1
-
-Use two variables in two loops to represent a palindrome:
-The external loop variable 'i' represents the center of the palindrome. The internal loop variable 'j' represents the 'radius' of the palindrome. Apparently, j <= i is a must.
-This palindrome can then be represented as s[i-j, i+j]. If this string is indeed a palindrome, then one possible value of cut[i+j] is cut[i-j] + 1, where cut[i-j] corresponds to s[0, i-j-1] and 1 correspond to the palindrome s[i-j, i+j];
-
-When the loops finish, you'll get the answer at cut[s.length]
-*/
-
+// no of partitions needed for string(i:n)
 
 class Solution {
 public:
-    int minCut(string s) {
-        int n = s.size();
-        if(n==1){
-            return 0;
-        }
-        vector<int> cut(n+1, 0);  // number of cuts for the first k characters
-        for (int i = 0; i <= n; i++) cut[i] = i-1;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; i-j >= 0 and i+j < n and s[i-j]==s[i+j] ; j++) // odd length palindrome
-                cut[i+j+1] = min(cut[i+j+1],1+cut[i-j]);
-
-            for (int j = 1; i-j+1 >= 0 and i+j < n and s[i-j+1] == s[i+j]; j++) // even length palindrome
-                cut[i+j+1] = min(cut[i+j+1],1+cut[i-j+1]);
-            
-          
+    int dp[2001];
+    int recur(int start , int n , string &s){
+        if(start==n) return 0;
+        if(dp[start]!=-1) return dp[start];
+        
+        int mi = INT_MAX;        
+        for (int i = start; i < n; i++) {
+            if (isPalindrome(s, start, i)) { 
+                mi = min(mi , 1 + recur(i+1, n, s));
+            }
         }
         
-
-        return cut[n];
+        return dp[start] = mi;    
+    }
+    
+    
+    bool isPalindrome(string& s, int l, int r) {
+        while (l < r) {
+            if (s[l++] != s[r--]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    int minCut(string s) {
+        int n = s.size();
+        memset(dp , -1 , sizeof dp);
+        return recur(0 , n, s)-1;
     }
 };
