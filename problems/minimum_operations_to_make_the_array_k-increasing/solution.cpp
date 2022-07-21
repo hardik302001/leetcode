a4a790dafe8d856b791b543d9d848b0a2eaede08
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-21 10:31:30 +0000

    Sync LeetCode submission - Minimum Operations to Make the Array K-Increasing (cpp)

--- problems/minimum_operations_to_make_the_array_k-increasing/solution.cpp
+++ problems/minimum_operations_to_make_the_array_k-increasing/solution.cpp
@@ -1,56 +1,51 @@
-// see https://leetcode.com/problems/longest-increasing-subsequence/submissions/
-// for more info
+/*
+There are two observations we need to make.
+
+It is independent to consider subproblems on 0, k, 2k, ..., 1, k+1, 2k+1, ... and so on.
+For each subproblem it is enough to solve LIS (longest increasing(not strictly) subsequence) problem. Indeed, if we have LIS of length t, than we need to change not more than R-t elements, where R is size of our subproblem. From the other side, we can not change less number of elements, because if we changed < R- t elements it means that > t elements were unchanged and it means we found LIS of length > t. This is called estimate + example technique in math.
+
+Complexity
+It is O(n log n) for time and O(n) for space.
 
 
 
+*/
 
-//  https://leetcode.com/problems/minimum-operations-to-make-the-array-k-increasing/discuss/1634980/C%2B%2B-Longest-Increasing-Subsequence
 
-// Time: O(Nlog(N/k))
-// Space: O(N/k)
-// class Solution {
-// public:
-//     int kIncreasing(vector<int>& A, int k) {
-//         int N = A.size(), ans = 0;
-//         for (int i = 0; i < k; ++i) {
-//             vector<int> v{A[i]};
-//             int len = 1;
-//             for (int j = i + k; j < N; j += k) {
-//                 auto i = upper_bound(begin(v), end(v), A[j]);
-//                 if (i == end(v)) v.push_back(A[j]);
-//                 else *i = A[j];
-//                 ++len;
-//             }
-//             ans += len - v.size();
-//         }
-//         return ans;
-//     }
-// };
+// here we will take upper bound because , we can have equal element, so lower bound will overwrite the value equal t it, rater it should upadate the next one..
+// TC: [2,2,2,2,2,1,1,4,4,3,3,3,3,3]
+//     1
 
-// upper_bound solution
+
+
 class Solution {
 public:
-    int LIS(vector<int> &arr) {
-        vector<int> lis(arr.size(), INT_MAX);
-        int ans = 0;
-        for(int i = 0; i < arr.size(); i++) {
-            int idx = upper_bound(lis.begin(), lis.end(), arr[i]) - lis.begin();
-            ans = max(idx + 1, ans);
-            lis[idx] = arr[i];
-        }
-        return arr.size() - ans;
-    }
     int kIncreasing(vector<int>& arr, int k) {
-        vector<vector<int> > group(k);
-        for(int i = 0; i < k; i++) {
-            for(int j = i; j < arr.size(); j+=k) {
-                group[i].push_back(arr[j]);
-            }
-        }
+        int n = arr.size();
         int ans = 0;
-        for(auto &v : group) {
-            ans += LIS(v);
+        for (int i = 0; i < k; ++i) {
+            vector<int> newArr;
+            for (int j = i; j < n; j += k)
+                newArr.push_back(arr[j]);
+            ans += newArr.size() - longestNonDecreasingSubsequence(newArr);
         }
         return ans;
+    }
+    
+    
+    int longestNonDecreasingSubsequence(const vector<int>& arr) {
+        vector<int> sub;
+        sub.push_back(arr[0]);
+        for (int i = 1; i < arr.size(); ++i) {
+            int x = arr[i];
+            if (sub.back() <= x) { // Append to LIS if new element is >= last element in LIS
+                sub.push_back(x);
+            } else {
+                int idx = upper_bound(sub.begin(), sub.end(), x) - sub.begin(); // Find the index of the smallest number > x
+                sub[idx] = x; // Replace that number with x
+            }
+        }
+
+        return sub.size();
     }
 };
