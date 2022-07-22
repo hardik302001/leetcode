9d3be1f3ac7b0caab8d4dd72ff32c2391e66af58
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-22 05:04:53 +0000

    Sync LeetCode submission - Egg Drop With 2 Eggs and N Floors (cpp)

---
+++ problems/egg_drop_with_2_eggs_and_n_floors/solution.cpp
@@ -0,0 +1,31 @@
+// binary search wont work , bcz only 2 choices
+
+// DP , TC = O(n^2)
+
+class Solution {
+public:
+    
+    int dp[1001][1001];    
+    int recur(int lo , int hi){
+        if(lo>=hi) return 0;
+        
+        if(dp[lo][hi]!=-1) return dp[lo][hi];
+        int ans = INT_MAX;
+        for(int floorId = lo+1; floorId<=hi ; floorId++){
+            
+            // +1 bcz we do onemove for current floor , and rest part for future moves
+            int movesIfFirstEggBreaks = 1 + (floorId-lo-1);
+            int movesIfFirstEggDontBreaks = 1 + recur(floorId, hi);
+            
+            ans = min(ans , max(movesIfFirstEggBreaks , movesIfFirstEggDontBreaks));
+        }
+        return dp[lo][hi] = ans;
+    }
+    
+    
+    int twoEggDrop(int n) {
+        if(n==1) return 1;
+        memset(dp , -1 , sizeof dp);
+        return recur(0 , n);
+    }
+};
