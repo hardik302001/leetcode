e90cfdc45491c202af7a9e959928c5dc8a7ee044
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-23 14:12:43 +0000

    Sync LeetCode submission - Path Sum III (cpp)

--- problems/path_sum_iii/solution.cpp
+++ problems/path_sum_iii/solution.cpp
@@ -1,32 +1,30 @@
-//O(n)
-//idea is similar to LEETCODE: subarray sum equals k
-//we ar visiting every node once
+// O(n)
+// idea is similar to LEETCODE: subarray sum equals k
+// we are visiting every node once
 
-//  https://leetcode.com/problems/path-sum-iii/discuss/91908/C%2B%2B-solution-with-prefix-sum-stored-in-hash-table
-
+// https://leetcode.com/problems/path-sum-iii/discuss/91908/C%2B%2B-solution-with-prefix-sum-stored-in-hash-table
 
 class Solution {
-    unordered_map<int, int> hashMap;
-    int count;
-    int target;
-    void find(TreeNode* root, long long int  sum) {
-        if (root == 0) return;
-        sum += root->val;
-        int prefixSum = sum - target;
-        if (hashMap.count(prefixSum)) {
-            count += hashMap[prefixSum];
-        }
-        hashMap[sum]++;
-        find(root->left, sum);
-        find(root->right, sum);
-        hashMap[sum]--;  // kind of backtarcking
-    }
 public:
-    int pathSum(TreeNode* root, int targetSum) {
-        count = 0;
-        target = targetSum;
-        hashMap[0] = 1;
-        find(root, 0);
-        return count;
+    int pathSum(TreeNode* root, int sum) {
+        unordered_map<long long int, int> m;
+        m[0]++;
+        
+        int total = 0;
+        helper(root, 0, sum, total, m);
+        return total;
+    }
+    
+    void helper(TreeNode *p, long long int cur, int sum, int &total, unordered_map<long long int, int> &m) {
+        if (!p) return;
+        
+        cur += p->val;
+        if (m.find(cur - sum) != m.end()) total += m[cur - sum];
+        m[cur]++;
+        
+        helper(p->left, cur, sum, total, m);
+        helper(p->right, cur, sum, total, m);
+        
+        m[cur]--;            // kind of backtracking
     }
 };
