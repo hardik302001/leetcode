5c93ddcc1035b93f52ad07aa5c53d40f4d917e2f
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-23 08:03:11 +0000

    Sync LeetCode submission - Cousins in Binary Tree (cpp)

--- problems/cousins_in_binary_tree/solution.cpp
+++ problems/cousins_in_binary_tree/solution.cpp
@@ -11,41 +11,37 @@ public:
         }
 		
         queue<pair<int, TreeNode*>> toVisite;
-        toVisite.push(make_pair(0,root));
-        int levelCount = 1;
-        int count = 0;
+        toVisite.push(make_pair(0,root));   // parent , node
 		TreeNode* node ; 
         int xParent = 0, yParent = 0;
 		
         while(!toVisite.empty()){
-		
-            while(levelCount --){          //all nodes of same level
-			
-                node = toVisite.front().second;
+		    int sz = toVisite.size();
+            while(sz--){          //all nodes of same level
+			    auto [par, node] = toVisite.front();
+                toVisite.pop(); 
                 if(node->val == x){
-                    xParent = toVisite.front().first;
+                    xParent = par;
                 }
                 if(node->val == y){
-                    yParent = toVisite.front().first;
+                    yParent = par;
                 }
-                if(yParent && xParent && yParent != xParent){
+                if(yParent!=0 && xParent!=0 && yParent != xParent){
                     return true;
                 }
                 
-                toVisite.pop(); 
+
                 if(node->left != NULL){
-                    count ++; 
                     toVisite.push(make_pair(node->val, node->left)); 
                 }
                 if(node->right != NULL){
-                    count ++; 
                     toVisite.push(make_pair(node->val, node->right)); 
                 }
-            }
+            }   
             
-            levelCount = count; 
-            count = 0; 
-            yParent = 0, xParent = 0; // fresh start for new level...
+            // bcz fresh start, every time, bcz we are using condition that xpar!=0 and ypar!=0
+            xParent = 0;
+            yParent = 0;
         }
         return false;
     }
@@ -55,7 +51,7 @@ public:
 
 
 
-//recursive: idea is same, just we are keepjng track of what we need that is x and y only here
+//recursive: idea is same, just we are keeping track of what we need that is x and y only here
 
 //O(1)  space
 //O(N)  time
@@ -63,30 +59,28 @@ public:
 class Solution {
 public:
 
-int xdepth, ydepth, xparent, yparent;
+    int xdepth, ydepth, xparent, yparent;
 
-void depth(TreeNode* root, int x, int y, int dep, int par_val){
-    if(root==NULL) return;
-    if(root->val==x){
-        xdepth=dep;
-        xparent=par_val;
-        return;
+    void depth(TreeNode* root, int x, int y, int dep, int par_val){
+        if(root==NULL) return;
+        if(root->val==x){
+            xdepth=dep;
+            xparent=par_val;
+        }
+        if(root->val==y){
+            ydepth=dep;
+            yparent=par_val;
+        }
+        depth(root->left, x, y, dep+1, root->val);
+        depth(root->right, x, y, dep+1, root->val);
     }
-    if(root->val==y){
-        ydepth=dep;
-        yparent=par_val;
-        return;
+    bool isCousins(TreeNode* root, int x, int y) {
+        if(root->val==x || root->val==y) return false;
+        depth(root, x, y, 0, 0);
+
+        if(xdepth==ydepth && xparent!=yparent) return true;
+        return false;
     }
-    depth(root->left, x, y, dep+1, root->val);
-    depth(root->right, x, y, dep+1, root->val);
-}
-bool isCousins(TreeNode* root, int x, int y) {
-    if(root->val==x || root->val==y) return false;
-    depth(root, x, y, 0, 0);
-    
-    if(xdepth==ydepth && xparent!=yparent) return true;
-    return false;
-}
 
 };
 
