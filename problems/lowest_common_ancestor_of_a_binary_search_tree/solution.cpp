142effa2196a40072d0dc0a6a99c00f79bd1788e
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-25 11:43:40 +0000

    Sync LeetCode submission - Lowest Common Ancestor of a Binary Search Tree (cpp)

--- problems/lowest_common_ancestor_of_a_binary_search_tree/solution.cpp
+++ problems/lowest_common_ancestor_of_a_binary_search_tree/solution.cpp
@@ -1,25 +1,53 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+
+// optimised
+// TC: O(h) , height of tree
+// idea: https://www.youtube.com/watch?v=cX_kPV_foZc&list=PLgUwDviBIf0q8Hkd7bK2Bpryj2xVJk8Vk&index=48
 
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-
-        if (root == NULL) return NULL;
-        //if both are more than root->val, then find lca for left part
-        if (root->val > p->val && root->val > q->val)
-            return lowestCommonAncestor(root->left, p, q);
-        //if both part are more than root->val, then find lca for right part
-        if (root->val < p->val  && root->val < q->val)
-            return lowestCommonAncestor(root->right,   p , q);
-        //now it means p and q are on differnet side node  root of bst, it means root is LCA 
-        return root;
+        if(root==NULL) return NULL;
+        if(root->val == p->val or root->val==q->val) return root;   // base case
+        
+        TreeNode* left = lowestCommonAncestor(root->left , p , q);
+        TreeNode* right = lowestCommonAncestor(root->right , p , q);
+        
+        // usually we explore both sides, but here we explore only one of the sides
+        if(root->val > p->val and root->val > q->val){
+            return left;
+        }
+        if(root->val < p->val and root->val < q->val){
+            return right;
+        }
+        
+        return root;         // p and q are on different sides of root, so root is LCA
     }
 };
+
+
+// ---------------------------------------------------------------------------
+
+// brute force will the normal lca, O(n)
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/submissions/
+// TC: O(n) , SC :o(1)
+/*
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root==NULL) return NULL;
+        if(root->val == p->val or root->val==q->val)return root;
+        
+        TreeNode* left = lowestCommonAncestor(root->left , p , q);
+        TreeNode* right = lowestCommonAncestor(root->right , p , q);
+        
+        
+        if(left==NULL and right==NULL) return NULL;
+        if(left!=NULL and right!=NULL) return root;
+        if(left!=NULL) return left;
+        if(right!=NULL) return right;
+        
+        return NULL;
+    }
+};
+
+*/
