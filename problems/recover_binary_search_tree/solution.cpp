d20ea639be475dd5ddc8b4aece30774352c4c6bb
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-25 18:58:58 +0000

    Sync LeetCode submission - Recover Binary Search Tree (cpp)

--- problems/recover_binary_search_tree/solution.cpp
+++ problems/recover_binary_search_tree/solution.cpp
@@ -13,22 +13,24 @@
 
 class Solution {
 public:
-    TreeNode *node_1 = NULL, *node_2 = NULL, *prev = NULL;
-
+    TreeNode *node_1 = NULL, *node_2 = NULL;
+    TreeNode *prev = new TreeNode(INT_MIN);
+    
     void recoverTree(TreeNode* root) {
         if (root == NULL) return;  
         inorder(root);
-        if (node_1 && node_2)
-            swap(node_1->val, node_2->val);
+        swap(node_1->val, node_2->val);
     }
 
     void inorder(TreeNode *root) {
         if (root == NULL) return;
         inorder(root->left);
-        if (prev != NULL && root->val < prev->val) { // bcz its inorder, so cur>pre, bcz it is increasingly sorted
+        if (root->val < prev->val) { // bcz its inorder, so cur>pre, bcz it is increasingly sorted
+            // very importnat , node_1 will go for prev and node_2 will go for root
             if (node_1 == NULL) {
                 node_1 = prev;
             } 
+            
             if (node_1 != NULL) {
                 node_2 = root;
             }
