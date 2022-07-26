81e4b75eef0b47f256bed84341c7ebc22e5069fc
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-26 04:43:10 +0000

    Sync LeetCode submission - Symmetric Tree (cpp)

--- problems/symmetric_tree/solution.cpp
+++ problems/symmetric_tree/solution.cpp
@@ -1,4 +1,4 @@
-//THE CODE IS QUITE SIMILAR TO SAME TREE QUESTION(EASY)
+//THE CODE IS QUITE SIMILAR TO SAME TREE QUESTION(EASY) : https://leetcode.com/problems/same-tree/
 // there we were chceking for every left we traversed left and for every right we traversed //right..but here it is opposite bcz its mirror image ...so for evry left we will check right and //for evry right we will check left
 
 class Solution {
@@ -12,18 +12,11 @@ public:
     
     
     //check the two nodes in symmetric position
-    bool checkSymmetric(TreeNode *leftSymmetricNode, TreeNode *rightSymmetricNode)
-    {
-        if (leftSymmetricNode == NULL && rightSymmetricNode == NULL)
-            return true;
-        if (leftSymmetricNode == NULL || rightSymmetricNode == NULL)
-            return false;
+    bool checkSymmetric(TreeNode* p, TreeNode* q) {
+        if((p and !q) or (!p and q)) return false;
+        if(!p and !q) return true;
         
-        if (leftSymmetricNode->val != rightSymmetricNode->val){
-            return false;
-        }
-        
-        return checkSymmetric(leftSymmetricNode->left, rightSymmetricNode->right) and                            checkSymmetric(leftSymmetricNode->right, rightSymmetricNode->left);
-        
+        if(p->val!= q->val) return false;
+        return checkSymmetric(p->left , q->right) and checkSymmetric(p->right , q->left);    // here changed
     }
 };
