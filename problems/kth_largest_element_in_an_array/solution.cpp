b8ab0601fe7c92ccb6a57d77582d4093af1ee8d8
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-26 12:03:20 +0000

    Sync LeetCode submission - Kth Largest Element in an Array (cpp)

--- problems/kth_largest_element_in_an_array/solution.cpp
+++ problems/kth_largest_element_in_an_array/solution.cpp
@@ -4,7 +4,7 @@
 
 
 //COURTESY: CODINGMINUTES..
-//QUICKSELECT :KTH SMALLEST
+//QUICKSELECT : KTH SMALLEST
 //BUT HERE kth largest..so n-k smallest in reversed!!
 //thats all we did
 
@@ -33,14 +33,12 @@ public:
         reverse(nums.begin(),nums.end());
         int s = 0;
         int e = nums.size()-1;
-        int c = (e + 1) - k;              //kth smallest here in n-k in reversed
+        int c = (e + 1) - k;              // kth smallest here in n-k in reversed
         while(1){
             //rather than going recursive we went for while 1 ..bcz we did not had s and e as parameters in actual function findKthLargest
-            
             //and only thing varying was also s and e
             
-            int p = partition(nums,s,e);
-            cout<<p<<endl;
+            int p = partition(nums,s,e);    // p is pivot
             if(p==c){
                 return nums[p];
             }
@@ -79,7 +77,7 @@ public:
     }
 };
 
-//Time: In worst case, O(nlogk).
+
 */
 
 
@@ -107,7 +105,6 @@ public:
     }
 };
 
-//Time: In worst case, O(nlogk).
 */
 
 
