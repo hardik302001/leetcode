97d868f483d61bc4d85b4af3611b099a5e80ac76
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-28 15:26:06 +0000

    Sync LeetCode submission - Palindrome Partitioning II (cpp)

--- problems/palindrome_partitioning_ii/solution.cpp
+++ problems/palindrome_partitioning_ii/solution.cpp
@@ -1,23 +1,23 @@
-// no of partitions needed for string(i:n)
 
 class Solution {
 public:
     int dp[2001];
     int recur(int start , int n , string &s){
-        if(start==n) return 0;
+        if(start>=n) return 0;
         if(dp[start]!=-1) return dp[start];
-        
+        if(isPalindrome(s,start,n) == true)
+            return dp[start] = 0;
         int mi = INT_MAX;        
-        for (int i = start; i < n; i++) {
+        
+        // cut after ith index
+        for (int i = start; i < n; i++) {  
             if (isPalindrome(s, start, i)) { 
                 mi = min(mi , 1 + recur(i+1, n, s));
             }
         }
-        
         return dp[start] = mi;    
     }
-    
-    
+     
     bool isPalindrome(string& s, int l, int r) {
         while (l < r) {
             if (s[l++] != s[r--]) {
@@ -30,6 +30,6 @@ public:
     int minCut(string s) {
         int n = s.size();
         memset(dp , -1 , sizeof dp);
-        return recur(0 , n, s)-1;
+        return recur(0 , n-1, s);
     }
 };
