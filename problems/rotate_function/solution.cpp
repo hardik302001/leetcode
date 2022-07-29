e3b10afc9c1b2b8f7892bd6f59e66d98715c94f3
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-29 05:00:44 +0000

    Sync LeetCode submission - Rotate Function (cpp)

---
+++ problems/rotate_function/solution.cpp
@@ -0,0 +1,33 @@
+
+// really good ques, sliding window
+// https://leetcode.com/problems/rotate-function/discuss/531675/Sliding-window-approach-in-C%2B%2B
+
+class Solution {
+public:
+    int maxRotateFunction(vector<int>& A) {
+        long sum = 0, fn = 0;
+        int len = A.size();
+        for(int i=0;i<len;i++) {
+            sum += A[i];
+            fn += (i * A[i]);
+        }
+
+        long l = 1, r;
+        long newfn = fn;
+        
+        while(l < len) {
+            r = l + len - 1;
+            
+            long removed = (l-1) * A[l-1];
+            long added = r * A[r%len];
+            
+            newfn = newfn - removed + added - sum;
+            
+            fn = max(fn, newfn);
+            
+            l++;
+        }
+        
+        return (int)fn;
+    }
+};
