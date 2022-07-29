2ac98648927d34177a430d0495805a2db4c0485e
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-07-29 04:57:53 +0000

    Sync LeetCode submission - Leaf-Similar Trees (cpp)

---
+++ problems/leaf-similar_trees/solution.cpp
@@ -0,0 +1,27 @@
+// no parallel traversal needed
+// brute force
+
+
+class Solution {
+public:
+    void dfs(TreeNode* root, vector<int>& v) {
+        if (!root) return;
+        if (!(root->left || root->right)) {
+            v.push_back(root->val);
+        }
+        dfs(root->left, v);
+        dfs(root->right, v);
+        
+    }
+    
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        vector<int> v1{};
+        vector<int> v2{};
+        
+        dfs(root1, v1);
+        dfs(root2, v2);
+        
+        return v1 == v2;
+        
+    }
+};
