71ae2c311d3ad5af04c0d35d86a954fc40da62c6
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-08-08 19:17:35 +0000

    Sync LeetCode submission - Minimum Window Substring (cpp)

--- problems/minimum_window_substring/solution.cpp
+++ problems/minimum_window_substring/solution.cpp
@@ -13,9 +13,9 @@ public:
         
         vector<int> tmap(128,0), smap(128,0);      //128 bcz ascii A = 65, ascii a = 97 , so large gap
         for(char c : t) tmap[c- 'A']++;  //why -'A' only, why not 'a'..??
-                                        // to keep indexes positive 
-                                        //  bcz ascii of 'A' < ascii of 'a' 
-                                        //so relatively s[i]-'A' = 0 , if we take s[i]-'a' = 0, then s[i]-'A' will be negative and we cant have negative indexes
+                                         // to keep indexes positive 
+                                         //  bcz ascii of 'A' < ascii of 'a' 
+                                         //so relatively s[i]-'A' = 0 , if we take s[i]-'a' = 0, then s[i]-'A' will be negative and we cant have negative indexes
         //tmap is the hashcode
         
         int start=0,end=0,counter=t.size(),minStart=0,minLen=INT_MAX;
@@ -40,7 +40,7 @@ public:
                 } //update the head and tail and length if needed, else, continue resizing
                 
                 smap[s[start]-'A']--; //going left
-                if(tmap[s[start]-'A'] > smap[s[start]-'A']){  //now if s[start] is a valid char,a nd it needs to be removed than  we also need to upadte c
+                if(tmap[s[start]-'A'] > smap[s[start]-'A']){  //now if s[start] is a valid char,and it needs to be removed than  we also need to upadte c
                     c--;
                 }
                 start++;
@@ -52,9 +52,7 @@ public:
         if(minLen!=INT_MAX){
             return s.substr(minStart,minLen);
         }
-        return "";
-        
+        return "";      
     }
 };
-
 //one optimization that can be done is that we are again and again cross checking both hashmap, it is time consuming..wecan use only single hashmap to verify the same
