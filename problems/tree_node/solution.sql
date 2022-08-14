c6e8aaf2bf51b7771d778ef896b7239e361f448a
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-08-14 20:37:16 +0000

    Sync LeetCode submission - Tree Node (mysql)

--- problems/tree_node/solution.sql
+++ problems/tree_node/solution.sql
@@ -1,7 +1,9 @@
 
-select id , (case 
- when p_id is null then 'Root'
- when id in (select p_id from tree where p_id is not null) then 'Inner'
- else 'Leaf' end) 
- as Type
-from Tree
+
+SELECT id, (
+    CASE
+    WHEN p_id IS NULL THEN 'Root'
+    WHEN p_id IS NOT NULL AND id IN (SELECT p_id FROM Tree) THEN 'Inner'
+    ELSE 'Leaf'
+    END
+) AS type FROM Tree;
