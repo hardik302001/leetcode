b62f08a60c33fe11a9a01cbb3097b2493f19df35
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-08-16 14:10:12 +0000

    Sync LeetCode submission - Sort Integers by The Power Value (cpp)

--- problems/sort_integers_by_the_power_value/solution.cpp
+++ problems/sort_integers_by_the_power_value/solution.cpp
@@ -1,35 +1,44 @@
-// class Solution {
-// public:
-
-// unordered_map<int, int> cnt;
-
-// int traverse(int x){
-//     if(cnt[x] || x == 1)
-//         return cnt[x] ;
-    
-//     if(x % 2)
-//         cnt[x] = 1+traverse(3*x + 1);
-//     else
-//         cnt[x] = 1+traverse(x/2);
-    
-//     return cnt[x] ;
-// }
-
-// int getKth(int lo, int hi, int k) {
-
-//     vector<pair<int,int>> arr;
-	
-//     for(int i = lo; i <= hi; i++)
-//     {
-//         arr.push_back({traverse(i), i}) ;
-//     }
-	
-//     sort(arr.begin(), arr.end()) ;
-//     return arr[k-1].second;
-// }
-// };
+// here we are doing full sorting
 
 
+/*
+
+class Solution {
+public:
+
+    unordered_map<int, int> cnt;
+
+    int traverse(int x){
+        if(cnt[x] || x == 1)
+            return cnt[x] ;
+
+        if(x % 2)
+            cnt[x] = 1+traverse(3*x + 1);
+        else
+            cnt[x] = 1+traverse(x/2);
+
+        return cnt[x] ;
+    }
+
+    int getKth(int lo, int hi, int k) {
+
+        vector<pair<int,int>> arr;
+        cnt[1] = 0;
+        for(int i = lo; i <= hi; i++)
+        {
+            arr.push_back({traverse(i), i}) ;
+        }
+
+        sort(arr.begin(), arr.end()) ;
+        return arr[k-1].second;
+    }
+};
+
+*/
+
+// ---------------------------------------------------------------------------
+
+// we used pq, just to avaoid full sorting
 
 
 class Solution {
