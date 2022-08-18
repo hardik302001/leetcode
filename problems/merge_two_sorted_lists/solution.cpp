eba37df3379affdc5b2a31906e1b79bff50dcfc9
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-08-18 06:38:15 +0000

    Sync LeetCode submission - Merge Two Sorted Lists (cpp)

--- problems/merge_two_sorted_lists/solution.cpp
+++ problems/merge_two_sorted_lists/solution.cpp
@@ -1,13 +1,4 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+/*
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* a, ListNode* b) {
@@ -30,5 +21,45 @@ public:
             c->next = mergeTwoLists(a,b->next);
         }
         return c;
+    }
+};
+
+*/
+
+// --------------------------------------------------------------
+
+// similar to : https://leetcode.com/problems/merge-k-sorted-lists/submissions/
+class compare{
+public:
+    bool operator()(ListNode* &l1, ListNode* &l2){
+        return l1->val > l2->val;
+    }
+};
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* a, ListNode* b) {
+        priority_queue<ListNode* , vector<ListNode*> , compare>pq;
+        if(a) pq.push(a); 
+        if(b) pq.push(b); 
+        
+        if(pq.size()==0) return NULL;
+        ListNode* cur = pq.top();
+        pq.pop();
+        
+        if(cur->next) pq.push(cur->next);
+        ListNode* head = cur;
+        
+        while(!pq.empty()){
+            auto top = pq.top();
+            pq.pop();
+            cur->next = top;
+            cur = cur->next;
+            if(top->next) pq.push(top->next);
+        }
+        
+        return head;
+        
+        
     }
 };
