025a02e40a01df0c7b08e43cab4a365c89ba259e
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-08-18 17:39:27 +0000

    Sync LeetCode submission - Design Twitter (cpp)

--- problems/design_twitter/solution.cpp
+++ problems/design_twitter/solution.cpp
@@ -47,12 +47,3 @@ public:
         following[followerId].erase(followeeId);
     }
 };
-
-/**
- * Your Twitter object will be instantiated and called as such:
- * Twitter* obj = new Twitter();
- * obj->postTweet(userId,tweetId);
- * vector<int> param_2 = obj->getNewsFeed(userId);
- * obj->follow(followerId,followeeId);
- * obj->unfollow(followerId,followeeId);
- */
