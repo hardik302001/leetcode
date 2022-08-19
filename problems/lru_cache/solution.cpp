b49db65109fde18b5ce28d60861eeaf5e5bae017
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-08-19 06:19:06 +0000

    Sync LeetCode submission - LRU Cache (cpp)

--- problems/lru_cache/solution.cpp
+++ problems/lru_cache/solution.cpp
@@ -1,24 +1,28 @@
-// courtesy ; striiver yt video
+// courtesy ; striver yt video
+
+
+class node{
+public:
+    int key; 
+    int val;
+    node* next;
+    node* prev;
+    node(int key , int val){
+        this->key = key;
+        this->val = val;
+        next = NULL;
+        prev = NULL;
+    }
+};
 
 class LRUCache {
 public:
-    
-    class node{
-        public:
-            int key; 
-            int val;
-            node* next;
-            node* prev;
-            node(int key , int val){
-                this->key = key;
-                this->val = val;
-            }
-    };
     
     node* head = new node(-1 , -1);
     node* tail = new node(-1 , -1);
     
     int cap;
+    
     unordered_map<int, node*>mp;
     LRUCache(int capacity) {
         cap = capacity;
@@ -26,7 +30,7 @@ public:
         tail->prev = head;
     }
     
-    void addnode(node* newnode){
+    void addnode(node* newnode){  // add after head
         node* temp = head->next;
         newnode->next = temp;
         newnode->prev = head;
@@ -34,39 +38,43 @@ public:
         temp->prev = newnode;
     }
     
-    void deletenode(node* delnode){
+    void deletenode(node* delnode){   // delete before tail
         node* delprev = delnode->prev;
         node* delnext = delnode->next;
         delprev->next = delnext;
         delnext->prev = delprev;
     }
     
+    
+    // whenever delete , remove from map, whenever add, add it to map
+    
     int get(int key) {
         if(mp.count(key)){
             node* resnode = mp[key];
             int res = resnode->val;
-            mp.erase(key);             // remove old address key from map
-            deletenode(resnode);      // delete it from link list
-            addnode(resnode);         // add new address node in linklist
+            mp.erase(key);
+            deletenode(resnode);       // delete it from link list
+            addnode(resnode);          // add new address node in linklist
             mp[key] = head->next;      // add new address key in map
-            return res;
-            
-            
-        }else{
+            return res;        
+       }else{
             return -1;
         }
     }
     
+    
     void put(int key, int value) {
-        if(mp.count(key)){  // key already exists , so for the time being just remove the previous address node
+        if(mp.count(key)){             // key already exists , so for the time being just remove the previous address node
             node* existingnode = mp[key];
             mp.erase(key);
-            deletenode(existingnode);
+            deletenode(existingnode);   // we need to update value for current key, so dlete it, add it, and update address in map
         }
+        
         if(mp.size()==cap){    // remove LRU
             mp.erase(tail->prev->key);
             deletenode(tail->prev);
         }
+        
         addnode(new node(key, value));
         mp[key] = head->next;
      }
