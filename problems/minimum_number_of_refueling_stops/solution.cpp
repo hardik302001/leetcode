7e8a5af15f774738fc1d995b4c99d45cf8714c45
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-08-20 08:09:31 +0000

    Sync LeetCode submission - Minimum Number of Refueling Stops (cpp)

--- problems/minimum_number_of_refueling_stops/solution.cpp
+++ problems/minimum_number_of_refueling_stops/solution.cpp
@@ -1,47 +1,53 @@
-// class Solution
-// {
-//     public:
-//         int solve(int cur, int pos, int target, vector<vector < int>> &stations)
-//         {
-//             if (pos >= target or cur>=target)
-//             {
-//                 return 0;
-//             }
+/*
 
-//             int ans = INT_MAX/2;
-//             for (auto pair : stations)
-//             {
-//                 auto p =  pair[0];
-//                 auto fuel = pair[1];
-//                 int dis = p - pos;
-//                 if (dis <= 0)
-//                 {
-//                     continue;	// we wont go back
-//                 }
-//                 else
-//                 {
-//                     if (cur - dis >= 0)
-//                     {
-//                         int taken = 1 + solve(cur + fuel - dis, p, target, stations);
-//                         int not_taken = solve(fuel- dis, p, target, stations);
-//                         ans = min({ans , taken , not_taken});
-//                     }else{
-//                         int not_taken = solve(fuel- dis, p, target, stations);
-//                         ans = min(ans , not_taken);
-//                     }
-//                 }
-//             }
-            
-                            
-//             return ans;
-//         }
+class Solution{
+public:
+    int solve(int cur, int pos, int target, vector<vector < int>> &stations)
+    {
+        if (pos >= target or cur>=target)
+        {
+            return 0;
+        }
 
-//     int minRefuelStops(int target, int startFuel, vector<vector < int>> &stations)
-//     {
-//         return solve(startFuel, 0, target, stations);
-//     }
-// };
+        int ans = INT_MAX/2;
+        for (auto pair : stations)
+        {
+            auto p =  pair[0];
+            auto fuel = pair[1];
+            int dis = p - pos;
+            if (dis <= 0)
+            {
+                continue;	// we wont go back
+            }
+            else
+            {
+                if (cur - dis >= 0)
+                {
+                    int taken = 1 + solve(cur + fuel - dis, p, target, stations);
+                    int not_taken = solve(fuel- dis, p, target, stations);
+                    ans = min({ans , taken , not_taken});
+                }else{
+                    int not_taken = solve(fuel- dis, p, target, stations);
+                    ans = min(ans , not_taken);
+                }
+            }
+        }
 
+
+        return ans;
+    }
+
+    int minRefuelStops(int target, int startFuel, vector<vector < int>> &stations)
+    {
+        return solve(startFuel, 0, target, stations);
+    }
+};
+
+*/
+
+// --------------------------------------------------------------------------------
+    
+    
 // https://leetcode.com/problems/minimum-number-of-refueling-stops/discuss/150514
 // priority_queue : O(nlogn)
 
