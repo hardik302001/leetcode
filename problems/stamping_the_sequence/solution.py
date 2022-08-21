4dbd83e052ef90b39e3537d48329392c6ea3289e
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-08-21 04:34:03 +0000

    Sync LeetCode submission - Stamping The Sequence (python3)

---
+++ problems/stamping_the_sequence/solution.py
@@ -0,0 +1,25 @@
+# idea : https://leetcode.com/problems/stamping-the-sequence/discuss/189576/C%2B%2B-simple-greedy
+
+
+
+class Solution:
+    def movesToStamp(self, s, t):
+        n, m, t, s, res = len(t), len(s), list(t), list(s), []
+
+        def check(i):
+            changed = False
+            for j in range(m):
+                if t[i + j] == '?': continue
+                if t[i + j] != s[j]: return False
+                changed = True
+            if changed:
+                t[i:i + m] = ['?'] * m
+                res.append(i)
+            return changed
+
+        changed = True
+        while changed:
+            changed = False
+            for i in range(n - m + 1):
+                changed |= check(i)
+        return res[::-1] if t == ['?'] * n else []
