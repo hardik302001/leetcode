dc1db4da70a1dac729c09ef7b50afb557cf6b09d
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-08-27 10:20:20 +0000

    Sync LeetCode submission - Shortest Subarray with Sum at Least K (cpp)

--- problems/shortest_subarray_with_sum_at_least_k/solution.cpp
+++ problems/shortest_subarray_with_sum_at_least_k/solution.cpp
@@ -11,11 +11,12 @@ public:
                 ans = min(ans, i+1);
             }
             while( pq.size() && sum - pq.top().first >= K ){
-                auto &p = pq.top();
+                auto &p = pq.top();            // here p is pref sum
                 ans = min(ans, i-p.second);
                 pq.pop();
             }
-            pq.push({sum, i});
+            
+            pq.push({sum, i});         // we are pushing pref sum in pq
         }
         return ans == 1e18?-1:ans;
     }
