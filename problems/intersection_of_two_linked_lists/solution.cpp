df4f1edf98b31de621f07de16735b7084b568ab0
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-08-31 06:19:06 +0000

    Sync LeetCode submission - Intersection of Two Linked Lists (cpp)

--- problems/intersection_of_two_linked_lists/solution.cpp
+++ problems/intersection_of_two_linked_lists/solution.cpp
@@ -16,6 +16,22 @@ TUF
    Replacing with head of other node is like doing (l1-l2) iteration in list of maximum list....so after the first cycle..both list with have same length(reach end of both lists at same time) ....
    now do second full traversal..if they are same at some point..else they reach NULL at same point..then retrurn NULL
 
+// explanation of logic : https://leetcode.com/problems/intersection-of-two-linked-lists/discuss/2064280/explained-why-2-pointer-works-space-O(1)
+
+PROOF:
+Assume we have 2 list A and B which collide at C then merges.
+elements in A before C point be N
+elements in B before C point be M
+elements in list after C point be D
+assume me switch pointer of listA into head of B when list is exhausted
+so till C of 2nd traversal it covered (N+D)+M elements
+same if we switch pointer of listB into head of A when list in exhausted
+so till c of 2nd traversal it covered (M+D) +N elements
+so we can see both pointer of A and pointer of B will cover M+D+N elements till it reaches C and both pointers are moving at same speed we know that both will cover same distance at same time , thus they will be at C at the same time.
+
+
+
+// they will meet after first traversal surely. 
 
 */
 
