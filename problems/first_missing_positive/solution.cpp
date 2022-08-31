f952376b0912bac088c58e2d78bfb9cd5a0ccf53
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-08-31 06:46:38 +0000

    Sync LeetCode submission - First Missing Positive (cpp)

--- problems/first_missing_positive/solution.cpp
+++ problems/first_missing_positive/solution.cpp
@@ -1,31 +1,40 @@
 //courtesy codencode yt
 
+
+// also see: https://takeuforward.org/data-structure/find-the-repeating-and-missing-numbers/
 // also see: https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/submissions/
 // also see: https://leetcode.com/problems/find-the-duplicate-number/
 
-
-
 // bring nums[i] at its corret position
-// [1, 2, 3 ,4 ..., n]
+//[1, 2, 3 ,4 ..., n]
 
-class Solution {
-public:
-    int firstMissingPositive(vector<int>& nums) {
-        int n = nums.size();
-        
-        for(int i = 0;i<n;i++){
-            while(nums[i]>0 and nums[i]<=n and nums[nums[i]-1]!=nums[i]){
-                swap(nums[i],nums[nums[i]-1]);                
+class Solution
+{
+    public:
+        int firstMissingPositive(vector<int> &nums)
+        {
+            int n = nums.size();
+
+            for (int i = 0; i < n; i++)
+            {
+                while (nums[i] > 0 and nums[i] <= n and nums[nums[i] - 1] != nums[i])
+                {
+                    swap(nums[i], nums[nums[i] - 1]);
+                    // for (auto it: nums)
+                    // {
+                    //     cout << it << " ";
+                    // }
+                    // cout << endl;
+                }
             }
-        }
-        for(auto it: nums){
-            cout<<it<<" ";
-        }
-        for(int i = 0;i<n;i++){
-            if(nums[i]!=i+1){
-                return i+1;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (nums[i] != i + 1)
+                {
+                    return i + 1;
+                }
             }
+            return n + 1;
         }
-        return n+1;
-    } 
 };
