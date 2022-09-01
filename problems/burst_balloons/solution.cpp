8b94175b5dd525ea57e97c7938a011c56a2543df
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-09-01 20:36:44 +0000

    Sync LeetCode submission - Burst Balloons (cpp)

--- problems/burst_balloons/solution.cpp
+++ problems/burst_balloons/solution.cpp
@@ -1,11 +1,11 @@
 // this problem is unique
 // also see: very importnat ques : https://www.youtube.com/watch?v=MM7fXopgyjw&list=PLgUwDviBIf0qUlt5H_kiKYaNSqJ81PMMY&index=53
 
-// very important to conisder states
+// very important to consider states
 
 
 // our subproblem will be to burst first , calculate and partition fr next stage
-// suppose 5 elements, i partition at 3rd position, next stae we have (0,2) and (4,5) and cur res = arr[2]*arr[3]*arr[4]
+// suppose 5 elements, i partition at 3rd position, next state we have (0,2) and (4,5) and cur res = arr[2]*arr[3]*arr[4]
 // next for (0,2), suppose i partition at 2, so for result calculation i will need arr[4], so substates are not independent...
 
 // thus our subproblem is wrong... we need other way to express our subproblem bcz states should be independent. 
