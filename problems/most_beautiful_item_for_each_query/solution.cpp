7773391de982a19e9f1e1b1d719a71d7e7f993a8
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-09-05 13:51:57 +0000

    Sync LeetCode submission - Most Beautiful Item for Each Query (cpp)

---
+++ problems/most_beautiful_item_for_each_query/solution.cpp
@@ -0,0 +1,34 @@
+
+// idea : https://leetcode.com/problems/most-beautiful-item-for-each-query/discuss/1575898/C++-Solution-or-Sorting
+
+
+class Solution {
+public:
+    vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
+        vector<int> ans(queries.size());
+        
+        vector<pair<int ,int>> queriesPair;
+        for (int i = 0; i < queries.size(); i++) {
+            queriesPair.push_back({queries[i], i});
+        }
+        
+        sort(queriesPair.begin(), queriesPair.end());
+        sort(items.begin(), items.end());
+        
+        int itemIndex = 0, maxBeauty = 0;
+        for (int i = 0; i < queriesPair.size(); i++) {
+            int maxPriceAllowed = queriesPair[i].first;
+            int queryOriginalIndex = queriesPair[i].second;
+            
+            // Iterate over items, stop when the price exceeds query price or no item left
+            while (itemIndex < items.size() && items[itemIndex][0] <= maxPriceAllowed) {
+                maxBeauty = max(maxBeauty, items[itemIndex][1]);
+                itemIndex++;
+            }
+            
+            ans[queryOriginalIndex] = maxBeauty;
+        }
+        
+        return ans;
+    }
+};
