41a091fe2b09aa15eb656b21dee41a4886792b0a
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-09-05 11:09:23 +0000

    Sync LeetCode submission - Word Break II (cpp)

--- problems/word_break_ii/solution.cpp
+++ problems/word_break_ii/solution.cpp
@@ -1,79 +1,81 @@
 
 
 // backtracking : O(n^n)
-
+/*
 class Solution {
 public:
-    vector<string> wordBreak(string s, vector<string>& wordDict) {
-        //insert all the words in the set
-        unordered_set<string> set;
-        vector<string> res;
-        for(auto word:wordDict)
-            set.insert(word);
-        //to store the current string 
-        string curr="";
-        findHelper(0,s,curr,set,res);
-        return res;
-    }
+    vector<string> ans;
+    unordered_set<string> hash;
     
-    void findHelper(int ind,string s,string curr,unordered_set<string> set,vector<string>& res)
-    {
-        if(ind==s.length())
-        {
-            //we have reached end
-            curr.pop_back(); //remove the trailing space
-            res.push_back(curr);
+    void helper(string s, vector<string> &curr){
+        if(s.length() == 0){
+            string res;
+            for(int i=0;i<curr.size(); ++i){
+                res += curr[i];
+                if(i != curr.size() - 1) res+= " ";
+            }
+            ans.push_back(res);
             return;
         }
-        string str="";
-        for(int i=ind;i<s.length();i++)
-        {
-            //get every substring and check if it exists in set
-            str.push_back(s[i]);
-            if(set.count(str))
-            {
-                //we have got a word in dict 
-                //explore more and get other substrings
-                findHelper(i+1,s,curr+str+" ",set,res);
+        
+        for(int i=1;i<=s.length(); ++i){
+            string left = s.substr(0, i);
+            if(hash.count(left)) {
+                curr.push_back(left);
+                helper(s.substr(i), curr);
+                curr.pop_back();
             }
         }
     }
+    
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        vector<string> curr;
+        for(auto x: wordDict) hash.insert(x);
+        helper(s, curr);
+        return ans;
+    }
 };
-
+*/
 
 
 // ------------------------------------------------------------------------------------------------------
 
+
 // RECURSION + dp type
-/*
+// memoising from s[:i] and finding s[i:n]
+// if we get s[i:n] in dict, then we use our memoised result to get answer
+
+
+
 class Solution {
 public:
-    unordered_map<string, vector<string>> m;
+    unordered_map<string, vector<string>> m;           // used for memoisation
     
     vector<string> wordBreak(string s, vector<string>& wordDict) {    
         unordered_set<string> dict(wordDict.begin(), wordDict.end());
         return solve(s, dict);
     }
     
-    vector<string>solve(string s, unordered_set<string> dict){       //recursive function
+    vector<string>solve(string s, unordered_set<string> dict){       // recursive function
         int n = s.size();
-        if(m.count(s)) return m[s]; //take from memory
+        if(m.count(s)) return m[s];    // take from memory
         vector<string> result;
-        if(dict.count(s)){ //a whole string is a word
+        if(dict.count(s)){             // a whole string is a word
             result.push_back(s);
         }
+        
         for(int i=1;i<s.size();++i){
             string word=s.substr(i, n);  
             if(dict.count(word)){
                 string rem=s.substr(0,i);
-                vector<string> prev=combine(word,solve(rem,dict));     //recursion
+                vector<string> prev=combine(word,solve(rem,dict));     // recursion
                 result.insert(result.end(),prev.begin(), prev.end());
             }
         }
+        
         m[s]=result; //memorize
         return result;
     }
-    
     
     
     vector<string> combine(string word, vector<string> prev){        // string (s) = rem(s[0:i]) + word( substring s[i:n])
@@ -86,7 +88,7 @@ public:
     
 };
 
-*/
+
 /*
 Paraphrasing the solution in my words
 
