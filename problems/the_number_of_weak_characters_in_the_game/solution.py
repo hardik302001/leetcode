8360488bf7e45afc5fe2e7e0ba399f744b239c48
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-09-09 03:28:14 +0000

    Sync LeetCode submission - The Number of Weak Characters in the Game (python3)

--- problems/the_number_of_weak_characters_in_the_game/solution.py
+++ problems/the_number_of_weak_characters_in_the_game/solution.py
@@ -1,33 +1,28 @@
 class Solution:
-    def numberOfWeakCharacters(self, p: List[List[int]]) -> int:
-        p.sort(key = lambda x: (x[0],x[1]))
-        l = []        # group of defeences, having diffrenet attcks 
-        a = p[0][0]
-        ll = []
-        for i in p:
-            if i[0]==a:
-                ll.append(i[1])
-            else:
-                l.append(ll)
-                a = i[0]
-                ll = []
-                ll.append(i[1])
-        l.append(ll)
+
+    def numberOfWeakCharacters(self, properties: List[List[int]]) -> int:
+
         
-        print(l)
+
+        properties.sort(key=lambda x: (-x[0],x[1]))
+
+        
+
         ans = 0
-        allmax = 0               #keep track of max of all elemnts from end..bcz list is reveersed
+
+        curr_max = 0
+
         
-        for listi in l[::-1]:
-            m = allmax       #m is max of all elements in listi, bcz we cant have eleemnts from same list .
-                                #we need a seperate variable to keep track of both maxxes, allmax is maximum of all eleemnets, it will be considered for all listi that occur after until we have calculated allmax
-                
-                #m is to get max of that list, so that after completeting that listi we can updatte allmax.
-            
-            for i in listi:
-                if i<m:
-                    ans = ans + 1
-                m = max(i,m)
-            allmax = max(allmax,m)
+
+        for _, d in properties:
+
+            if d < curr_max:
+
+                ans += 1
+
+            else:
+
+                curr_max = d
+
         return ans
-            
+        
