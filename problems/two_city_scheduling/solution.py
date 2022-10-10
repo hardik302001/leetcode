27dc0757249c13f8109ef99bdde39f2af2f29d1d
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-10-10 19:53:06 +0000

    Sync LeetCode submission - Two City Scheduling (python3)

--- problems/two_city_scheduling/solution.py
+++ problems/two_city_scheduling/solution.py
@@ -1,11 +1,11 @@
+# see cpp soln
+
+# noice ques
+
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        costs.sort(key = lambda x: x[0]-x[1])  # x[0]-x[1] is savings , if dif >0, choosing b is befeicial , dif<0 , choosimg a is better
-        
-        
-        c = 0
-        for i in range(len(costs)//2):
-            c+= costs[i][0] + costs[i+len(costs)//2][1]
-        return c
-        
-        
+        costs.sort(key=lambda cost: cost[0] - cost[1])
+        costs_for_A = sum([cost[0] for cost in costs[:len(costs) // 2]])
+        costs_for_B = sum([cost[1] for cost in costs[len(costs) // 2:]])
+        return costs_for_A + costs_for_B
+
