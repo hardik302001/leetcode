0487c399e62a7c05084e2fdd2fed4da86268ff8c
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-10-21 10:06:05 +0000

    Sync LeetCode submission - Contains Duplicate II (cpp)

--- problems/contains_duplicate_ii/solution.cpp
+++ problems/contains_duplicate_ii/solution.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
 bool containsNearbyDuplicate(vector<int>& nums, int k) {
-unordered_map<int,int> myMap;
-for(int i=0;i<nums.size();i++){
-if(myMap.find(nums[i])!=myMap.end()){
-if(i-myMap[nums[i]]<=k)
-return true;
-}
-myMap[nums[i]] = i;
-}
-return false;
-}
+    unordered_map<int,int> myMap;
+    for(int i=0;i<nums.size();i++){
+    if(myMap.find(nums[i])!=myMap.end()){
+        if(i-myMap[nums[i]]<=k)
+            return true;
+        }
+            myMap[nums[i]] = i;
+        }
+        return false;
+    }
 };
