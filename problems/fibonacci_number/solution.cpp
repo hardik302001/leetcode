c0140dd220f2be348d5dda286e5dcf691c71bb72
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-11-01 09:04:30 +0000

    Sync LeetCode submission - Fibonacci Number (cpp)

--- problems/fibonacci_number/solution.cpp
+++ problems/fibonacci_number/solution.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
-    int fib(int n) {
-            
-        if (n < 1) return n;
-        int a = 0, b = 1, c = a + b ;
-        while (n-- > 1) {
-            c = a + b, a = b, b = c;
-        }
-        return b;
+    int dp[31];
     
+    int recur(int n){
+        if(n==0) return 0; 
+        if(n==1) return 1;
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        
+        return (dp[n] = recur(n-1) + recur(n-2)); 
+    }
+    
+    int fib(int n) {
+        memset(dp , -1 , sizeof dp);
+        return recur(n);
     }
 };
