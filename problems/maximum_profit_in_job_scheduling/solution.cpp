7d980373218bad33eb56ede9957745fc955337e0
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-11-26 20:25:30 +0000

    Sync LeetCode submission - Maximum Profit in Job Scheduling (cpp)

--- problems/maximum_profit_in_job_scheduling/solution.cpp
+++ problems/maximum_profit_in_job_scheduling/solution.cpp
@@ -1,4 +1,64 @@
-//similar to https://leetcode.com/problems/maximum-earnings-from-taxi/
+
+// MEMOIZATION + BINARY SEARCH (DP Solution) [Accepted] && [ 30 / 30 test cases passed ]
+class Solution {
+private: int N, memo[1000001];
+private:
+    int dfs(vector<vector<int>>&nums, int idx){
+        if(idx >= N) return 0;
+        if(memo[idx]) return memo[idx];
+        int lo = 0, hi = N - 1, newJobTime = N + 1;
+        while(lo <= hi){
+            int mid = (lo + hi) >> 1;
+            if(nums[mid][0] >= nums[idx][1]) newJobTime = mid, hi = mid - 1;
+            else lo = mid + 1;
+        }
+        return memo[idx] = max(dfs(nums, idx + 1), nums[idx][2] + dfs(nums, newJobTime));
+    }
+public:
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        N = size(profit);
+        vector<vector<int>> nums;
+        for(int i = 0; i < N; ++i) nums.push_back({startTime[i], endTime[i], profit[i]});
+        sort(begin(nums), end(nums));
+        return dfs(nums, 0);
+    }
+};
+// Time Comeplxity : O(N * logN) , Where N : len of nums or startTime
+// Space Complexity : O(N), ignoring recursion stack
+
+
+// ---------------------------------------------------------------------
+
+/*
+// O(n^2) , dp
+class Solution {
+private: int memo[5000001], N;
+private: 
+    int dfs(vector<vector<int>> &nums, int idx, int prevEndTime){
+        if(idx >= N) return 0;
+        if(memo[prevEndTime] != -1) return memo[prevEndTime];
+        int skipIt = dfs(nums, idx + 1, prevEndTime);
+        int takeIt = prevEndTime <= nums[idx][0] ? nums[idx][2] + dfs(nums, idx + 1, nums[idx][1]) : 0;
+        return memo[prevEndTime] = max(skipIt, takeIt);
+    }
+public:
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+		N = size(startTime);   
+        vector<vector<int>> nums;
+        for(int i = 0; i < N; ++i)  nums.push_back({startTime[i], endTime[i], profit[i]});
+        sort(begin(nums), end(nums));
+		memset(memo, -1, sizeof(memo));    
+        return dfs(nums, 0, 1);
+    }
+};
+
+*/
+
+// ------------------------------------------------------------------------
+/*
+
+
+// similar to https://leetcode.com/problems/maximum-earnings-from-taxi/
 
 // https://leetcode.com/problems/maximum-profit-in-job-scheduling/discuss/917256/C%2B%2B-DP-Solution-in-O(n2)-and-O(nlogn)-time
 //also see prev soln, O(n2)
@@ -60,3 +120,5 @@ public:
     }
     
 };
+
+*/
