bbbd6020397201488dd48e84983ab710cb773adc
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-12-04 08:33:37 +0000

    Sync LeetCode submission - Circular Sentence (python3)

---
+++ problems/circular_sentence/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isCircularSentence(self, sentence: str) -> bool:
+        words = sentence.split(" ")
+        for i in range(len(words)):
+            if words[i-1][-1] != words[i][0]:
+                return False
+        return True
