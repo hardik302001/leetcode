cdebe2c51c39536ab7bc67b18973a4321c5eb5fe
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2022-12-12 14:26:27 +0000

    Sync LeetCode submission - Maximum Star Sum of a Graph (cpp)

--- problems/maximum_star_sum_of_a_graph/solution.cpp
+++ problems/maximum_star_sum_of_a_graph/solution.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     int maxStarSum(vector<int>& vals, vector<vector<int>>& edges, int k) {
-        
         int n = vals.size();
-        vector<int>indeg(n+1 , 0);
-        vector<vector<int>>g(n+1); 
         int ans = INT_MIN;
-        for(auto x:vals){
+        for(auto x:vals){        // if k==0 -> initial answer
             ans = max(ans , x);
         }
         vector<priority_queue<int>>pq(n+1);
         for(auto x: edges){
             int a = x[0];
             int b = x[1];
-            g[a].push_back(b);
-            g[b].push_back(a);
-            indeg[a]++;
-            indeg[b]++;
             pq[a].push(vals[b]);
             pq[b].push(vals[a]);
     
@@ -30,12 +23,10 @@ public:
             while(kk-- and !pqq.empty()){
                 auto topa = pqq.top(); pqq.pop();
                 s+= topa;
-                ans = max(ans , s);
+                ans = max(ans , s);   // bcz what if we start adding negative values, so maximise at every step
             }
         }
-        
-        
-        
+
         return ans;
         
     }
