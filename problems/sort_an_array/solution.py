bfc7f780fe8be2d69788e7d98982a647c21f808b
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2023-03-01 03:53:32 +0000

    Sync LeetCode submission - Sort an Array (python3)

--- problems/sort_an_array/solution.py
+++ problems/sort_an_array/solution.py
@@ -1,17 +1,25 @@
 #bucket sort
 
 
-#see the most bvotes comment in discuss
-
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
+        def counting_sort():
+            # Create the counting hash map.
+            counts = {}
+            # Find the minimum and maximum values in the array.
+            minVal, maxVal = min(nums), max(nums)
+            # Update element's count in the hash map.
+            for val in nums:
+                counts[val] = counts.get(val, 0) + 1
 
-        dic = {i:0 for i in range(-50000, 50001)}
-        for i in nums:
-            dic[i] += 1
-        res = []
-        for i in dic:
-            for j in range(dic[i]):
-                res.append(i)
+            index = 0
+            # Place each element in its correct position in the array.
+            for val in range(minVal, maxVal + 1, 1):
+                # Append all 'val's together if they exist.
+                while counts.get(val, 0) > 0:
+                    nums[index] = val
+                    index += 1
+                    counts[val] -= 1
 
-        return res
+        counting_sort()
+        return nums
