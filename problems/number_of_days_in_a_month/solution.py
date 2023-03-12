2727c98e33c1eaa2a84374bb4d63d8b6f5646907
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2023-03-12 06:15:34 +0000

    Sync LeetCode submission - Number of Days in a Month (python3)

---
+++ problems/number_of_days_in_a_month/solution.py
@@ -0,0 +1,5 @@
+class Solution:
+    def numberOfDays(self, Y: int, M: int) -> int:
+        if M in {1,3,5,7,8,10,12}: return 31
+        elif M != 2: return 30
+        else: return 28 + (Y % 4 == 0 and Y % 100 != 0 or Y % 400 == 0)
