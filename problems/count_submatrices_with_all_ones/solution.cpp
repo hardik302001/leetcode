fe1408995bd400207aad91302a03bb749ef050ee
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2023-03-28 04:57:04 +0000

    Sync LeetCode submission - Count Submatrices With All Ones (cpp)

---
+++ problems/count_submatrices_with_all_ones/solution.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    // count all submatrices with top-left corner at mat[a][b]
+    int helper(vector<vector<int>> & mat, int a, int b) {
+        
+        int m = mat.size();
+        int n = mat[0].size();
+        int count = 0;
+        int bound = n;
+        
+        for (int i=a; i<m; i++) {
+            for (int j=b; j<bound; j++) {
+                if (mat[i][j]) count += 1;
+                else bound = j;
+            }
+        }
+        
+        return count;
+
+    }
+
+    int numSubmat(vector<vector<int>>& mat) {
+        
+        int m = mat.size();
+        int n = mat[0].size();
+        int count = 0;
+        
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                count += helper(mat, i, j);
+            }
+        }
+        
+        return count;
+
+    }
+};
