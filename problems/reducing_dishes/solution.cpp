eef58f6e2da7bef25068a8fa9287633a9c9438d2
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2023-03-29 06:39:18 +0000

    Sync LeetCode submission - Reducing Dishes (cpp)

--- problems/reducing_dishes/solution.cpp
+++ problems/reducing_dishes/solution.cpp
@@ -40,22 +40,47 @@ public:
 // And similarly for i = 0 and and max of all currents is printed
 
 class Solution {
+
 public:
+
     int maxSatisfaction(vector<int>& satisfaction) {
+
+        
+
         sort(satisfaction.begin(),satisfaction.end());
+
         int n = satisfaction.size();
+
+        
+
         int Cumulative_sum = 0; // From back
 
+        
+
         int ans = 0;
+
         int cur = 0;
+
         for(int i = n-1;i>=0;i--)
+
         {
+
             cur += Cumulative_sum + satisfaction[i];
+
             Cumulative_sum += satisfaction[i];
+
+            
+
             ans = max(ans,cur);
+
         }
 
+        
+
         return ans;
+
+        
+
     }
 
 };
